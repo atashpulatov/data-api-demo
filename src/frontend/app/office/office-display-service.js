6602aeb4785ed25af95edd6dbd8615d8b851d190
Piotr Filipp <pfilipp@microstrategy.com>
2018-05-25 15:03:32 +0200

    Updated display service to use new row object representation.

--- src/frontend/app/office/office-display-service.js
+++ src/frontend/app/office/office-display-service.js
@@ -1,23 +1,21 @@
-import officeApiHelpers from './office-api-helpers.js'
+import officeApiHelpers from './office-api-helpers.js';
 
 export default function displayReport(reportConvertedData) {
-    Excel.run(function(context) {
+    Excel.run(function (context) {
         let sheet = context.workbook.worksheets.getActiveWorksheet();
         let mstrTable = sheet.tables.add('A1:D1', true /* hasHeaders */);
         //mstrTable.name = 'ExpensesTable';
 
         mstrTable.getHeaderRowRange().values = [reportConvertedData.headers];
 
-        mstrTable.rows.add(null /* add rows to the end of the table */,
-            reportConvertedData.rows,
-        );
-
-        /* Alternatively we can use below code but it would require changing converter
-                var newData = transactions.map(item =>
-            [item.DATE, item.MERCHANT, item.CATEGORY, item.AMOUNT]);
-
-            expensesTable.rows.add(null, newData);
-        */
+        let rows = reportConvertedData.rows.map((item) => {
+                let row = [];
+                reportConvertedData.headers.forEach((header) => {
+                    row.push(item[header]);
+                });
+                return row;
+        });
+        mstrTable.rows.add(null, rows);
 
         if (Office.context.requirements.isSetSupported('ExcelApi', 1.2)) {
             sheet.getUsedRange().format.autofitColumns();
@@ -30,7 +28,7 @@ export default function displayReport(reportConvertedData) {
     }).catch(function (error) {
         console.log('error: ' + error);
         if (error instanceof OfficeExtension.Error) {
-          console.log('Debug info: ' + JSON.stringify(error.debugInfo));
+            console.log('Debug info: ' + JSON.stringify(error.debugInfo));
         }
-      });
+    });
 }
