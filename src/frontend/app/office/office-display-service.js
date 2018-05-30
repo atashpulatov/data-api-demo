144423144467017832244cddf4f3098437a79db2
Piotr Filipp <pfilipp@microstrategy.com>
2018-05-30 10:31:37 +0200

    Added dynamic range - we can paste report starting from any cell.

--- src/frontend/app/office/office-display-service.js
+++ src/frontend/app/office/office-display-service.js
@@ -1,9 +1,15 @@
 import officeApiHelpers from './office-api-helpers.js';
 
 export default function displayReport(reportConvertedData) {
-    Excel.run(function (context) {
+    Excel.run( async function(context) {
         let sheet = context.workbook.worksheets.getActiveWorksheet();
-        let range = getRange(reportConvertedData.headers.length);
+
+        let selectedRangeStart = context.workbook.getSelectedRange();
+        selectedRangeStart.load('address');
+        await context.sync();
+
+        let startCell = selectedRangeStart.address.split('!')[1];
+        let range = getRange(reportConvertedData.headers.length, startCell);
         let mstrTable = sheet.tables.add(range, true /* hasHeaders */);
         // mstrTable.name = 'ExpensesTable';
 
@@ -23,7 +29,7 @@ export default function displayReport(reportConvertedData) {
         sheet.activate();
 
         return context.sync();
-    }).catch(function (error) {
+    }).catch(function(error) {
         console.log('error: ' + error);
         if (error instanceof OfficeExtension.Error) {
             console.log('Debug info: ' + JSON.stringify(error.debugInfo));
@@ -31,14 +37,20 @@ export default function displayReport(reportConvertedData) {
     });
 }
 
-function getRange(headerCount) {
+function lettersToNumber(letters) {
+    return letters.split('').reduce((r, a) => r * 26 + parseInt(a, 36) - 9, 0);
+}
+
+function getRange(headerCount, startCell) {
+    let startCellArray = startCell.split(/(\d+)/);
+    headerCount += parseInt(lettersToNumber(startCellArray[0]) - 1);
     let endRange = '';
-        for (let aNumber = 1, bNumber = 26;
-            (headerCount -= aNumber) >= 0;
-            aNumber = bNumber, bNumber *= 26) {
-          endRange = String.fromCharCode(parseInt(
-              (headerCount % bNumber) / aNumber) + 65)
-              + endRange;
-        }
-    return 'A1:'.concat(endRange).concat('1');
+    for (let aNumber = 1, bNumber = 26;
+        (headerCount -= aNumber) >= 0;
+        aNumber = bNumber, bNumber *= 26) {
+        endRange = String.fromCharCode(parseInt(
+            (headerCount % bNumber) / aNumber) + 65)
+            + endRange;
+    }
+    return startCell.concat(':').concat(endRange).concat(startCellArray[1]);
 }
