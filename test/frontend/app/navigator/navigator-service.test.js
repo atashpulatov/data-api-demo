c901eb7f88cec262263ad75ddea32b436c18073f
Piotr Filipp <pfilipp@microstrategy.com>
2018-06-12 16:56:34 +0200

    Created missing tests for navigator service Created test and implemented new /objects root in navigation service.

---
+++ test/frontend/app/navigator/navigator-service.test.js
@@ -0,0 +1,75 @@
+/* es-lint-disable */
+import NavigationService from '../../../../src/frontend/app/navigator/navigation-service';
+import projectRestService from '../../../../src/frontend/app/project/project-rest-service';
+import projectRestServiceMock from '../../../../src/frontend/app/project/project-rest-service-mock';
+import mstrObjectRestService from '../../../../src/frontend/app/mstr-object/mstr-object-rest-service';
+import mstrObjectRestServiceMock from '../../../../src/frontend/app/mstr-object/mstr-object-rest-service-mock';
+/* es-lint-enable */
+
+describe('NavigatorService', () => {
+    beforeAll(() => {
+    });
+
+    it('should give a path object to authentication page',
+            async () => {
+        // given
+        const authToken = sessionStorage.getItem('x-mstr-authtoken');
+        // when
+        const pathObject = await NavigationService.getNavigationRoute();
+        // then
+        expect(authToken).toBeNull();
+        expect(pathObject).toBeDefined();
+        expect(pathObject.pathname).toBeDefined();
+        expect(pathObject.pathname).toContain('/auth');
+        expect(pathObject.state).toBeDefined();
+    });
+    it('should give a path object to project page',
+            async () => {
+        // given
+        const authToken = 'someAuthToken';
+        sessionStorage.setItem('x-mstr-authtoken', authToken);
+        projectRestService.getProjectList = projectRestServiceMock.getProjectList;
+        // when
+        const pathObject = await NavigationService.getNavigationRoute();
+        // then
+        expect(authToken).toBeDefined();
+        expect(pathObject).toBeDefined();
+        expect(pathObject.pathname).toBeDefined();
+        expect(pathObject.pathname).toContain('/projects');
+        expect(pathObject.state.projects).toBeDefined();
+        expect(pathObject.state.projects.length).toBeGreaterThan(1);
+        expect(pathObject.state.projects[0]).toHaveProperty('id');
+        expect(pathObject.state.projects[0]).toHaveProperty('name');
+        expect(pathObject.state.projects[0]).toHaveProperty('alias');
+        expect(pathObject.state.projects[0]).toHaveProperty('description');
+        expect(pathObject.state.projects[0]).toHaveProperty('status');
+    });
+    it('should give a path object to folder contents',
+            async () => {
+        // given
+        const authToken = 'someAuthToken';
+        sessionStorage.setItem('x-mstr-authtoken', authToken);
+        const projectId = 'someProjectId';
+        sessionStorage.setItem('x-mstr-projectid', projectId);
+        mstrObjectRestService.getProjectContent
+                = mstrObjectRestServiceMock.getProjectContent;
+        // when
+        const pathObject = await NavigationService.getNavigationRoute();
+        // then
+        expect(authToken).toBeDefined();
+        expect(pathObject).toBeDefined();
+        expect(pathObject.pathname).toBeDefined();
+        expect(pathObject.pathname).toContain('/objects');
+        const pathObjectSet = pathObject.state.objects;
+        expect(pathObjectSet).toBeDefined();
+        expect(pathObjectSet.length).toBeGreaterThan(1);
+        expect(pathObjectSet[0]).toHaveProperty('id');
+        expect(pathObjectSet[0]).toHaveProperty('name');
+        expect(pathObjectSet[0]).toHaveProperty('type');
+        expect(pathObjectSet[0]).toHaveProperty('description');
+        expect(pathObjectSet[0]).toHaveProperty('subtype');
+        expect(pathObjectSet[0]).toHaveProperty('dateCreated');
+        expect(pathObjectSet[0]).toHaveProperty('dateModified');
+        expect(pathObjectSet[0]).toHaveProperty('version');
+    });
+});
