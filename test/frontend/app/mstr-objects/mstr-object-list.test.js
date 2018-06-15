9bb1502a01673bab42c5fbad876ca890de20b798
Otreba, Dominik <dotreba@microstrategy.com>
2018-06-15 15:42:59 +0200

    Revert "Fixed svg"
    This reverts commit 27325a3fc000ac8e87c1f114c19fe8ec9e86bae9.

--- test/frontend/app/mstr-objects/mstr-object-list.test.js
+++ test/frontend/app/mstr-objects/mstr-object-list.test.js
@@ -22,20 +22,8 @@ describe('MstrObjectList', () => {
         location.state = state;
     });
 
-    // // User sees all data
-    // it('should have all rows', () => {
-    //     // when
-    //     const componentWrapper = mount(<Img location={location} />);
-    //     // then
-
-    //     // mockMstrObjects consists of project representation also,
-    //     // which we don't want to display here
-    //     expect(componentWrapper.find('li'))
-    //         .toHaveLength(mockMstrObjects.length - 1);
-    // });
-
     // User sees all data
-    it('ututut should have all rows', () => {
+    it('should have all rows', () => {
         // when
         const componentWrapper = mount(<MstrObjects location={location} />);
         // then
@@ -46,109 +34,109 @@ describe('MstrObjectList', () => {
             .toHaveLength(mockMstrObjects.length - 1);
     });
 
-    // // User notices directories' info
-    // it('should directory rows be rendered', () => {
-    //     // when
-    //     const componentWrapper = mount(<MstrObjects location={location} />);
-    //     // then
-    //     const items = componentWrapper.find('ul');
-    //     const directories = items.at(0); // directories first
+    // User notices directories' info
+    it('should directory rows be rendered', () => {
+        // when
+        const componentWrapper = mount(<MstrObjects location={location} />);
+        // then
+        const items = componentWrapper.find('ul');
+        const directories = items.at(0); // directories first
 
-    //     directories.children().forEach((row) => {
-    //         const directory = row.props().directory;
-    //         // should have row defined
-    //         expect(directory).toBeDefined();
+        directories.children().forEach((row) => {
+            const directory = row.props().directory;
+            // should have row defined
+            expect(directory).toBeDefined();
 
-    //         // should have name and image
-    //         expect(row.find('h1').text()).toContain('Name:');
-    //         expect(row.find('MockResource').html()).toBeTruthy();
-    //     });
-    // });
+            // should have name and image
+            expect(row.find('h1').text()).toContain('Name:');
+            expect(row.find('img').html()).toBeTruthy();
+        });
+    });
 
-    // // User notices reports' info
-    // it('should report rows be rendered', () => {
-    //     // when
-    //     const componentWrapper = mount(<MstrObjects location={location} />);
-    //     // then
-    //     const items = componentWrapper.find('ul');
-    //     const reports = items.at(1); // reports second
+    // User notices reports' info
+    it('should report rows be rendered', () => {
+        // when
+        const componentWrapper = mount(<MstrObjects location={location} />);
+        // then
+        const items = componentWrapper.find('ul');
+        const reports = items.at(1); // reports second
 
-    //     reports.children().forEach((row) => {
-    //         const report = row.props().report;
-    //         // should have row defined
-    //         expect(report).toBeDefined();
+        reports.children().forEach((row) => {
+            const report = row.props().report;
+            // should have row defined
+            expect(report).toBeDefined();
 
-    //         // should have name and image
-    //         expect(row.find('h1').text()).toContain('Name:');
-    //         expect(row.find('MockResource').html()).toBeTruthy();
-    //     });
-    // });
+            // should have name and image
+            expect(row.find('h1').text()).toContain('Name:');
+            expect(row.find('img').html()).toBeTruthy();
+        });
+    });
 
-    // // User sees directories may be clicked
-    // it('should directory rows be clickable', () => {
-    //     // when
-    //     const componentWrapper = mount(<MstrObjects location={location} />);
-    //     // then
-    //     const items = componentWrapper.find('ul');
-    //     const directories = items.at(0); // directories first
+    // User sees directories may be clicked
+    it('should directory rows be clickable', () => {
+        // when
+        const componentWrapper = mount(<MstrObjects location={location} />);
+        // then
+        const items = componentWrapper.find('ul');
+        const directories = items.at(0); // directories first
 
-    //     directories.children().forEach((row) => {
-    //         const directoryRowLi = row.find('li');
-    //         expect(directoryRowLi.hasClass('cursorIsPointer')).toBeTruthy();
-    //     });
-    // });
+        directories.children().forEach((row) => {
+            const directoryRowLi = row.find('li');
+            expect(directoryRowLi.hasClass('cursorIsPointer')).toBeTruthy();
+        });
+    });
 
-    // // User can click a directory
-    // it('should directory row be reponsive', () => {
-    //     // given
-    //     const originalMethod = MstrObjects.prototype.navigateToDir;
-    //     const mockClick = jest.fn();
-    //     try {
-    //         MstrObjects.prototype.navigateToDir = mockClick;
-    //         // when
-    //         const componentWrapper = mount(<MstrObjects location={location} />);
+    // User can click a directory
+    it('should directory row be reponsive', () => {
+        // given
+        const originalMethod = MstrObjects.prototype.navigateToDir;
+        const mockClick = jest.fn();
+        try {
+            MstrObjects.prototype.navigateToDir = mockClick;
+            // when
+            const componentWrapper = mount(<MstrObjects location={location} />);
 
-    //         // then
-    //         const items = componentWrapper.find('ul');
-    //         const directories = items.at(0); // directories first
+            // then
+            const items = componentWrapper.find('ul');
+            const directories = items.at(0); // directories first
 
-    //         directories.children().forEach((row) => {
-    //             const directoryRowLi = row.find('li');
-    //             directoryRowLi.simulate('click');
-    //             expect(mockClick).toBeCalled();
-    //         });
-    //         expect(originalMethod).toBeDefined();
-    //     } finally {
-    //         MstrObjects.prototype.navigateToDir = originalMethod;
-    //     }
-    // });
+            directories.children().forEach((row) => {
+                const directoryRowLi = row.find('li');
+                directoryRowLi.simulate('click');
+                expect(mockClick).toBeCalled();
+            });
+            expect(originalMethod).toBeDefined();
+        } finally {
+            MstrObjects.prototype.navigateToDir = originalMethod;
+        }
+    });
 
-    // // User can open a directory
-    // it('should pass directory when clicked', () => {
-    //     // given
-    //     const expectedSessionObject = {};
-    //     // when
-    //     const componentWrapper = mount(<MstrObjects location={location} />);
-    //     const mockPush = jest.fn();
-    //     componentWrapper.setProps({ history: { push: mockPush } });
-    //     // then
-    //     const items = componentWrapper.find('ul');
-    //     const directories = items.at(0); // directories first
+    // User can open a directory
+    it('should pass directory when clicked', () => {
+        // given
+        const expectedSessionObject = {};
+        // when
+        const componentWrapper = mount(<MstrObjects location={location} />);
+        const mockPush = jest.fn();
+        componentWrapper.setProps({ history: { push: mockPush } });
+        // then
+        const items = componentWrapper.find('ul');
+        const directories = items.at(0); // directories first
 
-    //     let iterateId = 0;
-    //     directories.children().forEach((row) => {
-    //         const dirId = mockMstrObjects[iterateId].id;
-    //         expectedSessionObject[propertiesEnum.directoryId] = dirId;
-    //         const directoryRowLi = row.find('li');
-    //         directoryRowLi.simulate('click');
-    //         expect(mockPush).toBeCalledWith({
-    //             pathname: '/',
-    //             origin: componentWrapper.props().location,
-    //             sessionObject: expectedSessionObject,
-    //         });
-    //         ++iterateId;
-    //     });
-    // });
+        let iterateId = 0;
+        directories.children().forEach((row) => {
+            const dirId = mockMstrObjects[iterateId].id;
+            expectedSessionObject[propertiesEnum.directoryId] = dirId;
+            const directoryRowLi = row.find('li');
+            directoryRowLi.simulate('click');
+            expect(mockPush).toBeCalledWith({
+                pathname: '/',
+                origin: componentWrapper.props().location,
+                sessionObject: expectedSessionObject,
+            });
+            ++iterateId;
+        });
+    });
 
     // User sees reports may be clicked
 
