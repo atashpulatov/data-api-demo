bf5f5a74192897e9290fede921cc7e39c4d08917
Piotr Filipp <pfilipp@microstrategy.com>
2018-06-27 14:43:56 +0200

    Updated login view.

--- src/frontend/app/header.jsx
+++ src/frontend/app/header.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
+import logo from '../public/assets/mstr_logo.png';
 
 const Header = () => (
-    <div id='content-header' className='padding'>
-        <h1>Microstrategy Office</h1>
+    <div id='content-header'>
+        <img src={logo} className='header-image' />
+        <p className='header'>Microstrategy Office</p>
     </div>
 );
 
