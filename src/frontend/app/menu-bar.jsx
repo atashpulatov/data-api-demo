207952871ed2fc5df6903515b0c269ea956bdcae
Otreba, Dominik <dotreba@microstrategy.com>
2018-07-05 14:38:27 +0200

    Two other buttons implemented

--- src/frontend/app/menu-bar.jsx
+++ src/frontend/app/menu-bar.jsx
@@ -1,28 +1,45 @@
 import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
 import './menu-bar.css';
 import { historyProperties } from './history/history-properties';
-import { withRouter } from 'react-router'
+import { withRouter } from 'react-router';
 
 const back = '‹';
 const settings = '⏣';
 const logout = '⏏';
 const goTop = '«';
 
-const goUpObject = {
-    pathname: '/',
-    historyObject: {},
-};
-goUpObject.historyObject[historyProperties.command] = historyProperties.goUp;
-
 class MenuBar extends Component {
     constructor(props) {
         super(props);
 
         this.pushHistory = this.pushHistory.bind(this);
+
+        this.goUpObject = {
+            pathname: '/',
+            historyObject: {},
+        };
+        this.goUpObject.historyObject[historyProperties.command] =
+            historyProperties.goUp;
+
+        this.goProjectsObject = {
+            pathname: '/',
+            historyObject: {},
+        };
+        this.goUpObject.historyObject[historyProperties.command] =
+            historyProperties.goToProject;
+
+        this.logOutObject = {
+            pathname: '/',
+            historyObject: {},
+        };
+        this.logOutObject.historyObject[historyProperties.command] =
+            historyProperties.logOut;
     }
 
-    pushHistory() {
-        this.props.history.push(goUpObject);
+    pushHistory(historyObject) {
+        console.log(this);
+
+        this.props.history.push(historyObject);
     }
 
     render() {
@@ -30,13 +47,15 @@ class MenuBar extends Component {
             <div className='menu-bar-container'>
                 <div className='menu-bar-nav-container'>
                     <button className='menu-button menu-button-nav'
-                        onClick={this.pushHistory}>
+                        onClick={this.pushHistory.bind(this, this.goUpObject)}>
                         <div className='button-with-tooltip'>
                             {back}
                             <span className='button-tooltip-right'>Back</span>
                         </div>
                     </button>
-                    <button className='menu-button menu-button-nav'>
+                    <button className='menu-button menu-button-nav'
+                        onClick={this.pushHistory.bind(this,
+                            this.goProjectsObject)}>
                         <div className='button-with-tooltip'>
                             {goTop}
                             <span className='button-tooltip-right'>Go top</span>
@@ -45,7 +64,9 @@ class MenuBar extends Component {
                 </div>
                 <div className='menu-bar-options-container'>
                     {/* <button className='menu-button menu-button-nav'>{forward}</button> */}
-                    <button className='menu-button menu-button-options'>
+                    <button className='menu-button menu-button-options'
+                        onClick={this.pushHistory.bind(this,
+                            this.logOutObject)}>
                         <div className='button-with-tooltip'>
                             {logout}
                             <span className='button-tooltip-left'>Log out</span>
