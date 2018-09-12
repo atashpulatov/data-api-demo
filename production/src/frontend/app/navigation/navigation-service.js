b2f0759d7eb374c1a27dc6c25653d6c4a5ec0357
Piotr Filipp <pfilipp@mac-pfilipp.local>
2018-09-12 15:44:06 +0200

    Further tests for Office Display Service.

--- production/src/frontend/app/navigation/navigation-service.js
+++ production/src/frontend/app/navigation/navigation-service.js
@@ -38,37 +38,6 @@ class NavigationService {
         }
         return this.getObjectsRoute();
     }
-
-    saveSessionData(propertiesToSave) {
-        if (propertiesToSave[sessionProperties.username]) {
-            reduxStore.dispatch({
-                type: sessionProperties.actions.logIn,
-                username: propertiesToSave[sessionProperties.username],
-                envUrl: propertiesToSave[sessionProperties.envUrl],
-                isRememberMeOn: propertiesToSave[sessionProperties.isRememberMeOn],
-            });
-        }
-        if (propertiesToSave[sessionProperties.authToken]) {
-            reduxStore.dispatch({
-                type: sessionProperties.actions.loggedIn,
-                authToken: propertiesToSave[sessionProperties.authToken],
-            });
-        }
-        if (propertiesToSave[historyProperties.projectId]) {
-            reduxStore.dispatch({
-                type: historyProperties.actions.goInsideProject,
-                projectId: propertiesToSave[historyProperties.projectId],
-                projectName: propertiesToSave[historyProperties.projectName],
-            });
-            return;
-        }
-        if (propertiesToSave[historyProperties.directoryId]) {
-            reduxStore.dispatch({
-                type: historyProperties.actions.goInside,
-                dirId: propertiesToSave[historyProperties.directoryId],
-            });
-        }
-    }
 }
 
 export const navigationService = new NavigationService();
--- production/test/frontend/app/office/office-display-service.test.js
+++ production/test/frontend/app/office/office-display-service.test.js
@@ -1,6 +1,9 @@
+/* eslint-disable */
 import { officeApiHelper } from '../../../../src/frontend/app/office/office-api-helper';
 import { officeContextMock } from './__mock__object__/OfficeContext';
 import { officeDisplayService } from '../../../../src/frontend/app/office/office-display-service';
+import { OfficeBindingError } from '../../../../src/frontend/app/office/office-error';
+/* eslint-enable */
 
 describe('OfficeDisplayService', () => {
     let originalMethod;
@@ -13,8 +16,88 @@ describe('OfficeDisplayService', () => {
     afterAll(() => {
         officeApiHelper._getOfficeContext = originalMethod;
     });
+
+    it('should return proper bindingId', () => {
+        // given
+        const reportId = 'someReportId';
+        const reportName = 'someReportName';
+        const convertedReportDataMock = {
+            id: reportId,
+            name: reportName,
+        };
+        const separator = '_';
+        const startCell = 'someCELL';
+        const expectedBindId = `${reportName}_${startCell}_${reportId}`;
+        // when
+        const receivedBindId = officeDisplayService._createBindingId(convertedReportDataMock, startCell, separator);
+        // then
+        expect(receivedBindId).toEqual(expectedBindId);
+    });
+    it('should return proper bindingId with different separator', () => {
+        // given
+        const reportId = 'someReportId';
+        const reportName = 'someReportName';
+        const convertedReportDataMock = {
+            id: reportId,
+            name: reportName,
+        };
+        const separator = '-';
+        const startCell = 'someCELL';
+        const expectedBindId = `${reportName}-${startCell}-${reportId}`;
+        // when
+        const receivedBindId = officeDisplayService._createBindingId(convertedReportDataMock, startCell, separator);
+        // then
+        expect(receivedBindId).toEqual(expectedBindId);
+    });
+    it('should throw error due to missing convertedReportData', () => {
+        // given
+        const convertedReportDataMock = undefined;
+        const separator = '-';
+        const startCell = 'someCELL';
+        // when
+        const wrongMethodCall = () => {
+            officeDisplayService._createBindingId(convertedReportDataMock, startCell, separator);
+        };
+        // then
+        expect(wrongMethodCall).toThrowError(OfficeBindingError);
+        expect(wrongMethodCall).toThrowError('Missing reportConvertedData');
+    });
+    it('should throw error due to missing startCell', () => {
+        // given
+        const reportId = 'someReportId';
+        const reportName = 'someReportName';
+        const convertedReportDataMock = {
+            id: reportId,
+            name: reportName,
+        };
+        const startCell = undefined;
+        const separator = '-';
+        // when
+        const wrongMethodCall = () => {
+            officeDisplayService._createBindingId(convertedReportDataMock, startCell, separator);
+        };
+        // then
+        expect(wrongMethodCall).toThrowError(OfficeBindingError);
+        expect(wrongMethodCall).toThrowError('Missing startCell');
+    });
+    it('should return proper bindingId despite not providing separator', () => {
+        // given
+        const reportId = 'someReportId';
+        const reportName = 'someReportName';
+        const convertedReportDataMock = {
+            id: reportId,
+            name: reportName,
+        };
+        const startCell = 'someCELL';
+        const expectedBindId = `${reportName}_${startCell}_${reportId}`;
+        // when
+        const receivedBindId = officeDisplayService._createBindingId(convertedReportDataMock, startCell);
+        // then
+        expect(receivedBindId).toEqual(expectedBindId);
+    });
+
     describe('_insertDataIntoExcel', async () => {
-        it('should test', async () => {
+        it('should return table husk with proper name and invoke required methods', async () => {
             // given
             const getActiveWorksheetMock = jest.fn();
             const mockedTable = {
@@ -34,8 +117,9 @@ describe('OfficeDisplayService', () => {
             officeDisplayService._formatTable = mockedFormatTable;
             getActiveWorksheetMock.mockReturnValue(mockedWorksheet);
             officeContextMock.workbook.worksheets.getActiveWorksheet = getActiveWorksheetMock;
-
+            const startCell = 'A1';
             const reportConvertedData = {
+                name: 'testName',
                 headers: {
                     length: 2,
                 },
@@ -43,9 +127,10 @@ describe('OfficeDisplayService', () => {
             // when
             const context = officeContextMock;
             // then
-            const result = await officeDisplayService._insertDataIntoExcel(reportConvertedData, context, 'A1:A10');
+            const result = await officeDisplayService._insertDataIntoExcel(reportConvertedData, context, startCell);
             // then
             expect(getActiveWorksheetMock).toBeCalled();
+            expect(result.name).toEqual(`${reportConvertedData.name}${startCell}`);
         });
     });
     it('it should fail', () => {
