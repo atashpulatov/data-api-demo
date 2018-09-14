c84790915cc942ceb714274d585129b131c39323
Piotr Filipp <pfilipp@mac-pfilipp.local>
2018-09-14 16:47:15 +0200

    Redo navigation to antd

--- production/src/frontend/app/menu-bar/menu-bar.jsx
+++ production/src/frontend/app/menu-bar/menu-bar.jsx
@@ -2,13 +2,9 @@
 import React, { Component } from 'react';
 import { menuBarService } from './menu-bar-service';
 import { connect } from 'react-redux';
+import { Icon, Tooltip } from 'antd';
 import './menu-bar.css';
 /* eslint-enable */
-
-const back = '‹';
-const settings = '⏣';
-const logout = '⏏';
-const goTop = '«';
 
 export class _MenuBar extends Component {
     render() {
@@ -17,35 +13,31 @@ export class _MenuBar extends Component {
                 ?
                 <div className='menu-bar-container'>
                     <div className='menu-bar-nav-container'>
-                        <button className='menu menu-nav'
-                            onClick={menuBarService.goUp}>
-                            <div className='with-tooltip'>
-                                {back}
-                                <span className='tooltip right'>Back</span>
-                            </div>
-                        </button>
-                        <button className='menu menu-nav'
-                            onClick={menuBarService.goProjects}>
-                            <div className='with-tooltip'>
-                                {goTop}
-                                <span className='tooltip right'>Go top</span>
-                            </div>
-                        </button>
+                        <Tooltip placement="bottomLeft" title='Back'>
+                            <button className='menu menu-nav'
+                                onClick={menuBarService.goUp}>
+                                <Icon type='left' />
+                            </button>
+                        </Tooltip>
+                        <Tooltip placement="bottom" title='Go top'>
+                            <button className='menu menu-nav'
+                                onClick={menuBarService.goProjects}>
+                                <Icon type='double-left' />
+                            </button>
+                        </Tooltip>
                     </div>
                     <div className='menu-bar-options-container'>
-                        <button className='menu menu-options'
-                            onClick={menuBarService.logOut}>
-                            <div className='with-tooltip'>
-                                {logout}
-                                <span className='tooltip left'>Log out</span>
-                            </div>
-                        </button>
-                        <button className='menu menu-options'>
-                            <div className='with-tooltip'>
-                                {settings}
-                                <span className='tooltip left'>Settings</span>
-                            </div>
-                        </button>
+                        <Tooltip placement="bottom" title='Log out'>
+                            <button className='menu menu-options'
+                                onClick={menuBarService.logOut}>
+                                <Icon type='logout' />
+                            </button>
+                        </Tooltip>
+                        <Tooltip placement="bottomRight" title='Settings'>
+                            <button className='menu menu-options'>
+                                <Icon type='setting' />
+                            </button>
+                        </Tooltip>
                     </div>
                 </div>
                 : null
