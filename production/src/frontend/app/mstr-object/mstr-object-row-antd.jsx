5065d164beddf0ce75923f2393b2b3ac416d918f
Piotr Filipp <pfilipp@mac-pfilipp.local>
2018-10-03 13:56:35 +0200

    Implemented UI changes to allow user to either import whole report or choose data.

--- production/src/frontend/app/mstr-object/mstr-object-row-antd.jsx
+++ production/src/frontend/app/mstr-object/mstr-object-row-antd.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import React from 'react';
-import { Row, Col, Icon, message } from 'antd';
+import { Row, Col, Icon, message, Tooltip } from 'antd';
 /* eslint-enable */
 
 export const DirectoryRow = ({ directory, onClick }) => (
@@ -18,20 +18,31 @@ export const DirectoryRow = ({ directory, onClick }) => (
     </Row>
 );
 
-export const ReportRow = ({ report, onClick }) => (
+export const ReportRow = ({ report, onClick, onFilterReport }) => (
     <Row
         type='flex'
-        className='cursor-is-pointer row'
-        onClick={ async () => {
-            await onClick(report.id);
-            message.success(`Loaded report: ${report.name}`);
-        }}>
+        className='cursor-is-pointer row'>
         <Col>
             <Icon type="file-text" theme="outlined" />
         </Col>
         <Col
-            offset={1}>
+            onClick={async () => {
+                await onClick(report.id);
+                message.success(`Loaded report: ${report.name}`);
+            }}
+            span={18}
+            offset={1}
+        >
             {report.name}
+        </Col>
+        <Col
+            offset={1}
+            onClick={() => {
+                onFilterReport(report.id);
+            }}>
+            <Tooltip placement="left" title='Choose data'>
+                <Icon type="filter" theme="outlined" />
+            </Tooltip>
         </Col>
     </Row>
 );
