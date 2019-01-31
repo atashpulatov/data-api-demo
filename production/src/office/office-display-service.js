58206acb69c1dbf5cb35d9dd06c3176d315790b8
Gaszczak, Jakub <jgaszczak@microstrategy.com>
2019-01-31 15:28:42 +0100

    Resolving conflicts

--- production/src/office/office-display-service.js
+++ production/src/office/office-display-service.js
@@ -3,11 +3,8 @@ import { mstrObjectRestService } from '../mstr-object/mstr-object-rest-service';
 import { officeConverterService } from './office-converter-service';
 import { reduxStore } from '../store';
 import { officeProperties } from './office-properties';
-import { globalDefinitions } from '../global-definitions';
-import { sessionHelper } from '../storage/session-helper';
 import { officeStoreService } from './store/office-store-service';
-
-const separator = globalDefinitions.reportBindingIdSeparator;
+import { errorService } from '../error/error-handler';
 
 class OfficeDisplayService {
     constructor() {
@@ -17,42 +14,43 @@ class OfficeDisplayService {
         this.refreshReport = this.refreshReport.bind(this);
     }
 
-    async printObject(objectId, startCell, officeTableId, bindingId, body) {
-        sessionHelper.enableLoading();
-        const excelContext = await officeApiHelper.getExcelContext();
-        if (!startCell) {
-            startCell = await officeApiHelper.getSelectedCell(excelContext);
-        }
-        const jsonData = await mstrObjectRestService.getObjectContent(objectId, body);
-        if (jsonData && jsonData.result.data.root == null){
-            //report returned no data
+    printObject = async (objectId, startCell, officeTableId, bindingId, body) => {
+        try {
+            const excelContext = await officeApiHelper.getExcelContext();
+            startCell = startCell || await officeApiHelper.getSelectedCell(excelContext);
+            const jsonData = await mstrObjectRestService.getObjectContent(objectId, body);
+            if (jsonData && jsonData.result.data.root == null){
+                //report returned no data
+                sessionHelper.disableLoading();
+                return {success: false, message: 'No data returned by the report: ' + jsonData.name};
+            }
+            const convertedReport = officeConverterService
+                .getConvertedTable(jsonData);
+            const newOfficeTableId = officeTableId || await officeApiHelper.findAvailableOfficeTableId(excelContext);
+            await this._insertDataIntoExcel(convertedReport, excelContext, startCell, newOfficeTableId);
+            const { envUrl, projectId } = officeApiHelper.getCurrentMstrContext();
+            bindingId = bindingId || newOfficeTableId;
+            await excelContext.sync();
+            officeApiHelper.bindNamedItem(newOfficeTableId, bindingId);
+            if (!officeTableId) {
+                await this.addReportToStore({
+                    id: convertedReport.id,
+                    name: convertedReport.name,
+                    bindId: bindingId,
+                    tableId: newOfficeTableId,
+                    projectId,
+                    envUrl,
+                });
+            }
             sessionHelper.disableLoading();
-            return {success: false, message: 'No data returned by the report: ' + jsonData.name};
+            return {success: true, message: 'Loaded report: ' + jsonData.name};    
+        } catch (error) {
+            throw errorService.errorOfficeFactory(error);
         }
-        const convertedReport = officeConverterService
-            .getConvertedTable(jsonData);
-        const newOfficeTableId = officeTableId || await officeApiHelper.findAvailableOfficeTableId(convertedReport.name, excelContext);
-        await this._insertDataIntoExcel(convertedReport, excelContext, startCell, newOfficeTableId);
-        const { envUrl, projectId } = officeApiHelper.getCurrentMstrContext();
-        bindingId = bindingId || newOfficeTableId;
-        await excelContext.sync();
-        officeApiHelper.bindNamedItem(newOfficeTableId, bindingId);
-        if (!officeTableId) {
-            await this.addReportToStore({
-                id: convertedReport.id,
-                name: convertedReport.name,
-                bindId: bindingId,
-                tableId: newOfficeTableId,
-                projectId,
-                envUrl,                
-            });
-        }
-        sessionHelper.disableLoading();
-        return {success: true, message: 'Loaded report: ' + jsonData.name};
     }
 
     // TODO: move it to api helper?
-    addReportToStore(report) {
+    addReportToStore = (report) => {
         reduxStore.dispatch({
             type: officeProperties.actions.loadReport,
             report: {
@@ -67,7 +65,7 @@ class OfficeDisplayService {
         officeStoreService.preserveReport(report);
     }
 
-    async removeReportFromExcel(bindingId, isRefresh) {
+    removeReportFromExcel = async (bindingId, isRefresh) => {
         const officeContext = await officeApiHelper.getOfficeContext();
         await officeContext.document.bindings.releaseByIdAsync(bindingId, (asyncResult) => {
             console.log('released binding');
@@ -85,9 +83,8 @@ class OfficeDisplayService {
         }
     }
 
-    // FIXME: report after refresh goes to bottom of list
     // TODO: we could filter data to display options related to current envUrl
-    async refreshReport(bindingId) {
+    refreshReport = async (bindingId) => {
         const isRefresh = true;
         const excelContext = await officeApiHelper.getExcelContext();
         const range = officeApiHelper.getBindingRange(excelContext, bindingId);
@@ -99,7 +96,7 @@ class OfficeDisplayService {
         await this.printObject(refreshReport.id, startCell, bindingId, bindingId);
     }
 
-    async _insertDataIntoExcel(reportConvertedData, context, startCell, tableName) {
+    _insertDataIntoExcel = async (reportConvertedData, context, startCell, tableName) => {
         const hasHeaders = true;
         const sheet = context.workbook.worksheets.getActiveWorksheet();
         const range = officeApiHelper
@@ -114,7 +111,7 @@ class OfficeDisplayService {
         return mstrTable;
     }
 
-    _pushRows(reportConvertedData, mstrTable) {
+    _pushRows = (reportConvertedData, mstrTable) => {
         const dataRows = reportConvertedData.rows
             .map((item) => reportConvertedData.headers
                 .map((header) => item[header]));
