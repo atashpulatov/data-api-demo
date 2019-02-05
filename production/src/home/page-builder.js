1ba57da8d1a7d1ef0adde006ae60e214d9050267
asedano <asedano@microstrategy.com>
2019-02-05 17:23:32 +0100

    Add tabs component in placeholder

--- production/src/home/page-builder.js
+++ production/src/home/page-builder.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Header} from './header.jsx';
-import {MenuBar} from '../menu-bar/menu-bar.jsx';
 import {FileHistoryContainer} from '../file-history/file-history-container.jsx';
 import {Spin} from 'antd';
 import {Notifications} from '../notification/notifications.jsx';
@@ -9,19 +8,20 @@ import {Placeholder} from './placeholder.jsx';
 
 class PageBuilder {
     getPage = (loading, authToken, reportArray) => {
-      console.log('getting page');
-      return (
-        <div id='content'>
-          <Notifications />
-          <Header />
-          {authToken && <MenuBar />}
-          {(reportArray && reportArray.length !== 0) && <FileHistoryContainer />}
-          {!(reportArray && reportArray.length !== 0) && <Placeholder />}
-          <Spin spinning={loading}>
-            {!authToken && <Authenticate />}
-          </Spin>
-        </div>
-      );
+        console.log('getting page');
+        return (
+            <div id='content'>
+                <Notifications />
+                <Header />
+                {/* Logout button will be moved next to the username in the header */}
+                {/* {authToken && <MenuBar />} */}
+                {(reportArray && reportArray.length !== 0) && <FileHistoryContainer />}
+                {!(reportArray && reportArray.length !== 0) && <Placeholder />}
+                <Spin spinning={loading}>
+                    {!authToken && <Authenticate />}
+                </Spin>
+            </div>
+        );
     }
 }
 
