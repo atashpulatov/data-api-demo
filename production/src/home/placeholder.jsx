4f7e7305dfd7ade3f1b575a17afb9bb569510cec
asedano <asedano@microstrategy.com>
2019-02-05 16:32:27 +0100

    UI for get started container

--- production/src/home/placeholder.jsx
+++ production/src/home/placeholder.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import image from '../ynodata.jpeg';
-import { sessionHelper } from '../storage/session-helper';
-import { Button } from 'antd';
-import { popupController } from '../popup-controller';
+import {sessionHelper} from '../storage/session-helper';
+import {Button} from 'antd';
+import {popupController} from '../popup-controller';
+
 
 export const Placeholder = () => {
-    sessionHelper.disableLoading();
-    return (
-        <div>
-            <img width='350px' src={image} />
-            <Button
-                type='primary'
-                onClick={popupController.runPopupNavigation}>
+  sessionHelper.disableLoading();
+  return (
+    <div className='get-started-container'>
+      <img width='189px' height='108px' src='/assets/folder-art.svg' alt='Office Add-in logo' />
+      <h2>Let's get started</h2>
+      <p>You haven’t imported any report/dataset yet. Let’s import data to start!</p>
+      <Button
+        type='primary'
+        onClick={popupController.runPopupNavigation}>
                 Import data
-            </Button>
-        </div>
-    );
+      </Button>
+    </div>
+  );
 };
