f827e88e3e9f5d0941fb335835887476de60118e
asedano <asedano@microstrategy.com>
2019-02-07 17:13:06 +0100

    Prepare components for filter panel

--- production/src/attribute-selector/attribute-selector.jsx
+++ production/src/attribute-selector/attribute-selector.jsx
@@ -1,27 +1,25 @@
-/* eslint-disable */
-import React, { Component } from 'react';
-import { AttributeMetricFilter, ErrorBoundary } from 'mstr-react-library';
-/* esling-enable */
-
+import React, {Component} from 'react';
+import {AttributeMetricFilter, ErrorBoundary} from 'mstr-react-library';
 export class AttributeSelector extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: false,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+    };
+  }
 
-    render() {
-        return (
-            <ErrorBoundary>
-                <AttributeMetricFilter
-                    key={this.props.reportId}
-                    session={this.props.session}
-                    triggerUpdate={this.props.triggerUpdate}
-                    onTriggerUpdate={this.props.onTriggerUpdate}
-                    withDataPreview
-                    reportId={this.props.reportId} />
-            </ErrorBoundary>
-        );
-    }
+  render() {
+    return (
+      <ErrorBoundary>
+        <AttributeMetricFilter
+          key={this.props.reportId}
+          session={this.props.session}
+          triggerUpdate={this.props.triggerUpdate}
+          onTriggerUpdate={this.props.onTriggerUpdate}
+          withDataPreview
+          reportId={this.props.reportId}
+          withFolderTree={false} />
+      </ErrorBoundary>
+    );
+  }
 }
