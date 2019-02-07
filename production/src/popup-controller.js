4815534f995d80c155176e779d349c184ca2b941
asedano <asedano@microstrategy.com>
2019-02-07 09:59:08 +0100

    Add Excel global variable to eslint config

--- production/src/popup-controller.js
+++ production/src/popup-controller.js
@@ -1,48 +1,48 @@
-import { environment } from './global-definitions';
-import { officeContext } from './office/office-context';
-import { selectorProperties } from './attribute-selector/selector-properties';
-import { officeDisplayService } from './office/office-display-service';
-import { PopupTypeEnum } from './home/popup-type-enum';
-import { sessionHelper } from './storage/session-helper';
+import {environment} from './global-definitions';
+import {officeContext} from './office/office-context';
+import {selectorProperties} from './attribute-selector/selector-properties';
+import {officeDisplayService} from './office/office-display-service';
+import {PopupTypeEnum} from './home/popup-type-enum';
+import {sessionHelper} from './storage/session-helper';
 
-class PopupController{
+class PopupController {
     runPopupNavigation = () => {
-        const session = sessionHelper.getSession();
-        const excelObject = officeContext.getExcel();
-        Excel.run(async (context) => {
-            const officeObject = officeContext.getOffice();
-            officeObject.context.ui.displayDialogAsync(
-                `${environment.scheme}://${environment.host}:${environment.port}/popup.html`
+      const session = sessionHelper.getSession();
+      const excelObject = officeContext.getExcel();
+      Excel.run(async (context) => {
+        const officeObject = officeContext.getOffice();
+        officeObject.context.ui.displayDialogAsync(
+            `${environment.scheme}://${environment.host}:${environment.port}/popup.html`
                 + '?popupType=' + PopupTypeEnum.navigationTree
                 + '&envUrl=' + session.url
                 + '&token=' + session.authToken,
-                { height: 70, width: 75, displayInIframe: true },
-                (asyncResult) => {
-                    const dialog = asyncResult.value;
-                    dialog.addEventHandler(
-                        officeObject.EventType.DialogMessageReceived,
-                        this.onMessageFromPopup.bind(null, dialog));
-                });
-            await context.sync();
-        });
+            {height: 80, width: 80, displayInIframe: true},
+            (asyncResult) => {
+              const dialog = asyncResult.value;
+              dialog.addEventHandler(
+                  officeObject.EventType.DialogMessageReceived,
+                  this.onMessageFromPopup.bind(null, dialog));
+            });
+        await context.sync();
+      });
     }
 
     onMessageFromPopup = (dialog, arg) => {
-        const message = arg.message
-        const response = JSON.parse(message);
-        switch (response.command) {
-            case selectorProperties.commandOk:
-                if (response.chosenObject) {
-                    officeDisplayService.printObject(response.chosenObject, response.chosenProject);
-                }
-                dialog.close();
-                break;
-            case selectorProperties.commandCancel:
-                dialog.close();
-                break;
-            default:
-                break;
-        }
+      const message = arg.message;
+      const response = JSON.parse(message);
+      switch (response.command) {
+        case selectorProperties.commandOk:
+          if (response.chosenObject) {
+            officeDisplayService.printObject(response.chosenObject, response.chosenProject);
+          }
+          dialog.close();
+          break;
+        case selectorProperties.commandCancel:
+          dialog.close();
+          break;
+        default:
+          break;
+      }
     }
 };
 
