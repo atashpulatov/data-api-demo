2b350391722c7f79bde17c453da0d209031ce1e8
asedano <asedano@microstrategy.com>
2019-02-18 10:58:58 +0100

    Add mstr-object-rest-service

---
+++ production/src/mstr-object/mstr-object-rest-service.js
@@ -0,0 +1,78 @@
+import {errorService} from '../error/error-handler';
+import {reduxStore} from '../store';
+import {moduleProxy} from '../module-proxy';
+
+const sharedFolderIdType = 7;
+
+class MstrObjectRestService {
+  async getProjectContent(envUrl, authToken, projectId,
+      folderType = sharedFolderIdType) {
+    const fullPath = `${envUrl}/folders/preDefined/${folderType}`;
+    return await moduleProxy.request
+        .get(fullPath)
+        .set('x-mstr-authtoken', authToken)
+        .set('x-mstr-projectid', projectId)
+        .withCredentials()
+        .then((res) => {
+          return res.body;
+        })
+        .catch((err) => {
+          throw errorService.errorRestFactory(err);
+        });
+  }
+
+  async getFolderContent(envUrl, authToken, projectId, folderId) {
+    const fullPath = `${envUrl}/folders/${folderId}`;
+    return await moduleProxy.request
+        .get(fullPath)
+        .set('x-mstr-authtoken', authToken)
+        .set('x-mstr-projectid', projectId)
+        .withCredentials()
+        .then((res) => {
+          return res.body;
+        })
+        .catch((err) => {
+          throw errorService.errorRestFactory(err);
+        });
+  }
+
+  async _getInstanceId(fullPath, authToken, projectId, body) {
+    return await moduleProxy.request
+        .post(fullPath)
+        .set('x-mstr-authtoken', authToken)
+        .set('x-mstr-projectid', projectId)
+        .send(body)
+        .withCredentials()
+        .then((res) => {
+          return res.body.instanceId;
+        })
+        .catch((err) => {
+          throw errorService.errorRestFactory(err);
+        });
+  }
+
+  async getObjectContent(objectId, projectId, isReport = true, body) {
+    const storeState = reduxStore.getState();
+    const envUrl = storeState.sessionReducer.envUrl;
+    const authToken = storeState.sessionReducer.authToken;
+    const objectType = isReport ? 'reports' : 'cubes';
+    let fullPath = `${envUrl}/${objectType}/${objectId}/instances`;
+
+    const reportInstance = await this._getInstanceId(fullPath, authToken, projectId, body);
+    fullPath += `/${reportInstance}`;
+    return await moduleProxy.request
+        .get(fullPath)
+        .set('x-mstr-authtoken', authToken)
+        .set('x-mstr-projectid', projectId)
+        .withCredentials()
+        .then((res) => {
+          return res.body;
+        })
+        .catch((err) => {
+          throw errorService.errorRestFactory(err);
+        });
+  }
+};
+
+
+export const mstrObjectRestService = new MstrObjectRestService();
