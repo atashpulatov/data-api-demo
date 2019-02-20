4034dc64374d73dc9db37778615fc05a7de93009
Piotr Filipp <pfilipp@mac-pfilipp.local>
2019-02-20 15:10:30 +0100

    Refactored the way we extract cookies from browser. Finished tests

--- production/src/home/home-helper.js
+++ production/src/home/home-helper.js
@@ -1,16 +1,17 @@
-import { reduxStore } from '../store';
-import { sessionHelper } from '../storage/session-helper';
+import {reduxStore} from '../store';
+import {sessionHelper} from '../storage/session-helper';
 
 class HomeHelper {
   saveLoginValues = () => {
     const token = reduxStore.getState().sessionReducer.authToken;
-    if (window.location.origin.search('localhost') !== -1) {
-      if (!token) {
+    const location = this.getWindowLocation();
+    if (location.origin.search('localhost') !== -1) {
+      if (token === undefined || token === '') {
         sessionHelper.logOut();
         return;
       }
     } else {
-      const envUrl = `${window.location.origin}/MicroStrategyLibrary/api`;
+      const envUrl = `${location.origin}/MicroStrategyLibrary/api`;
       const values = {
         envUrl,
       };
@@ -18,27 +19,34 @@ class HomeHelper {
     }
   };
 
-  getCookiesToArray = () => {
-    const cookieJar = document.cookie;
-    const splittedCookies = cookieJar.split(';');
-    return splittedCookies.map((cookie) => {
-      const slicedCookie = cookie.split('=');
-      return {
-        name: slicedCookie[0],
-        value: slicedCookie[1],
-      };
-    });
+  getParsedCookies = () => {
+    const cookieJar = this.getDocumentCookie();
+    return cookieJar.split(';')
+        .reduce((res, c) => {
+          const [key, val] = c.trim().split('=').map(decodeURIComponent);
+          const allNumbers = (str) => /^\d+$/.test(str);
+          try {
+            return Object.assign(res, {[key]: allNumbers(val) ? val : JSON.parse(val)});
+          } catch (e) {
+            return Object.assign(res, {[key]: val});
+          }
+        }, {});
   };
 
   saveTokenFromCookies = () => {
-    const splittedCookiesJar = this.getCookiesToArray();
-    const authToken = splittedCookiesJar.filter((cookie) => {
-      return cookie.name === ' iSession';
-    });
-    if (authToken[0]) {
-      sessionHelper.logIn(authToken[0].value);
+    const splittedCookiesJar = this.getParsedCookies();
+    if (splittedCookiesJar.iSession) {
+      sessionHelper.logIn(splittedCookiesJar.iSession);
     }
   };
+
+  getWindowLocation = () => {
+    return window.location;
+  }
+
+  getDocumentCookie = () => {
+    return document.cookie;
+  }
 }
 
 export const homeHelper = new HomeHelper();
--- production/test/home/home.test.js
+++ production/test/home/home.test.js
@@ -1,15 +1,18 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import {Provider} from 'react-redux';
-import {shallow, mount} from 'enzyme';
-import {Home, _Home} from '../../src/home/home.jsx';
-import {Header} from '../../src/home/header.jsx';
-import {sessionHelper} from '../../src/storage/session-helper';
-import {officeApiHelper} from '../../src/office/office-api-helper';
-import {reduxStore} from '../../src/store';
+import { Provider } from 'react-redux';
+import { mount } from 'enzyme';
+import { Home, _Home } from '../../src/home/home.jsx';
+import { Header } from '../../src/home/header.jsx';
+import { sessionHelper } from '../../src/storage/session-helper';
+import { officeApiHelper } from '../../src/office/office-api-helper';
+import { reduxStore } from '../../src/store';
+import { homeHelper } from '../../src/home/home-helper.js';
+import { pageBuilder } from '../../src/home/page-builder.js';
 
 jest.mock('../../src/storage/session-helper');
 jest.mock('../../src/office/office-api-helper');
+jest.mock('../../src/home/home-helper');
 
 /* eslint-enable  */
 
@@ -21,52 +24,53 @@ describe('Home', () => {
     const sessionHelperSpy = jest.spyOn(sessionHelper, 'disableLoading');
     const officeHelperSpy = jest
         .spyOn(officeApiHelper, 'loadExistingReportBindingsExcel')
-        .mockImplementation( async () => null);
+        .mockImplementation(async () => null);
     sessionHelperSpy.mockClear();
     officeHelperSpy.mockClear();
     // when
     const componentWrapper = mount(
-        <Provider store={reduxStore}>
-          <Home />
-        </Provider>
+      <Provider store={reduxStore}>
+        <Home />
+      </Provider>
     );
     // then
     setImmediate(() => tempPromise);
-    await(tempPromise);
+    await (tempPromise);
     expect(componentWrapper.contains(headerWrapper.get(0))).toBe(true);
     expect(officeHelperSpy).toHaveBeenCalled();
     expect(sessionHelperSpy).toHaveBeenCalled();
   });
 
-  it('should try to login user', () => {
+  it('should trigger saveLoginValues and saveTokenFromCookies on mount', () => {
     // given
     const props = {
       loading: false,
       authToken: false,
       reportArray: false,
     };
-    const sessionHelperSpy = jest.spyOn(sessionHelper, 'logIn');
-    const componentWrapper = shallow(<_Home {...props} />);
-    componentWrapper.instance().getCookiesToArray = () => [{name: ' iSession', value: 'token'}];
+    jest.spyOn(pageBuilder, 'getPage').mockReturnValueOnce(null);
     // when
-    componentWrapper.instance().saveTokenFromCookies();
+    const wrappedComponent = mount(<_Home {...props} />);
     // then
-    expect(sessionHelperSpy).toHaveBeenCalledWith('token');
+    expect(homeHelper.saveLoginValues).toBeCalled();
+    expect(homeHelper.saveTokenFromCookies).toBeCalled();
   });
 
-  it('should save tokens from cookies after update', () => {
+  it('should trigger saveTokenFromCookies on update', () => {
     // given
     const props = {
       loading: false,
       authToken: false,
       reportArray: false,
     };
-    const componentWrapper = shallow(<_Home {...props} />);
-    const saveTokenSpy = jest.spyOn(componentWrapper.instance(), 'saveTokenFromCookies');
-    saveTokenSpy.mockClear();
+    jest.spyOn(pageBuilder, 'getPage').mockReturnValue(null);
+    const wrappedComponent = mount(<_Home {...props} />);
     // when
-    componentWrapper.setState({});
+    wrappedComponent.setProps({
+      ...props,
+      authToken: 'new',
+    });
     // then
-    expect(saveTokenSpy).toHaveBeenCalled();
+    expect(homeHelper.saveTokenFromCookies).toBeCalledTimes(3);
   });
 });
