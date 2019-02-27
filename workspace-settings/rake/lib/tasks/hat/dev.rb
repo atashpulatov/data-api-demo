757c9dac1d6848013f60b078e0c4793b6674ad1b
yaou <yaou@microstrategy.com>
2019-02-27 14:35:31 -0500

    rm node modules first

--- workspace-settings/rake/lib/tasks/hat/dev.rb
+++ workspace-settings/rake/lib/tasks/hat/dev.rb
@@ -23,6 +23,7 @@ end
 
 
 def install_dependencies()
+  shell_command! "rm -rf node_modules", cwd: "#{$WORKSPACE_SETTINGS[:paths][:project][:production][:home]}"
   update_package_json()
   shell_command! "yarn install", cwd: "#{$WORKSPACE_SETTINGS[:paths][:project][:production][:home]}"
 end
