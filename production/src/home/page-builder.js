ceba58861959f0fcdfb8aee6afed8bcae8c6dfd0
Koziel, Bartosz <bkoziel@microstrategy.com>
2019-03-07 11:00:08 +0100

    changes in "imported data" tab display

--- production/src/home/page-builder.js
+++ production/src/home/page-builder.js
@@ -13,20 +13,22 @@ class PageBuilder {
     return (
       <div id='content'>
         <Notifications />
-        {(authToken && <Header />)}
-        {/* Logout button will be moved next to the username in the header */}
-        <Tabs defaultActiveKey="data" className="tabs-container">
-          {/* <TabPane tab="Environment" key="environment">
-            <div>TODO</div>
-          </TabPane> */}
-          <TabPane tab="Imported Data" key="data">
-            {(reportArray && reportArray.length !== 0) && authToken && <FileHistoryContainer />}
-            {(!reportArray || !reportArray.length) && authToken && <Placeholder />}
-            <Spin spinning={loading}>
-              {!authToken && <Authenticate />}
-            </Spin>
-          </TabPane>
-        </Tabs>
+        {authToken && <Header />}
+
+        {authToken ?
+          <Tabs defaultActiveKey="data" className="tabs-container">
+            {/* <TabPane tab="Environment" key="environment">
+              <div>TODO</div>
+              </TabPane> */}
+            <TabPane tab="Imported Data" key="data">
+              {(reportArray && reportArray.length !== 0) && <FileHistoryContainer />}
+              {(!reportArray || !reportArray.length) && <Placeholder />}
+
+            </TabPane>
+          </Tabs> :
+          <Spin spinning={loading}>
+            <Authenticate />
+          </Spin>}
       </div>
     );
   }
