0714151710f4eb9e76622ac305a37130783f2e94
asedano <asedano@microstrategy.com>
2019-03-07 16:51:36 +0100

    Basic loading page component

--- production/src/loading/loading-page.jsx
+++ production/src/loading/loading-page.jsx
@@ -1,7 +1,21 @@
 import React, {Component} from 'react';
+import {LoadingText} from 'mstr-react-library';
 
-export class LoadingPage extends Component {
-  render() {
-    return <div></div>;
-  }
+const dialogStyle = {
+  position: 'fixed',
+  top: '50%',
+  transform: 'translateY(-50%)',
+};
+
+const titleStyle = {
+
+};
+
+export const LoadingPage = ({title}) => {
+  return (
+    <dialog className='loading-page' style={dialogStyle}>
+      <h1 style={titleStyle}>{title}</h1>
+      <LoadingText text='Loading data.' />
+    </dialog>
+  );
 };
