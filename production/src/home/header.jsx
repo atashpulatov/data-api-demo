81ccf21bf23816ffa1927888d32f6beab514cfaa
Koziel, Bartosz <bkoziel@microstrategy.com>
2019-03-11 09:15:22 +0100

    Changes to userData display

--- production/src/home/header.jsx
+++ production/src/home/header.jsx
@@ -8,23 +8,29 @@ import {userRestService} from './user-rest-service';
 
 
 export class _Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {class: ''};
+  }
+
   componentDidMount = async () => {
     const {envUrl, authToken} = this.props;
     const userData = await userRestService.getUserData(authToken, envUrl);
     sessionHelper.saveUserInfo(userData);
+    this.setState({class: 'gotUserData'});
   }
 
   render() {
     const {userFullName, userInitials} = this.props;
     return (
       <header id='app-header'>
-        <span id='profileImage'>
+        <span id='profileImage' className={this.state.class}>
           {userInitials !== null ?
             <span id='initials' alt='User profile'>{userInitials}</span> :
             <img src={logo} alt='User profile' />
             /* TODO: When rest api returns profileImage use it as source*/}
         </span>
-        <span className='header-name'>{userFullName}</span>
+        <span className={` ${this.state.class} header-name`}>{userFullName}</span>
         <Button id='logOut' onClick={logout} size='small'>Log out</Button>
       </header >
     );
