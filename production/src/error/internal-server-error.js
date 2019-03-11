369adfa1d77a28a44e618c20de9687b717e942c8
Slazyk <jslazyk@microstrategy.com>
2019-03-11 13:06:39 +0100

    Add default unsupported iServerCode if errorBody is missing

--- production/src/error/internal-server-error.js
+++ production/src/error/internal-server-error.js
@@ -1,6 +1,6 @@
 export class InternalServerError extends Error {
   constructor(errorBody, ...params) {
     super(...params);
-    this.iServerCode = errorBody.iServerCode;
+    this.iServerCode = errorBody && errorBody.iServerCode ? errorBody.iServerCode : '-2147171501';
   }
 };
