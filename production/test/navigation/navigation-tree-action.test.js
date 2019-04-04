44f1e9c7f44c5229ffc8e0acc1f56ba90abea9a8
Oleksandr Lisovyk <olisovyk@microstrategy.com>
2019-04-04 13:08:37 +0200

    DE132128;Fix unit tests

--- production/test/navigation/navigation-tree-action.test.js
+++ production/test/navigation/navigation-tree-action.test.js
@@ -55,4 +55,44 @@ describe('NavigationTree Actions', () => {
     // then
     expect(listener).toHaveBeenCalledWith({type: officeProperties.actions.startLoading});
   });
+  it('should dispatch proper changeSorting action', () => {
+    // given
+    const listener = jest.fn();
+
+    // when
+    actions.changeSorting(true)(listener);
+
+    // then
+    expect(listener).toHaveBeenCalledWith({type: actions.CHANGE_SORTING, data: true});
+  });
+  it('should dispatch proper changeSearching action', () => {
+    // given
+    const listener = jest.fn();
+
+    // when
+    actions.changeSearching(true)(listener);
+
+    // then
+    expect(listener).toHaveBeenCalledWith({type: actions.CHANGE_SEARCHING, data: true});
+  });
+  it('should dispatch proper updateScroll action', () => {
+    // given
+    const listener = jest.fn();
+
+    // when
+    actions.updateScroll(true)(listener);
+
+    // then
+    expect(listener).toHaveBeenCalledWith({type: actions.UPDATE_SCROLL, data: true});
+  });
+  it('should dispatch proper updateSize action', () => {
+    // given
+    const listener = jest.fn();
+
+    // when
+    actions.updateSize(true)(listener);
+
+    // then
+    expect(listener).toHaveBeenCalledWith({type: actions.UPDATE_SIZE, data: true});
+  });
 });
