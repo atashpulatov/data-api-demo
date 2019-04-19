718368fa6f68d66995e2e2cc03f3a9d7466d72a9
Oleksandr Lisovyk <olisovyk@microstrategy.com>
2019-04-19 17:19:00 +0200

    US167262;Fix some unit-tests

--- production/test/popup/popup-view-selector.test.js
+++ production/test/popup/popup-view-selector.test.js
@@ -4,7 +4,6 @@ import {Office} from '../mockOffice';
 import {selectorProperties} from '../../src/attribute-selector/selector-properties';
 import {_PopupViewSelector} from '../../src/popup/popup-view-selector';
 import {PromptsWindow} from '../../src/prompts/prompts-window';
-import {PopupTypeEnum} from "../../src/home/popup-type-enum";
 
 describe('PopupViewSelector', () => {
   it('should handle request import when not prompted', () => {
