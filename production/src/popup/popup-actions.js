bd76ab68700bcaf1150ae18b407b73c8295b2484
Filip Bugała <fbugala@microstrategy.com>
2019-05-02 12:42:01 +0200

    Rest-factory-changed-to-office-factory

--- production/src/popup/popup-actions.js
+++ production/src/popup/popup-actions.js
@@ -92,7 +92,7 @@ export function refreshReport(bindingId, objectType, isRefreshAll = false, index
       });
       if (isRefreshAll) {
         const fromStorage = JSON.parse(localStorage.getItem('results'));
-        const restError = errorService.errorRestFactory(error);
+        const restError = errorService.errorOfficeFactory(error);
         const errorMessage = errorService.getErrorMessage(restError);
         fromStorage[index].result = errorMessage;
         fromStorage[index].isError = true;
