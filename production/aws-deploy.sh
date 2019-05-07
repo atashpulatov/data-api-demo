a77f5b8aa3c9940fe39878cce6b2c69d46ed17ff
asedano <asedano@microstrategy.com>
2019-05-07 13:45:44 +0200

    Improve aws deployment script

--- production/aws-deploy.sh
+++ production/aws-deploy.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
-read -n 1 -p "Do you want to build the add-in? (y/n)" shouldBuild
+echo "Do you want to build the add-in? (y/n)"
+read -n 1 shouldBuild
 if [ $shouldBuild == "y" ]; then 
     yarn build
 else
     echo -e "Build skipped\n" 
 fi
-read -p "Enter the Intelligence Server Address IP: " iserver
+echo "Enter the Intelligence Server Address IP: "
+read iserver
 scp -r build/* mstr@$iserver:/opt/apache/tomcat/apache-tomcat-9.0.12/webapps/MicroStrategyLibrary/apps/addin-mstr-office
