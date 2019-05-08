c97c7cf5a39cc04fd5a9e418174abf8b7dad1450
Filip Bugała <fbugala@microstrategy.com>
2019-05-08 12:11:20 +0200

    New error message for prompted reports

--- production/src/error/constants.js
+++ production/src/error/constants.js
@@ -6,7 +6,8 @@ const withDefaultValue = (obj, defaultValue) => {
 
 export const GENERIC_SERVER_ERR = 'This object cannot be imported.';
 export const NOT_SUPPORTED_NO_ATTRIBUTES = 'This object cannot be imported. Objects without attributes are not supported in this version of MicroStrategy for Office.';
-export const NOT_SUPPORTED_SERVER_ERR = 'This object cannot be imported. Objects with prompts, cross tabs, totals, or subtotals are not supported in this version of MicroStrategy for Office.';
+export const NOT_SUPPORTED_SERVER_ERR = 'This object cannot be imported. Objects with cross tabs, totals, or subtotals are not supported in this version of MicroStrategy for Office.';
+export const NOT_SUPPORTED_PROMPTS_REFRESH = 'Objects with prompts cannot be refreshed in this version of MicroStrategy for Office.';
 export const NOT_PUBLISHED_CUBE = 'This object cannot be imported. The Intelligent Cube is not published.';
 export const NOT_IN_METADATA = 'The object does not exist in the metadata.';
 export const PROJECT_ROW_LIMIT = 'The object exceeds project rows limitation';
