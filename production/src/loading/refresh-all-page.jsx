bb0bb6d6228c04677252815156987d192c6a485d
Filip Bugała <fbugala@microstrategy.com>
2019-05-20 12:42:59 +0200

    Refresh all popup tests

--- production/src/loading/refresh-all-page.jsx
+++ production/src/loading/refresh-all-page.jsx
@@ -57,13 +57,14 @@ export class RefreshAllPage extends Component {
     clearInterval(this.intervalId);
   }
 
-  finished = () => {
-    localStorage.removeItem('refreshData');
-    const okObject = {
-      command: selectorProperties.commandOk,
-    };
-    Office.context.ui.messageParent(JSON.stringify(okObject));
-  }
+  // TODO: This will be used when button Ok will be added
+  // finished = () => {
+  //   localStorage.removeItem('refreshData');
+  //   const okObject = {
+  //     command: selectorProperties.commandOk,
+  //   };
+  //   Office.context.ui.messageParent(JSON.stringify(okObject));
+  // }
 
   getIcon = (res) => {
     if (res.isError === false) {
@@ -124,11 +125,3 @@ export class RefreshAllPage extends Component {
     </dialog>);
   }
 };
-
-// const mapStateToProps = ({popupReducer}) => {
-//   return {
-//     name: popupReducer.refreshingReport,
-//   };
-// };
-
-// export const RefreshAllPage = connect(mapStateToProps)(_RefreshAllPage);
