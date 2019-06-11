18b8f089f3bb63ae15b5c1a4a0c4fad2eacf68d1
Oleksandr Lisovyk <olisovyk@microstrategy.com>
2019-06-11 17:26:44 +0200

    DE138298; Fix notifications area and small improvement in French

--- production/src/notification/notifications.jsx
+++ production/src/notification/notifications.jsx
@@ -58,17 +58,19 @@ export class NotificationsWithoutRedux extends Component {
     }
     notification.open({
       message: t(title),
-      description: t(content),
+      description: this.translateContent(content, t),
       icon,
       btn: btn,
       key,
     });
   };
 
+  translateContent = (content, t) => content.includes('Excel returned error') ? `${t('Excel returned error')}: ${content.split(':')[1]}` : t(content);
+
   displayMessage = () => {
     const {messageType, content, t} = this.props;
-    message[messageType](content.includes('Excel returned error') ? `${t('Excel returned error')}: ${content.split(':')[1]}` : t(content));
-  }
+    message[messageType](this.translateContent(content, t));
+  };
 
   render() {
     return (
