1d651e9fcad5743f5bbb65da492184549cf99124
asedano <asedano@microstrategy.com>
2019-06-13 14:56:54 +0200

    US168577; Normalized JSON handler

---
+++ production/src/mstr-object/mstr-normalized-json-handler.js
@@ -0,0 +1,105 @@
+/**
+ * Helper class to manipulate the new normalized REST API V2
+ *
+ * @export
+ * @class NormalizedJsonHandler
+ */
+
+class NormalizedJsonHandler {
+  /**
+   * Gets the element name based on its index, returns object with an additional value key.
+   * Generate unified value property for single-form attribute element, multi-form
+   * attribute element and metric-object-as-element.
+   *
+   * @param {Object} definition - Dataset definition
+   * @param {string} axis - 'rows' or 'columns'
+   * @param {number} attributeIndex - Array index that corresponds to an attribute
+   * @param {number} elementIndex - Array index that corresponds to an attribue element
+   *
+   * @memberof JSONHandler
+   * @return {Object}
+   */
+  lookupElement = (definition, axis, attributeIndex, elementIndex) => {
+    const rawElement = definition.grid[axis][attributeIndex].elements[elementIndex];
+    const {name, formValues} = rawElement;
+    return {...rawElement, value: formValues || [name]};
+  };
+
+  /**
+   * Get an array with element names
+   *
+   * @param {Object} definition - Dataset definition
+   * @param {string} axis - 'rows' or 'columns'
+   * @param {number} elementIndices - Array index that corresponds to an attribue element
+   * @memberof NormalizedJsonHandler
+   * @return {array}
+   */
+  mapElementIndicesToElements = (definition, axis, elementIndices) => {
+    return elementIndices.map((elementIndex, attributeIndex) =>
+      // For elementsIndices tuple, each subscript is an attribute index and each value is an element index.
+      this.lookupElement(definition, axis, attributeIndex, elementIndex));
+  };
+
+  /**
+   *
+   * Creates a 2D Array with row attribute headers and metric values
+   *
+   * @param {Object} definition - Dataset definition
+   * @param {Object} data - Response data object
+   * @param {function} onElement - Callback function to process elements
+   * @param {function} onMetricValue - Callback function to process metric values
+   *
+   * @memberof NormalizedJsonHandler
+   * @return {Array}
+   */
+  renderTabular = (definition, data, onElement, onMetricValue) => {
+    // For each row in header zone.
+    const {headers, metricValues} = data;
+
+    return headers.rows.map((headerCells, rowIndex) => {
+      const rowElements = this.mapElementIndicesToElements(definition, 'rows', headerCells);
+      return (
+        // Process elements
+        rowElements.map((e, attributeIndex) => onElement(e, rowIndex, attributeIndex))
+      ).concat(
+          // Process metric values of the same row
+          metricValues[rowIndex].map((mv, mvZoneColumnIndex) => onMetricValue(mv, rowIndex, mvZoneColumnIndex))
+      );
+    });
+  };
+
+  /**
+   * For keep-only/exclude on an attribute cell
+   *
+   * @param {Object} definition - Dataset definition
+   * @param {string} axis - 'rows' or 'columns'
+   * @param {number} attributeIndex - Array index that corresponds to an attribute
+   * @param {number} headerIndex - Array index that corresponds to the header
+   *
+   * @memberof NormalizedJsonHandler
+   * @return {Array}
+   */
+  getElementIdForGivenHeaderCell = (definition, axis, attributeIndex, headerIndex) => {
+    // axis is either "rows" or "columns"
+    const attribute = definition.grid[axis][attributeIndex];
+    const element = attribute.elements[headerIndex];
+    return element.id;
+  }
+
+  /**
+   * For keep-only/exclude on a metric value cell
+   * @param {array} headers - Array with row and column header index
+   * @param {number} mvZoneRowIndex - Metric value row index
+   * @param {number} mvZoneColumnIndex - Metric value column index
+   *
+   * @memberof NormalizedJsonHandler
+   * @return {Array}
+   */
+  getElementIdListForGivenMetricCell = (headers, mvZoneRowIndex, mvZoneColumnIndex) => {
+    const rowHeader = headers.rows[mvZoneRowIndex];
+    const columnHeader = headers.columns[mvZoneColumnIndex];
+    return (rowHeader.concat(columnHeader)).map((element) => element.id);
+  }
+}
+
+export default new NormalizedJsonHandler();
---
+++ production/test/mstr-object/mstr-normalized-json-handler.test.js
@@ -0,0 +1,119 @@
+import jsonHandler from '../../src/mstr-object/mstr-normalized-json-handler';
+import response from '../../src/mstr-object/rest-api-v2.json';
+
+
+describe('Normalized JSON Handler', () => {
+  it('should lookup for column elements by index', () => {
+    // given
+    const {definition} = response;
+    const axis = 'columns';
+    const attributeIndex = 0;
+    const elementIndex = 1;
+    const expectedElement = {
+      'id': 'h2;1D5F4A7811E97722F1050080EF65506C',
+      'formValues': ['DCA', '2'],
+      'value': ['DCA', '2'],
+    };
+    // when
+    const element = jsonHandler.lookupElement(definition, axis, attributeIndex, elementIndex);
+    // then
+    expect(element).toEqual(expectedElement);
+  });
+  it('should lookup for row elemens by index', () => {
+    // given
+    const {definition} = response;
+    const axis = 'rows';
+    const attributeIndex = 0;
+    const elementIndex = 1;
+    const expectedElement = {'id': 'h2010;9BC4691C11E97721AF570080EF55306C', 'formValues': ['2010'], 'value': ['2010']};
+    // when
+    const element = jsonHandler.lookupElement(definition, axis, attributeIndex, elementIndex);
+    // then
+    expect(element).toEqual(expectedElement);
+  });
+  it('should map indices to elements', () => {
+    // given
+    const {definition} = response;
+    const axis = 'columns';
+    const elementIndices = [0];
+    const expectedElements = [{'formValues': ['BWI', '1'], 'id': 'h1;1D5F4A7811E97722F1050080EF65506C', 'value': ['BWI', '1']}];
+    // when
+    const elements = jsonHandler.mapElementIndicesToElements(definition, axis, elementIndices);
+    // then
+    expect(elements).toEqual(expectedElements);
+  });
+  it('should map indices to elements', () => {
+    // given
+    const {definition} = response;
+    const axis = 'columns';
+    const elementIndices = [0];
+    const expectedElements = [{'formValues': ['BWI', '1'], 'id': 'h1;1D5F4A7811E97722F1050080EF65506C', 'value': ['BWI', '1']}];
+    // when
+    const elements = jsonHandler.mapElementIndicesToElements(definition, axis, elementIndices);
+    // then
+    expect(elements).toEqual(expectedElements);
+  });
+  it('should render tabular data', () => {
+    // given
+    const {definition, data} = response;
+    const onElement = (element) => element.value[0];
+    const onMetricValue = (metric) => metric.rv;
+    const expectedFirstRow = ['2009', 'January', 3139, 17046.02, 4543, 2406, 20915.41, 3449];
+    // when
+    const tabular = jsonHandler.renderTabular(definition, data, onElement, onMetricValue);
+    // then
+    expect(tabular[0]).toEqual(expectedFirstRow);
+  });
+  it('should get element id for a given cell', () => {
+    // given
+    const {definition} = response;
+    const axis = 'columns';
+    const attributeIndex = 0;
+    const headerIndex = 1;
+    const expectedId = 'h2;1D5F4A7811E97722F1050080EF65506C';
+    // when
+    const tabular = jsonHandler.getElementIdForGivenHeaderCell(definition, axis, attributeIndex, headerIndex);
+    // then
+    expect(tabular).toEqual(expectedId);
+  });
+
+  it('should render column headers', () => {
+    // given
+    const {definition, data} = response;
+    const axis = 'columns';
+    const {headers} = data;
+    const expectedHeaders = [
+      ['BWI', 'Flight Delayed'],
+      ['BWI', 'Avg Delay (min)'],
+      ['BWI', 'On-Time'],
+      ['DCA', 'Flight Delayed'],
+      ['DCA', 'Avg Delay (min)'],
+      ['DCA', 'On-Time']];
+    // when
+    const colHeaders = jsonHandler.renderHeaders(definition, axis, headers);
+    // then
+    expect(colHeaders).toEqual(expectedHeaders);
+  });
+
+  it('should render row headers', () => {
+    // given
+    const {definition, data} = response;
+    const axis = 'rows';
+    const {headers} = data;
+    const expectedHeaders = [
+      ['2009', 'January'],
+      ['2009', 'February'],
+      ['2009', 'March'],
+      ['2009', 'Total'],
+      ['2010', 'January'],
+      ['2010', 'February'],
+      ['2010', 'March'],
+      ['2010', 'Total'],
+    ];
+    // when
+    const colHeaders = jsonHandler.renderHeaders(definition, axis, headers);
+    // then
+    expect(colHeaders).toEqual(expectedHeaders);
+  });
+});
+
