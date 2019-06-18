494e80e06ca11c7136a7ce115f58573e5ba22798
asedano <asedano@microstrategy.com>
2019-06-18 11:08:13 +0200

    DE138989; Add new auth logic outside of iframe

--- office-loader/src/index.js
+++ office-loader/src/index.js
@@ -1,20 +1,101 @@
 const Office = window.Office;
+const OFFICE_PRIVILEGE_ID = '273';
+const libraryUrl = getLibraryUrl();
+let popup = null;
 
 function officeInitialize() {
   Office.onReady()
     .then(() => {
-      translate();
-      if (canSaveCookies()) {
-        const currentPath = window.location.pathname;
-        const pathBeginning = currentPath.split('/static/')[0];
-        const loginParams = 'source=addin-mstr-office';
-        window.location.replace(`${pathBeginning}/apps/addin-mstr-office/index.html?${loginParams}`);
+      if (!canSaveCookies()) {
+        showCookieWarning();
       } else {
-        document.getElementById('cookies-not-needed').style.display = 'none';
-        document.getElementById('cookies-needed').style.display = 'block';
-        document.getElementById('accept-cookies-btn').addEventListener('click', acceptCookies);
+        startAuthentication();
       }
     });
+}
+
+function startAuthentication() {
+  verifyToken(libraryUrl).then(valid => {
+    if (valid) {
+      goToReact(libraryUrl);
+    }
+    else {
+      showLoginBtn();
+    }
+  }).catch((e) => {
+    console.log(e);
+    showLoginBtn();
+  });
+}
+
+function onLoginClick() {
+  verifyToken(libraryUrl)
+    .then((valid) => valid ? goToReact(libraryUrl) : openAuthDialog(libraryUrl))
+    .catch((e) => console.log(e) || openAuthDialog(libraryUrl))
+}
+
+function goToReact(url) {
+  const loginParams = 'source=addin-mstr-office';
+  window.location.replace(`${url}/apps/addin-mstr-office/index.html?${loginParams}`);
+}
+
+function getLibraryUrl() {
+  const currentPath = window.location.href;
+  return currentPath.split('/static/')[0];
+
+}
+
+function verifyToken(libraryUrl) {
+  const url = libraryUrl + '/api/sessions/privileges/' + OFFICE_PRIVILEGE_ID;
+  const token = getCookie(window);
+  const headers = {
+    'X-MSTR-AuthToken': token
+  };
+  return fetch(url, {credentials: 'include', headers}).then((res) => res.ok);
+}
+
+function openAuthDialog(url) {
+  const popupUrl = `${url}/apps/addin-mstr-office/index.html?source=addin-mstr-office`;
+  openPopup(popupUrl);
+  // Clear old session cookie
+  const listenAuthToken = () => {
+    verifyToken(url).then(valid => {
+      if (valid) {
+        popup.close();
+        goToReact(url);
+      } else {
+        !popup.closed && setTimeout(listenAuthToken, 2000)
+      }
+    }).catch((e) => {
+      console.log(e);
+      popup.close();
+    });
+  }
+  listenAuthToken();
+}
+
+function openPopup(url) {
+  if (popup === null || popup.closed) {
+    popup = window.open(url, 'MicroStrategy_for_Office', 'resizable=1,status=1,height=600,width=400,location=0,chrome=1,centerscreen=1,dependent=1,minimizable=0,alwaysRaised=1,alwaysOnTop=1');
+  } else {
+    popup.focus();
+  };
+}
+
+function getCookie(win) {
+  return win.document.cookie.replace(/(?:(?:^|.*;\s*)iSession\s*=\s*([^;]*).*$)|^.*$/, '$1');
+}
+
+function showCookieWarning() {
+  document.getElementById('cookies-not-needed').style.display = 'none';
+  document.getElementById('cookies-needed').style.display = 'block';
+  document.getElementById('accept-cookies-btn').addEventListener('click', acceptCookies);
+}
+
+function showLoginBtn() {
+  document.getElementById('cookies-not-needed').style.display = 'none';
+  document.getElementById('login-container').style.display = 'block';
+  document.getElementById('login-btn').addEventListener('click', onLoginClick);
 }
 
 function canSaveCookies() {
@@ -24,68 +105,6 @@ function canSaveCookies() {
     return document.cookie.indexOf(TEMP_COOKIE) !== -1;
   } catch {
     return false;
-  }
-}
-
-function translate() {
-  const cookiesNotNeededTranslations = {
-    'en-US': 'Loading...',
-    'de-DE': 'Ladevorgang läuft...',
-    'zh-CN': '正在加载...',
-    'es-ES': 'Cargando...',
-    'fr-FR': 'Chargement...',
-    'it-IT': 'Caricamento in corso...',
-    'zh-TW': '載入中...',
-    'ko-KR': '로딩 중...',
-    'pt-BR': 'Carregando...',
-    'nl-NL': 'Laden...',
-    'sv-SE': 'Läser in ...',
-    'ja-JP': 'ロード中...',
-    'da-DK': 'Indlæser...'
-  }
-  const noCookiesTranslations = {
-    'en-US': 'Please enable cookies to start using the add-in',
-    'de-DE': 'Aktivieren Sie Cookies, um das Add-in zu verwenden',
-    'zh-CN': '请启用 Cookies 以使用加载项',
-    'es-ES': 'Habilite las cookies para empezar a usar el complemento',
-    'fr-FR': 'Veuillez activer les cookies pour pouvoir utiliser le complément',
-    'it-IT': 'Per cominciare a utilizzare il componente aggiuntivo è necessario abilitare i cookie',
-    'zh-TW': '請啟用 Cookie 以開始使用增益集',
-    'ko-KR': '추가 기능을 사용하려면 쿠키를 활성화하십시오',
-    'pt-BR': 'Ative os cookies para começar a usar o suplemento',
-    'nl-NL': 'Schakel cookies in om de invoegtoepassing te gebruiken',
-    'sv-SE': 'Aktivera cookies för att börja använda tillägget',
-    'ja-JP': 'アドインを使用するには cookie を有効にする必要があります',
-    'da-DK': 'Aktivér cookies for at starte denne add-in'
-  }
-  const enableButtonTranslations = {
-    'en-US': 'Enable',
-    'de-DE': 'Aktivieren',
-    'zh-CN': '启用',
-    'es-ES': 'Habilitar',
-    'fr-FR': 'Activer',
-    'it-IT': 'Abilita',
-    'zh-TW': '啟用',
-    'ko-KR': '활성화',
-    'pt-BR': 'Permitir',
-    'nl-NL': 'Inschakelen',
-    'sv-SE': 'Aktivera',
-    'ja-JP': '有効化',
-    'da-DK': 'Aktiver'
-  }
-  try {
-    const locale = Office.context.displayLanguage || navigator.language;
-
-    if (!!cookiesNotNeededTranslations[locale]) {
-      const cookiedNotNeededHeader = document.getElementById('cookies-not-needed');
-      const cookiedAreNeededHeader = document.getElementById('cookies-are-needed-header');
-      const cookiedAcceptButton = document.getElementById('accept-cookies-btn');
-      cookiedNotNeededHeader.innerText = cookiesNotNeededTranslations[locale];
-      cookiedAreNeededHeader.innerText = noCookiesTranslations[locale];
-      cookiedAcceptButton.innerText = enableButtonTranslations[locale];
-    };
-  } catch (e) {
-    console.log(e)
   }
 }
 
