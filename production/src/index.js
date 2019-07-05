71c8de9756e1a4370a75b03d6f4e4d98b86c14e6
asedano <asedano@microstrategy.com>
2019-07-05 11:55:04 +0200

    Remove workaround for disabling back navigation

--- production/src/index.js
+++ production/src/index.js
@@ -52,22 +52,6 @@ function goReact() {
   );
 }
 
-/**
- * This function adds a # value to iframe URL and modifies it n times
- * This should prevent navigating back to login page via browser 'Back' button
- * @param {number} count Amount of attempts
- */
-function disableBackNavigation(count) {
-  if (count) {
-    window.setTimeout(function() {
-      window.location.hash = count;
-      disableBackNavigation(count - 1);
-    }, 50);
-  }
-}
-
-// goReact();
-disableBackNavigation(10);
 officeInitialize();
 
 // If you want your app to work offline and load faster, you can change
