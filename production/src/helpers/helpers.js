648715ffa82ecaf4002d27dfef1cf8b16f5b47a3
Filip Bugała <fbugala@microstrategy.com>
2019-07-16 10:50:47 +0200

    Parameter name changed

--- production/src/helpers/helpers.js
+++ production/src/helpers/helpers.js
@@ -1,7 +1,7 @@
 class Helper {
-  isOverflown = (userName, containerWidth) => {
+  isOverflown = (text, containerWidth) => {
     const measureReportName = document.createElement('SPAN');
-    measureReportName.innerHTML = userName;
+    measureReportName.innerHTML = text;
     measureReportName.setAttribute('id', 'measure-text');
     document.body.appendChild(measureReportName);
     // we compare report name length with all popup width - 90px of paddings and icon container
