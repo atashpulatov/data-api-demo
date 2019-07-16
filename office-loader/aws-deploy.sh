073ef7930a5a075a7995cce893373bfc45f6f161
asedano <asedano@microstrategy.com>
2019-07-16 15:03:56 +0200

    Update aws deployment scripts

--- office-loader/aws-deploy.sh
+++ office-loader/aws-deploy.sh
@@ -1,11 +1,19 @@
 #! /bin/bash
-echo "Do you want to build the add-in? (y/n)"
-read -n 1 shouldBuild
-if [ $shouldBuild == "y" ]; then 
+if [ "$1" == "" ]; then
+  echo "Do you want to build the add-in? (y/n)"
+  read -n 1 shouldBuild
+else
+  shouldBuild="$1"
+fi
+if [ "$shouldBuild" == "y" ]; then 
     yarn build
 else
     echo -e "Build skipped\n" 
 fi
-echo "Enter the Intelligence Server Address IP: "
-read iserver
+if [ "$2" == "" ]; then
+  echo "Enter the Intelligence Server Address IP: "
+  read iserver
+else
+  iserver="$2"
+fi
 scp -r build/* mstr@$iserver:/opt/apache/tomcat/apache-tomcat-9.0.12/webapps/MicroStrategyLibrary/static/loader-mstr-office
