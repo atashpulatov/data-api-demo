f217deb67b4b1e2495998ffce0e61a9c7a104e5e
kackowalski <kackowalski@microstrategy.com>
2019-07-17 10:31:06 +0200

    DE141428; added semantic elements

--- production/src/home/tabs.js
+++ production/src/home/tabs.js
@@ -4,16 +4,16 @@ export const Tabs = ({t, children}) => {
   return (
     <div className="tabs">
       <div className="tab-container">
-        <div className="tabs-nav">
+        <nav className="tabs-nav">
           <div className="tab-header">{t('Imported Data')}</div>
           <div className="tab-line"></div>
-        </div>
+        </nav>
       </div>
-      <div className="tab-content">
+      <section className="tab-content">
         <div className="tabpane">
           {children}
         </div>
-      </div>
+      </section>
     </div>
   );
 };
