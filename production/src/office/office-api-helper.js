9a907bae22b11367981b3bf1885f459ec200f1bd
Filip Bugała <fbugala@microstrategy.com>
2019-08-09 13:52:49 +0200

    DE144768; Preserve cross tab reports when clearing

--- production/src/office/office-api-helper.js
+++ production/src/office/office-api-helper.js
@@ -255,9 +255,12 @@ class OfficeApiHelper {
 
   deleteObjectTableBody = async (context, object) => {
     try {
+      context.runtime.enableEvents = false;
+      await context.sync();
       const tableObject = context.workbook.tables.getItem(object.bindId);
       const tableRange = tableObject.getDataBodyRange();
       tableRange.clear(Excel.ClearApplyTo.contents);
+      context.runtime.enableEvents = true;
       await context.sync();
     } catch (error) {
       console.error('Error: ' + error);
