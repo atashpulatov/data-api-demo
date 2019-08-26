d7013337e2c2fd0cbc47d237531e6b568038294b
eozturk <eozturk@microstrategy.com>
2019-08-26 13:48:50 +0200

    US201891; Settings Component created

---
+++ production/src/settings/settings-component.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {withTranslation} from 'react-i18next';
+
+export class _SettingComponent extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+
+}
