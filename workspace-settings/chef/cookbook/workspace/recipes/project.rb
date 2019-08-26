f79367c3f581a519d88beb68b5faae9e3acc0f56
asedano <asedano@microstrategy.com>
2019-08-26 14:19:55 +0200

    Add avahi-compat-libdns_sd-devel

--- workspace-settings/chef/cookbook/workspace/recipes/project.rb
+++ workspace-settings/chef/cookbook/workspace/recipes/project.rb
@@ -32,7 +32,7 @@ when 'redhat', 'centos', 'fedora', 'amazon'
   bash 'install linux dependencies' do
     user 'root'
     code <<-EOH
-      sudo yum install -y avahi-compat-libdns_sd* make gcc gcc-c++ kernel-devel
+      sudo yum install -y avahi-compat-libdns_sd-devel make gcc gcc-c++ kernel-devel
     EOH
     only_if "echo $USER | grep jenkins"
 end
