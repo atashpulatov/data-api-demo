f471bcf54475a537ac5b76e922a5464385f023f9
eozturk <eozturk@microstrategy.com>
2019-08-26 13:51:13 +0200

    US201891; settings-component render method

--- production/src/settings/settings-component.jsx
+++ production/src/settings/settings-component.jsx
@@ -6,7 +6,11 @@ export class _SettingComponent extends Component {
   constructor(props) {
     super(props);
   }
-
-
-
+  render() {
+    const {t} = this.props;
+    return (<div>
+      {t('SETTINGS WILL BE HERE')}
+      <button >{t('BACK')}</button>
+    </div>);
+  }
 }
