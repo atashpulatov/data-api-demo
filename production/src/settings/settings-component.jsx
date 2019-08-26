63aee4653775f22533b7115790f6db6f9143fa1e
eozturk <eozturk@microstrategy.com>
2019-08-26 14:32:17 +0200

    US201891; Implementation of the back action

--- production/src/settings/settings-component.jsx
+++ production/src/settings/settings-component.jsx
@@ -3,18 +3,37 @@ import {connect} from 'react-redux';
 import {withTranslation} from 'react-i18next';
 import {toggleRenderSettingsFlag} from '../office/office-actions';
 
-export class SettingsComponent extends Component {
+export class _SettingsComponent extends Component {
   constructor(props) {
     super(props);
   }
 
+  closeSettingsComponent = () => {
+    this.props.toggleRenderSettingsFlag(!this.props.shouldRenderSettings);
+  }
+
   render() {
+    const {t} = this.props;
     return (
       <div>
-        SETTINGS WILL BE HERE
+        {t('SETTINGS WILL BE HERE')}
+        <button onClick={this.closeSettingsComponent}>{t('BACK')}</button>
       </div>
     );
   }
 }
 
+_SettingsComponent.defaultProps = {
+  t: (text) => text,
+};
 
+function mapStateToProps({officeReducer}) {
+  const {shouldRenderSettings} = officeReducer;
+  return {shouldRenderSettings};
+}
+
+const mapDispatchToProps = {
+  toggleRenderSettingsFlag,
+};
+
+export const SettingsComponent = connect(mapStateToProps, mapDispatchToProps)(withTranslation('common')(_SettingsComponent));
