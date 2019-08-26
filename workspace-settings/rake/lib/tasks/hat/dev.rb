c6df68d8b0585395aa6b3209b75ce01c82c06fe6
asedano <asedano@microstrategy.com>
2019-08-26 16:11:40 +0200

    Move i18next backend to dev

--- workspace-settings/rake/lib/tasks/hat/dev.rb
+++ workspace-settings/rake/lib/tasks/hat/dev.rb
@@ -13,9 +13,9 @@ task :build do
   shell_command! "npm run build", cwd: "#{$WORKSPACE_SETTINGS[:paths][:project][:production][:home]}"
   shell_command! "zip -r office-#{Common::Version.application_version}.zip .", cwd: "#{$WORKSPACE_SETTINGS[:paths][:project][:production][:home]}/build"
 
-  # build the office_loader.zip
-  shell_command! "yarn run build", cwd: "#{$WORKSPACE_SETTINGS[:paths][:project][:home]}/office-loader/build"
-  shell_command! "zip -r office-loader-#{Common::Version.application_version}.zip .", cwd: "#{$WORKSPACE_SETTINGS[:paths][:project][:home]}/office-loader/build"
+  # # build the office_loader.zip
+  # shell_command! "yarn run build", cwd: "#{$WORKSPACE_SETTINGS[:paths][:project][:home]}/office-loader/build"
+  # shell_command! "zip -r office-loader-#{Common::Version.application_version}.zip .", cwd: "#{$WORKSPACE_SETTINGS[:paths][:project][:home]}/office-loader/build"
 end
 
 task :clean do
@@ -81,7 +81,7 @@ def install_dependencies(working_dir)
   shell_command! "rm -rf node_modules", cwd: "#{working_dir}/office-loader"
   update_package_json(working_dir)
   shell_command! "npm install --production --network-concurrency 1", cwd: "#{working_dir}/production"
-  shell_command! "yarn install --network-concurrency 1", cwd: "#{working_dir}/office-loader"
+  # shell_command! "yarn install --network-concurrency 1", cwd: "#{working_dir}/office-loader"
 end
 
 def update_package_json(working_dir)
