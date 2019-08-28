2530f14c2ddd0ad309edf1bee27e59ee08a295e9
Zawadzki <azawadzki@microstrategy.com>
2019-08-28 16:47:46 +0200

    US201894; Added unit tests and requested string changes

--- production/src/_tests_/popup/popup-view-selector.test.js
+++ production/src/_tests_/popup/popup-view-selector.test.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import {shallow} from 'enzyme';
-import {Office} from '../mockOffice';
-import {selectorProperties} from '../../attribute-selector/selector-properties';
-import {_PopupViewSelector, mapStateToProps} from '../../popup/popup-view-selector';
-import {PromptsWindow} from '../../prompts/prompts-window';
-import {PopupTypeEnum} from '../../home/popup-type-enum';
-import {NavigationTree} from '../../navigation/navigation-tree';
-import {AttributeSelectorWindow} from '../../attribute-selector/attribute-selector-window';
+import { shallow } from 'enzyme';
+import { Office } from '../mockOffice';
+import { selectorProperties } from '../../attribute-selector/selector-properties';
+import { _PopupViewSelector, mapStateToProps } from '../../popup/popup-view-selector';
+import { PromptsWindow } from '../../prompts/prompts-window';
+import { PopupTypeEnum } from '../../home/popup-type-enum';
+import { NavigationTree } from '../../navigation/navigation-tree';
+import { AttributeSelectorWindow } from '../../attribute-selector/attribute-selector-window';
+import { DossierWindow } from '../../dossier/dossier-window';
 
 describe('PopupViewSelector', () => {
   it('should render navigation tree when requested', () => {
@@ -28,6 +29,27 @@ describe('PopupViewSelector', () => {
     />);
     // then
     expect(componentWrapper.find(NavigationTree).get(0)).toBeDefined();
+  });
+
+  it('should render navigation tree when requested', () => {
+    // given
+    const location = {
+      search: {},
+    };
+    const props = {
+      popupType: PopupTypeEnum.dossierWindow,
+      propsToPass: {},
+      authToken: 'token',
+    };
+    // when
+    // eslint-disable-next-line react/jsx-pascal-case
+    const componentWrapper = shallow(<_PopupViewSelector
+      location={location}
+      {...props}
+      methods={{}}
+    />);
+    // then
+    expect(componentWrapper.find(DossierWindow).get(0)).toBeDefined();
   });
 
   it('should render AttributeSelectorWindow when requested', () => {
@@ -104,7 +126,7 @@ describe('PopupViewSelector', () => {
     // eslint-disable-next-line react/jsx-pascal-case
     shallow(<_PopupViewSelector
       location={location}
-      importRequested={true}
+      importRequested
       {...reduxMethods}
       {...resultAction}
       authToken={{}}
@@ -280,7 +302,7 @@ describe('PopupViewSelector', () => {
       preparedInstance: instanceId,
       isPrompted: true,
       editedReport: {
-        instanceId: instanceId,
+        instanceId,
         // selectedAttributes: undefined,
         selectedMetrics: [],
         selectedFilters: {},
@@ -322,7 +344,7 @@ describe('PopupViewSelector', () => {
       preparedInstance: instanceId,
       isPrompted: true,
       editedReport: {
-        instanceId: instanceId,
+        instanceId,
         selectedAttributes: 'notEmptyThing',
         selectedMetrics: 'notEmptyThing',
         selectedFilters: 'notEmptyThing',
@@ -396,18 +418,18 @@ describe('PopupViewSelector', () => {
     const attributeId = 'ACF673EC11E9554D08E20080EF651EBC';
     const metricId = 'ACF6AF8811E9554D08EE0080EF651EBC';
     const filterValue = {
-      'ACF66B9A11E9554D08E20080EF651EBC': [
+      ACF66B9A11E9554D08E20080EF651EBC: [
         'ACF66B9A11E9554D08E20080EF651EBC:Europe',
         'ACF66B9A11E9554D08E20080EF651EBC:North America',
       ],
     };
     // eslint-disable-next-line max-len
-    const complexFilterValue = {'8D679D3F11D3E4981000E787EC6DE8A4': ['8D679D3F11D3E4981000E787EC6DE8A4:1', '8D679D3F11D3E4981000E787EC6DE8A4:2', '8D679D3F11D3E4981000E787EC6DE8A4:3', '8D679D3F11D3E4981000E787EC6DE8A4:4', '8D679D3F11D3E4981000E787EC6DE8A4:5', '8D679D3F11D3E4981000E787EC6DE8A4:7', '8D679D3F11D3E4981000E787EC6DE8A4:8', '8D679D3F11D3E4981000E787EC6DE8A4:9', '8D679D3F11D3E4981000E787EC6DE8A4:10', '8D679D3F11D3E4981000E787EC6DE8A4:11', '8D679D3F11D3E4981000E787EC6DE8A4:12', '8D679D3F11D3E4981000E787EC6DE8A4:13', '8D679D3F11D3E4981000E787EC6DE8A4:14', '8D679D3F11D3E4981000E787EC6DE8A4:15', '8D679D3F11D3E4981000E787EC6DE8A4:16', '8D679D3F11D3E4981000E787EC6DE8A4:17', '8D679D3F11D3E4981000E787EC6DE8A4:18', '8D679D3F11D3E4981000E787EC6DE8A4:19', '8D679D3F11D3E4981000E787EC6DE8A4:20', '8D679D3F11D3E4981000E787EC6DE8A4:21', '8D679D3F11D3E4981000E787EC6DE8A4:24', '8D679D3F11D3E4981000E787EC6DE8A4:25', '8D679D3F11D3E4981000E787EC6DE8A4:27', '8D679D3F11D3E4981000E787EC6DE8A4:28', '8D679D3F11D3E4981000E787EC6DE8A4:30', '8D679D3F11D3E4981000E787EC6DE8A4:31', '8D679D3F11D3E4981000E787EC6DE8A4:32', '8D679D3F11D3E4981000E787EC6DE8A4:33', '8D679D3F11D3E4981000E787EC6DE8A4:34', '8D679D3F11D3E4981000E787EC6DE8A4:35', '8D679D3F11D3E4981000E787EC6DE8A4:36', '8D679D3F11D3E4981000E787EC6DE8A4:37', '8D679D3F11D3E4981000E787EC6DE8A4:38', '8D679D3F11D3E4981000E787EC6DE8A4:39'], '8D679D4B11D3E4981000E787EC6DE8A4': ['8D679D4B11D3E4981000E787EC6DE8A4:4', '8D679D4B11D3E4981000E787EC6DE8A4:2', '8D679D4B11D3E4981000E787EC6DE8A4:1', '8D679D4B11D3E4981000E787EC6DE8A4:6', '8D679D4B11D3E4981000E787EC6DE8A4:5', '8D679D4B11D3E4981000E787EC6DE8A4:3', '8D679D4B11D3E4981000E787EC6DE8A4:7', '8D679D4B11D3E4981000E787EC6DE8A4:12']};
+    const complexFilterValue = { '8D679D3F11D3E4981000E787EC6DE8A4': ['8D679D3F11D3E4981000E787EC6DE8A4:1', '8D679D3F11D3E4981000E787EC6DE8A4:2', '8D679D3F11D3E4981000E787EC6DE8A4:3', '8D679D3F11D3E4981000E787EC6DE8A4:4', '8D679D3F11D3E4981000E787EC6DE8A4:5', '8D679D3F11D3E4981000E787EC6DE8A4:7', '8D679D3F11D3E4981000E787EC6DE8A4:8', '8D679D3F11D3E4981000E787EC6DE8A4:9', '8D679D3F11D3E4981000E787EC6DE8A4:10', '8D679D3F11D3E4981000E787EC6DE8A4:11', '8D679D3F11D3E4981000E787EC6DE8A4:12', '8D679D3F11D3E4981000E787EC6DE8A4:13', '8D679D3F11D3E4981000E787EC6DE8A4:14', '8D679D3F11D3E4981000E787EC6DE8A4:15', '8D679D3F11D3E4981000E787EC6DE8A4:16', '8D679D3F11D3E4981000E787EC6DE8A4:17', '8D679D3F11D3E4981000E787EC6DE8A4:18', '8D679D3F11D3E4981000E787EC6DE8A4:19', '8D679D3F11D3E4981000E787EC6DE8A4:20', '8D679D3F11D3E4981000E787EC6DE8A4:21', '8D679D3F11D3E4981000E787EC6DE8A4:24', '8D679D3F11D3E4981000E787EC6DE8A4:25', '8D679D3F11D3E4981000E787EC6DE8A4:27', '8D679D3F11D3E4981000E787EC6DE8A4:28', '8D679D3F11D3E4981000E787EC6DE8A4:30', '8D679D3F11D3E4981000E787EC6DE8A4:31', '8D679D3F11D3E4981000E787EC6DE8A4:32', '8D679D3F11D3E4981000E787EC6DE8A4:33', '8D679D3F11D3E4981000E787EC6DE8A4:34', '8D679D3F11D3E4981000E787EC6DE8A4:35', '8D679D3F11D3E4981000E787EC6DE8A4:36', '8D679D3F11D3E4981000E787EC6DE8A4:37', '8D679D3F11D3E4981000E787EC6DE8A4:38', '8D679D3F11D3E4981000E787EC6DE8A4:39'], '8D679D4B11D3E4981000E787EC6DE8A4': ['8D679D4B11D3E4981000E787EC6DE8A4:4', '8D679D4B11D3E4981000E787EC6DE8A4:2', '8D679D4B11D3E4981000E787EC6DE8A4:1', '8D679D4B11D3E4981000E787EC6DE8A4:6', '8D679D4B11D3E4981000E787EC6DE8A4:5', '8D679D4B11D3E4981000E787EC6DE8A4:3', '8D679D4B11D3E4981000E787EC6DE8A4:7', '8D679D4B11D3E4981000E787EC6DE8A4:12'] };
 
     // eslint-disable-next-line max-len
-    const reportBody = {'requestedObjects': {'attributes': [{'id': attributeId}], 'metrics': [{'id': metricId}]}, 'viewFilter': {'operator': 'In', 'operands': [{'type': 'attribute', 'id': 'ACF66B9A11E9554D08E20080EF651EBC'}, {'type': 'elements', 'elements': [{'id': 'ACF66B9A11E9554D08E20080EF651EBC:Europe'}, {'id': 'ACF66B9A11E9554D08E20080EF651EBC:North America'}]}]}};
+    const reportBody = { requestedObjects: { attributes: [{ id: attributeId }], metrics: [{ id: metricId }] }, viewFilter: { operator: 'In', operands: [{ type: 'attribute', id: 'ACF66B9A11E9554D08E20080EF651EBC' }, { type: 'elements', elements: [{ id: 'ACF66B9A11E9554D08E20080EF651EBC:Europe' }, { id: 'ACF66B9A11E9554D08E20080EF651EBC:North America' }] }] } };
     // eslint-disable-next-line max-len
-    const reportComplexBody = {'requestedObjects': {'attributes': [{'id': '8D679D3F11D3E4981000E787EC6DE8A4'}], 'metrics': [{'id': '4C05177011D3E877C000B3B2D86C964F'}]}, 'viewFilter': {'operator': 'And', 'operands': [{'operator': 'In', 'operands': [{'type': 'attribute', 'id': '8D679D4B11D3E4981000E787EC6DE8A4'}, {'type': 'elements', 'elements': [{'id': '8D679D4B11D3E4981000E787EC6DE8A4:4'}, {'id': '8D679D4B11D3E4981000E787EC6DE8A4:2'}, {'id': '8D679D4B11D3E4981000E787EC6DE8A4:1'}, {'id': '8D679D4B11D3E4981000E787EC6DE8A4:6'}, {'id': '8D679D4B11D3E4981000E787EC6DE8A4:5'}, {'id': '8D679D4B11D3E4981000E787EC6DE8A4:3'}, {'id': '8D679D4B11D3E4981000E787EC6DE8A4:7'}, {'id': '8D679D4B11D3E4981000E787EC6DE8A4:12'}]}]}, {'operator': 'In', 'operands': [{'type': 'attribute', 'id': '8D679D3F11D3E4981000E787EC6DE8A4'}, {'type': 'elements', 'elements': [{'id': '8D679D3F11D3E4981000E787EC6DE8A4:1'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:2'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:3'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:4'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:5'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:7'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:8'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:9'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:10'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:11'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:12'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:13'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:14'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:15'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:16'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:17'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:18'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:19'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:20'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:21'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:24'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:25'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:27'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:28'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:30'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:31'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:32'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:33'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:34'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:35'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:36'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:37'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:38'}, {'id': '8D679D3F11D3E4981000E787EC6DE8A4:39'}]}]}]}};
+    const reportComplexBody = { requestedObjects: { attributes: [{ id: '8D679D3F11D3E4981000E787EC6DE8A4' }], metrics: [{ id: '4C05177011D3E877C000B3B2D86C964F' }] }, viewFilter: { operator: 'And', operands: [{ operator: 'In', operands: [{ type: 'attribute', id: '8D679D4B11D3E4981000E787EC6DE8A4' }, { type: 'elements', elements: [{ id: '8D679D4B11D3E4981000E787EC6DE8A4:4' }, { id: '8D679D4B11D3E4981000E787EC6DE8A4:2' }, { id: '8D679D4B11D3E4981000E787EC6DE8A4:1' }, { id: '8D679D4B11D3E4981000E787EC6DE8A4:6' }, { id: '8D679D4B11D3E4981000E787EC6DE8A4:5' }, { id: '8D679D4B11D3E4981000E787EC6DE8A4:3' }, { id: '8D679D4B11D3E4981000E787EC6DE8A4:7' }, { id: '8D679D4B11D3E4981000E787EC6DE8A4:12' }] }] }, { operator: 'In', operands: [{ type: 'attribute', id: '8D679D3F11D3E4981000E787EC6DE8A4' }, { type: 'elements', elements: [{ id: '8D679D3F11D3E4981000E787EC6DE8A4:1' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:2' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:3' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:4' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:5' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:7' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:8' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:9' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:10' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:11' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:12' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:13' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:14' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:15' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:16' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:17' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:18' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:19' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:20' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:21' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:24' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:25' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:27' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:28' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:30' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:31' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:32' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:33' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:34' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:35' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:36' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:37' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:38' }, { id: '8D679D3F11D3E4981000E787EC6DE8A4:39' }] }] }] } };
 
     it('should parse edited report properties', () => {
       // given
@@ -428,7 +450,7 @@ describe('PopupViewSelector', () => {
         },
       };
       // when
-      const {editedReport} = mapStateToProps(reduxState);
+      const { editedReport } = mapStateToProps(reduxState);
       // then
       expect(editedReport.projectId).toEqual(reportInRedux.projectId);
       expect(editedReport.reportSubtype).toEqual(768);
@@ -463,7 +485,7 @@ describe('PopupViewSelector', () => {
         },
       };
       // when
-      const {editedReport} = mapStateToProps(reduxState);
+      const { editedReport } = mapStateToProps(reduxState);
       // then
       expect(editedReport.projectId).toEqual(reportInRedux.projectId);
       expect(editedReport.reportSubtype).toEqual(768);
@@ -495,7 +517,7 @@ describe('PopupViewSelector', () => {
         },
       };
       // when
-      const {editedReport} = mapStateToProps(reduxState);
+      const { editedReport } = mapStateToProps(reduxState);
       // then
       expect(editedReport.projectId).toEqual(reportInRedux.projectId);
       expect(editedReport.reportSubtype).toEqual(768);
@@ -525,7 +547,7 @@ describe('PopupViewSelector', () => {
         },
       };
       // when
-      const {editedReport} = mapStateToProps(reduxState);
+      const { editedReport } = mapStateToProps(reduxState);
       // then
       expect(editedReport.projectId).toEqual(datesetInRedux.projectId);
       expect(editedReport.reportSubtype).toEqual(779);
@@ -550,7 +572,7 @@ describe('PopupViewSelector', () => {
         },
       };
       // when
-      const {preparedInstance} = mapStateToProps(reduxState);
+      const { preparedInstance } = mapStateToProps(reduxState);
       // then
       expect(preparedInstance).toEqual(reduxState.popupReducer.preparedInstance);
     });
--- production/src/dossier/dossier-window.jsx
+++ production/src/dossier/dossier-window.jsx
@@ -5,10 +5,9 @@ import PropTypes from 'prop-types';
 import { PopupButtons } from '../popup/popup-buttons';
 import { selectorProperties } from '../attribute-selector/selector-properties';
 
-const { Office } = window;
-
 export default class _DossierWindow extends React.Component {
   handleCancel() {
+    const { Office } = window;
     const cancelObject = {
       command: selectorProperties.commandCancel,
     };
@@ -17,12 +16,12 @@ export default class _DossierWindow extends React.Component {
 
   render() {
     const {
-      dossierId, dossierName, handleBack, t,
+      dossierName, handleBack, t,
     } = this.props;
     return (
       <div>
-        <h1 title={dossierName} className="ant-col folder-browser-title">{`${t('Importing')} ${dossierName} with id = ${dossierId}`}</h1>
-        <p>DossierWindowComponent</p>
+        <h1 title={dossierName} className="ant-col folder-browser-title">{`${t('Import Dossier')} > ${dossierName}`}</h1>
+        {/* TODO:  Insert  dossier iframe for embeded API */}
         <PopupButtons
           handleBack={handleBack}
           handleCancel={this.handleCancel}
@@ -33,14 +32,14 @@ export default class _DossierWindow extends React.Component {
 }
 
 _DossierWindow.propTypes = {
-  dossierId: PropTypes.number,
+  // TODO: dossierId: PropTypes.number,
   dossierName: PropTypes.string,
   handleBack: PropTypes.func,
   t: PropTypes.func,
 };
 
 _DossierWindow.defaultProps = {
-  dossierId: 1234,
+  // TODO: dossierId: 1234,
   dossierName: 'testingName',
   handleBack: () => { },
   t: (text) => text,
