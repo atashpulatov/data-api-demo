a0b96234c5632fca4422602892aa1f91e82bea04
bkoziel <bkoziel@microstrategy.com>
2019-08-30 13:48:27 +0200

    US202095;Prepare right panel for visualisations

--- production/src/file-history/file-history-rename-input.jsx
+++ production/src/file-history/file-history-rename-input.jsx
@@ -14,40 +14,6 @@ export class _RenameInput extends React.Component {
     };
   }
 
-  static defaultProps = {
-    t: (text) => text,
-  };
-
-  renameReport = /* istanbul ignore next */ ({ target }) => {
-    const { bindingId, fileName } = this.props;
-    const newName = target.value || fileName;
-    this.setState({ value: newName });
-    this.setEditable(false);
-    newName && bindingId && officeStoreService.preserveReportValue(bindingId, 'name', newName);
-  };
-
-  selectTextAsync = (id) => {
-    // TODO: Timeout hardcoded value, without it cannot select text of the input
-    setTimeout(() => {
-      /* istanbul ignore next */
-      document.getElementById(id).select();
-    }, 100);
-  };
-
-  handleChange = (e) => {
-    this.setState({ value: e.target.value });
-  }
-
-  setEditable = (editable) => {
-    this.setState({ editable });
-  }
-
-  enableEdit = (e) => {
-    e.domEvent && e.domEvent.stopPropagation();
-    this.selectTextAsync(`input-${this.props.bindingId}`);
-    this.setEditable(true);
-  }
-
   getNameContainer(editable, bindingId, fileName, value) {
     if (editable) {
       return (
@@ -67,10 +33,43 @@ export class _RenameInput extends React.Component {
     return <div className="rename-container" id={`rename-container-${bindingId}`}>{value}</div>;
   }
 
+  renameReport = /* istanbul ignore next */ ({ target }) => {
+    const { bindingId, fileName } = this.props;
+    const newName = target.value || fileName;
+    this.setState({ value: newName });
+    this.setEditable(false);
+    if (newName && bindingId) officeStoreService.preserveReportValue(bindingId, 'name', newName);
+  };
+
+  selectTextAsync = (id) => {
+    // TODO: Timeout hardcoded value, without it cannot select text of the input
+    setTimeout(() => {
+      /* istanbul ignore next */
+      document.getElementById(id).select();
+    }, 100);
+  };
+
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  }
+
+  setEditable = (editable) => {
+    this.setState({ editable });
+  }
+
+  enableEdit = (e) => {
+    if (e.domEvent) e.domEvent.stopPropagation();
+    const { bindingId } = this.state;
+    this.selectTextAsync(`input-${bindingId}`);
+    this.setEditable(true);
+  }
+
+
   copyValue = /* istanbul ignore next */ (e) => {
+    const { value } = this.state;
     e.domEvent.stopPropagation();
     const text = document.createElement('textarea');
-    text.value = this.state.value;
+    text.value = value;
     document.body.appendChild(text);
     text.select();
     document.execCommand('copy');
@@ -79,12 +78,18 @@ export class _RenameInput extends React.Component {
 
   render() {
     const { editable, value } = this.state;
-    const { fileName, bindingId, t } = this.props;
+    const {
+      fileName, bindingId, t, isPrompted, buttonsFunctions,
+    } = this.props;
     const nameContainer = this.getNameContainer(editable, bindingId, fileName, value);
     const menu = (
       <Menu>
-        <Menu.Item key="copy" onClick={this.copyValue}>{t('Copy')}</Menu.Item>
+        {isPrompted && <Menu.Item key="reprompt" onClick={(e) => { e.domEvent.stopPropagation(); buttonsFunctions.reprompt(); }}>{t('Reprompt')}</Menu.Item>}
+        <Menu.Item key="edit" onClick={(e) => { e.domEvent.stopPropagation(); buttonsFunctions.edit(); }}>{t('Edit')}</Menu.Item>
+        <Menu.Item key="refresh" onClick={(e) => { e.domEvent.stopPropagation(); buttonsFunctions.refresh(); }}>{t('Refresh')}</Menu.Item>
+        <Menu.Item key="remove" onClick={(e) => { e.domEvent.stopPropagation(); buttonsFunctions.delete(); }}>{t('Remove')}</Menu.Item>
         <Menu.Item key="rename" onClick={this.enableEdit}>{t('Rename')}</Menu.Item>
+        <Menu.Item key="copy" onClick={this.copyValue}>{t('Copy')}</Menu.Item>
       </Menu>
     );
     return (
@@ -98,5 +103,8 @@ export class _RenameInput extends React.Component {
     );
   }
 }
+_RenameInput.defaultProps = {
+  t: (text) => text,
+};
 
 export default withTranslation('common')(_RenameInput);
