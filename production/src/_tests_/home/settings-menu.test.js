e5f150a8c47fb0778bcedf583cd4d13bfb8fd71b
asedano <asedano@microstrategy.com>
2019-09-13 15:50:39 +0200

    Update unit tests

--- production/src/_tests_/home/settings-menu.test.js
+++ production/src/_tests_/home/settings-menu.test.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import {mount} from 'enzyme';
-import {sessionHelper} from '../../storage/session-helper';
-import {_SettingsMenu} from '../../home/settings-menu';
-import {Office} from '../mockOffice';
+import { mount } from 'enzyme';
+import { sessionHelper } from '../../storage/session-helper';
+import { _SettingsMenu } from '../../home/settings-menu';
+import { Office } from '../mockOffice';
 
 describe('Settings Menu', () => {
   afterEach(() => {
     jest.resetAllMocks();
   });
 
-  it('should log out on element logout click', async () => {
+  it('should log out on element logout click and delete the cache DB', async () => {
     // given
-    const logOutRestSpy = jest.spyOn(sessionHelper, 'logOutRest').mockImplementation(() => {});
+    const clearDB = jest.spyOn(sessionHelper, 'clearDB').mockImplementation(() => { });
+    const logOutRestSpy = jest.spyOn(sessionHelper, 'logOutRest').mockImplementation(() => { });
     const logOutSpy = jest.spyOn(sessionHelper, 'logOut');
     const logOutRedirectSpy = jest.spyOn(sessionHelper, 'logOutRedirect');
     const menuWrapper = mount(<_SettingsMenu />);
@@ -19,6 +20,7 @@ describe('Settings Menu', () => {
     // when
     buttonWrapper.simulate('click');
     // then
+    await expect(clearDB).toBeCalled();
     await expect(logOutRestSpy).toBeCalled();
     await expect(logOutSpy).toBeCalled();
     await expect(logOutRedirectSpy).toBeCalled();
