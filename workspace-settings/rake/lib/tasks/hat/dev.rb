c259d19477db83843adb59ba9092ce1ae5f7ea9b
asedano <asedano@microstrategy.com>
2019-09-25 10:28:57 +0200

    Add time now to calculate duration of UT

--- workspace-settings/rake/lib/tasks/hat/dev.rb
+++ workspace-settings/rake/lib/tasks/hat/dev.rb
@@ -163,10 +163,10 @@ task :stage_0_test do
     init_base_branch_repo(ENV["ghprbTargetBranch"])
     #run test with base branch
     run_test(base_repo_path)
+    get_unit_test_metrics("#{$WORKSPACE_SETTINGS[:paths][:project][:home]}")
     generate_comparison_report_html
     generate_comparison_report_markdown
     generate_eslint_report
-    get_unit_test_metrics("#{$WORKSPACE_SETTINGS[:paths][:project][:home]}")
   rescue Exception => e
     message = "Waring: Failed to run test with base branch and generate report, caught exception #{e}!"
     warn(message)
@@ -289,15 +289,14 @@ def generate_comparison_report_markdown
 end
 
 def get_unit_test_metrics(working_dir)
-  base_report_dir = "#{working_dir}/production/coverage"
-  unit_test_result_path = "#{$WORKSPACE_SETTINGS[:paths][:project][:home]}/production/coverage/test-results.json"
+  unit_test_result_path = "#{working_dir}/production/coverage/test-results.json"
   unit_result_json = JSON.parse((File.read(unit_test_result_path)).to_json)
 
   total_tests = unit_result_json['numTotalTests'].to_i
   total_passed = unit_result_json['numPassedTests'].to_i
   total_failures = unit_result_json['numFailedTests'].to_i
   total_skipped = unit_result_json['numPendingTests'].to_i
-  total_duration = 0 #TODO unit_result_json['startTime'] - now()?
+  total_duration = Time.now.to_f - unit_result_json['startTime']
   metrics_unit = {}
   metrics_unit['UNIT_TEST_TOTAL'] = total_tests
   metrics_unit['UNIT_TEST_FAILURES'] = total_failures
