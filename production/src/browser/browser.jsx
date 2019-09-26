da4038211e41d381041cdc87261dde2d6a8cd11b
msabatura <msabatura@microstrategy.com>
2019-09-26 18:32:54 +0200

    added propTypes for Browser

--- production/src/browser/browser.jsx
+++ production/src/browser/browser.jsx
@@ -1,36 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { ObjectTable } from '@mstr/rc';
 import { PopupButtons } from '../popup/popup-buttons';
-import { reportsExample } from './objects';
-import { projectsExample } from './projects';
 
-export const Browser = (props) => {
-  // TODO: replace the storybook code snippets below
-  const objects = reportsExample.result;
-  const projects = projectsExample;
-  console.log({ objects });
-  return (
-    <>
+export const _Browser = ({
+  objects, projects, selected, onSelect, locale, sort, onSortChange, filter, myLibrary,
+}) => (
+  <>
       <ObjectTable
         objects={objects}
         projects={projects}
-        sort={{
-          sortBy: 'dateModified',
-          sortDirection: 'DESC',
-        }}
-        onSortChange={(data) => console.log(data)}
-        selected={{
-          id: '02DDEFDA460B58681B005AAB4A1CBFD3',
-          projectId: 'CE52831411E696C8BD2F0080EFD5AF44',
-        }}
-        onSelect={(data) => console.log(data)}
-        locale="en-US"
-        filter={{}}
-        myLibrary={false}
+        sort={sort}
+        onSortChange={onSortChange}
+        selected={selected}
+        onSelect={onSelect}
+        locale={locale}
+        filter={filter}
+        myLibrary={myLibrary}
       />
       <PopupButtons />
     </>
-  );
+);
+_Browser.propTypes = {
+  objects: PropTypes.arrayOf(PropTypes.object).isRequired,
+  projects: PropTypes.arrayOf(PropTypes.object).isRequired,
+  selected: PropTypes.shape({
+    id: PropTypes.string,
+    projectId: PropTypes.string,
+  }).isRequired,
+  onSelect: PropTypes.func.isRequired,
+  locale: PropTypes.string,
+  sort: PropTypes.shape({
+    sortBy: PropTypes.string.isRequired,
+    sortDirection: PropTypes.string.isRequired,
+  }),
+  onSortChange: PropTypes.func.isRequired,
+  filter: PropTypes.shape({
+    projects: PropTypes.arrayOf(PropTypes.string),
+    types: PropTypes.arrayOf(PropTypes.shape({
+      type: PropTypes.number,
+      subtypes: PropTypes.arrayOf(PropTypes.number),
+    })),
+    certified: PropTypes.bool,
+    owners: PropTypes.arrayOf(PropTypes.string),
+    dateModified: PropTypes.shape({
+      from: PropTypes.object,
+      to: PropTypes.object,
+    }),
+  }),
+  myLibrary: PropTypes.bool,
 };
 
-export default Browser;
+export default _Browser;
