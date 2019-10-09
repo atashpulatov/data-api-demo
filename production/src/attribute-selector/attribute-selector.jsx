039cba5b8f7a1dc1e09ded3bfed66fcd84d54569
Zawadzki <azawadzki@microstrategy.com>
2019-10-09 16:56:25 +0200

    DE146768; New handler and logout when library throws unauthorized error.

--- production/src/attribute-selector/attribute-selector.jsx
+++ production/src/attribute-selector/attribute-selector.jsx
@@ -5,9 +5,30 @@ import { withTranslation } from 'react-i18next';
 export class _AttributeSelector extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      loading: false,
-    };
+    this.state = { loading: false, };
+    this.handleUnauthorized = this.handleUnauthorized.bind(this);
+  }
+
+  /**
+   * Handles unathorized error from library - rearrange
+   * properties to be compatible with errorService.handleError
+   *
+   * @param {Error} e -  Error thrown by mstrReactLibrary
+   */
+  handleUnauthorized(e) {
+    const { handlePopupErrors } = this.props;
+    const newErrorObject = {
+      status: e.status,
+      response: {
+        ...e.response,
+        body: {
+          code: 'ERR009',
+          message: 'The user\'s session has expired, please reauthenticate',
+        },
+        text: '{"code":"ERR009","message":"The user\'s session has expired, please reauthenticate"}',
+      }
+    }
+    handlePopupErrors(newErrorObject)
   }
 
   render() {
@@ -35,6 +56,7 @@ export class _AttributeSelector extends Component {
           closeModal={closeModal}
           toggleSubtotal={toggleSubtotal}
           importSubtotal={mstrData.importSubtotal}
+          handleUnauthorized={this.handleUnauthorized}
         />
       </ErrorBoundary>
     );
