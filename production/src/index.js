5bbf66a2402f6229728ebe3c859d989ea58ed07f
asedano <asedano@microstrategy.com>
2019-10-10 11:14:35 +0200

    Add @mstr/rc to package json

--- production/src/index.js
+++ production/src/index.js
@@ -52,8 +52,8 @@ function goReact() {
       <PersistGate persistor={reduxPersistor}>
         <Home loading={false} />
       </PersistGate>
-                    </Provider>,
-    document.getElementById('root'), () => console.timeEnd('React loading time'));
+    </Provider>,
+      document.getElementById('root'), () => console.timeEnd('React loading time'));
   } else {
     ReactDOM.render(<Provider store={reduxStore}><Popup /></Provider>,
       document.getElementById('root'), () => console.timeEnd('React loading time'));
@@ -65,4 +65,4 @@ officeInitialize();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
