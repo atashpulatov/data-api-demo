889a0e6cd09e42bd2fd5658dd3ab3e11e46b0c93
Zawadzki <azawadzki@microstrategy.com>
2019-10-30 12:49:01 +0100

    Prompts window tests

--- production/src/_tests_/prompts/prompts-window.test.js
+++ production/src/_tests_/prompts/prompts-window.test.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import {mount} from 'enzyme';
-import {_PromptsWindow} from '../../prompts/prompts-window';
+import { shallow } from 'enzyme';
+import { _PromptsWindow } from '../../prompts/prompts-window';
+import { notificationService } from '../../notification/notification-service';
+import { authenticationHelper } from '../../authentication/authentication-helper';
 
-describe('NavigationTree', () => {
-  afterAll(() => {
+describe('_PromptsWindow', () => {
+  afterEach(() => {
     jest.restoreAllMocks();
   });
 
-  it.skip('should render with props given', () => {
+  it('should render with props given', () => {
     // given
     const mstrData = {
       envUrl: 'env',
@@ -16,10 +18,119 @@ describe('NavigationTree', () => {
       reportId: 'reportId',
     };
     // when
-    const wrappedComponent = mount(<_PromptsWindow mstrData={mstrData} />);
+    const wrappedComponent = shallow(<_PromptsWindow mstrData={mstrData} />);
     // then
     expect(wrappedComponent.instance()).toBeDefined();
     expect(wrappedComponent.find('PromptsContainer').get(0)).toBeDefined();
-    expect(wrappedComponent.find('PromptWindowButtons').length).toBe(1);
+  });
+
+  it('addEventListener should be called on mount', () => {
+    // given
+    const mstrData = {
+      envUrl: 'env',
+      token: 'token',
+      projectId: 'projectId',
+      reportId: 'reportId',
+    };
+    const addEventListener = jest.spyOn(window, 'addEventListener');
+    // when
+    shallow(<_PromptsWindow mstrData={mstrData} />);
+    // then
+    expect(addEventListener).toHaveBeenCalled();
+  });
+
+  it('removeEventListener should be called on unmount', () => {
+    // given
+    const mstrData = {
+      envUrl: 'env',
+      token: 'token',
+      projectId: 'projectId',
+      reportId: 'reportId',
+    };
+    const removeEventListener = jest.spyOn(window, 'removeEventListener');
+    // when
+    const wrappedComponent = shallow(<_PromptsWindow mstrData={mstrData} />);
+    wrappedComponent.unmount();
+    // then
+    expect(removeEventListener).toHaveBeenCalled();
+  });
+
+  it('watchForIframeAddition,loadEmbeddedDossier should be called on onPromptsContainerMount with proper params', () => {
+    // given
+    const mstrData = {
+      envUrl: 'env',
+      token: 'token',
+      projectId: 'projectId',
+      reportId: 'reportId',
+    };
+    const ref = React.createRef();
+    // when
+    const wrappedComponent = shallow(<_PromptsWindow mstrData={mstrData} />);
+    const watchForIframeAddition = jest.spyOn(wrappedComponent.instance(), 'watchForIframeAddition').mockImplementation(() => true);
+    const loadEmbeddedDossier = jest.spyOn(wrappedComponent.instance(), 'loadEmbeddedDossier').mockImplementation(() => true);
+    const onIframeLoad = jest.spyOn(wrappedComponent.instance(), 'onIframeLoad').mockImplementation(() => true);
+    wrappedComponent.instance().onPromptsContainerMount(ref);
+    // then
+    expect(watchForIframeAddition).toHaveBeenCalledWith(ref, onIframeLoad);
+    expect(loadEmbeddedDossier).toHaveBeenCalledWith(ref);
+  });
+
+  it('displayNotification should be called on proper messageReceived', () => {
+    // given
+    const mstrData = {
+      envUrl: 'env',
+      token: 'token',
+      projectId: 'projectId',
+      reportId: 'reportId',
+    };
+    const givenMessage = { data: { value: { iServerErrorCode: 1234, message: 'test' } } };
+    const displayNotificationSpy = jest.spyOn(notificationService, 'displayNotification');
+    // when
+    const wrappedComponent = shallow(<_PromptsWindow mstrData={mstrData} />);
+    wrappedComponent.instance().messageReceived(givenMessage);
+    // then
+    const expectedObject = { type: 'warning', content: 'This object cannot be imported.', details: givenMessage.data.value.message };
+    expect(displayNotificationSpy).toBeCalledWith(expectedObject);
+  });
+
+  it('displayNotification should not be called on diffrent messageReceived', () => {
+    // given
+    const mstrData = {
+      envUrl: 'env',
+      token: 'token',
+      projectId: 'projectId',
+      reportId: 'reportId',
+    };
+    const displayNotificationSpy = jest.spyOn(notificationService, 'displayNotification');
+    // when
+    const wrappedComponent = shallow(<_PromptsWindow mstrData={mstrData} />);
+    wrappedComponent.instance().messageReceived();
+    // then
+    expect(displayNotificationSpy).not.toBeCalled();
+  });
+
+  it('handleRun should call handlePopupErrors on not valid auth token ', async () => {
+    // given
+    const mstrData = {
+      envUrl: 'env',
+      token: 'token',
+      projectId: 'projectId',
+      reportId: 'reportId',
+    };
+    const handlePopupErrors = jest.fn();
+    authenticationHelper.validateAuthToken = jest
+      .fn()
+      .mockImplementation(() => {
+        throw Error();
+      });
+    // when
+    const wrappedComponent = shallow(<_PromptsWindow
+      mstrData={mstrData}
+      handlePopupErrors={handlePopupErrors}
+    />);
+    await wrappedComponent.instance().handleRun();
+    // then
+    expect(authenticationHelper.validateAuthToken).toBeCalled();
+    expect(handlePopupErrors).toBeCalled();
   });
 });
