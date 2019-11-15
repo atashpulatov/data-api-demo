8ad03a09cc15a711a0010cf77ea9433dad762ca7
asedano <asedano@microstrategy.com>
2019-11-15 14:26:13 +0100

    US212945;  Split code to speed up dialog loading

---
+++ production/src/entry-point/dialog-entry-point.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { reduxStore } from '../store';
+import { Popup } from '../popup/popup';
+
+
+export default function DialogEntryPoint() {
+  return (
+    <Provider store={reduxStore}>
+      <Popup />
+    </Provider>
+  );
+}
