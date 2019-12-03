79a8c72be1aa7e4659bbbae39bfcc3df841dd107
bkoziel <bkoziel@microstrategy.com>
2019-12-03 14:32:43 +0100

    US215952;changes to formatting

--- production/src/office/office-api-helper.js
+++ production/src/office/office-api-helper.js
@@ -7,7 +7,6 @@ import { officeStoreService } from './store/office-store-service';
 import { notificationService } from '../notification/notification-service';
 import { errorService } from '../error/error-handler';
 import mstrNormalizedJsonHandler from '../mstr-object/mstr-normalized-json-handler';
-import { CONTEXT_LIMIT } from '../mstr-object/mstr-object-rest-service';
 import { authenticationHelper } from '../authentication/authentication-helper';
 import { OBJ_REMOVED_FROM_EXCEL } from '../error/constants';
 import { ProtectedSheetError } from '../error/protected-sheets-error';
@@ -161,96 +160,6 @@ class OfficeApiHelper {
     const { envUrl } = reduxStore.getState().sessionReducer;
     const { username } = reduxStore.getState().sessionReducer;
     return { envUrl, username };
-  }
-
-  formatTable = async (table, isCrosstab, crosstabHeaderDimensions, context) => {
-    if (Office.context.requirements.isSetSupported('ExcelApi', 1.2)) {
-      if (isCrosstab) {
-        const { rowsX } = crosstabHeaderDimensions;
-        table.getDataBodyRange().getColumnsBefore(rowsX).format.autofitColumns();
-      }
-      try {
-        const { columns } = table;
-        columns.load('count');
-        await context.sync();
-        for (let index = 0; index < columns.count; index++) {
-          columns.getItemAt(index).getRange().format.autofitColumns();
-          await context.sync();
-        }
-      } catch (error) {
-        console.log('Error when formatting - no columns autofit applied', error);
-      }
-    } else {
-      notificationService.displayNotification({ type: 'warning', content: 'Unable to format table.' });
-    }
-  }
-
-  formatNumbers = (table, reportConvertedData, isCrosstab) => {
-    const { columnInformation } = reportConvertedData;
-    let filteredColumnInformation;
-    const { columns } = table;
-    if (isCrosstab) {
-      // we store attribute informations in column information in crosstab attributes are
-      // in headers not excel table so we dont need them here.
-      filteredColumnInformation = columnInformation.filter((e) => !e.isAttribute);
-    } else {
-      filteredColumnInformation = columnInformation;
-    }
-    const offset = columnInformation.length - filteredColumnInformation.length;
-    for (const object of filteredColumnInformation) {
-      if (Object.keys(object).length) {
-        const columnRange = columns.getItemAt(object.index - offset).getDataBodyRange();
-        let format = '';
-        if (!object.isAttribute) {
-          if (object.category === 9) {
-            format = this.getNumberFormattingCategoryName(object);
-          } else {
-            format = object.formatString;
-
-            if (format.indexOf('$') !== -1) {
-              // Normalizing formatString from MicroStrategy when locale codes are used [$-\d+]
-              format = format.replace(/\[\$-/g, '[$$$$-')
-                .replace(/\$/g, '\\$')
-                .replace(/\\\$\\\$/g, '$')
-                .replace(/"/g, '');
-            }
-
-            // for fractions set General format
-            if (object.formatString.match(/# \?+?\/\?+?/)) (format = 'General');
-          }
-        }
-        columnRange.numberFormat = format;
-      }
-    }
-  }
-
-  getNumberFormattingCategoryName = (metric) => {
-    switch (metric.category) {
-    case -2:
-      return 'Default';
-    case 9:
-      return 'General';
-    case 0:
-      return 'Fixed';
-    case 1:
-      return 'Currency';
-    case 2:
-      return 'Date';
-    case 3:
-      return 'Time';
-    case 4:
-      return 'Percentage';
-    case 5:
-      return 'Fraction';
-    case 6:
-      return 'Scientific';
-    case 7: // 'Custom'
-      return metric.formatString;
-    case 8:
-      return 'Special';
-    default:
-      return 'General';
-    }
   }
 
   /**
@@ -534,70 +443,6 @@ class OfficeApiHelper {
     const rowOffset = crosstabHeaderDimensions.columnsY;
     const colOffset = crosstabHeaderDimensions.rowsX;
     return this.offsetCellBy(startCell, rowOffset, colOffset);
-  }
-
-  /**
-   * Gets range of subtotal row based on subtotal cell
-   *
-   * @param {Office} startCell Starting table body cell
-   * @param {Office} cell Starting subtotal row cell
-   * @param {Object} mstrTable mstrTable object instance definition
-   * @memberof OfficeApiHelper
-   * @return {Office} Range of subtotal row
-   */
-  getSubtotalRange = (startCell, cell, mstrTable) => {
-    const { headers } = mstrTable;
-    const { axis } = cell;
-    let offsets = {};
-
-    if (axis === 'rows') {
-      offsets = {
-        verticalFirstCell: cell.colIndex,
-        horizontalFirstCell: -(headers.rows[0].length - cell.attributeIndex),
-        verticalLastCell: cell.colIndex,
-        horizontalLastCell: headers.columns[0].length - 1,
-      };
-    } else if (axis === 'columns') {
-      offsets = {
-        verticalFirstCell: -((headers.columns.length - cell.attributeIndex) + 1),
-        horizontalFirstCell: cell.colIndex,
-        verticalLastCell: mstrTable.tableSize.rows,
-        horizontalLastCell: cell.colIndex,
-      };
-    } else { // if not a crosstab
-      offsets = {
-        verticalFirstCell: cell.rowIndex + 1,
-        horizontalFirstCell: cell.attributeIndex,
-        verticalLastCell: cell.rowIndex + 1,
-        horizontalLastCell: mstrTable.tableSize.columns - 1,
-      };
-    }
-    const firstSubtotalCell = startCell.getOffsetRange(offsets.verticalFirstCell, offsets.horizontalFirstCell);
-    const lastSubtotalCell = startCell.getOffsetRange(offsets.verticalLastCell, offsets.horizontalLastCell);
-    return firstSubtotalCell.getBoundingRect(lastSubtotalCell);
-  }
-
-  /**
-   *Sets bold format for all subtotal rows
-   *
-   * @param {Office} startCell Starting table body cell
-   * @param {Office} subtotalCells 2d array of all starting subtotal row cells (each element contains row and colum number of subtotal cell in headers columns)
-   * @param {Object} mstrTable mstrTable object instance definition
-   * @param {Office} context Excel context
-   * @memberof OfficeApiHelper
-   * @return {Promise} Context.sync
-   */
-  formatSubtotals = async (startCell, subtotalCells, mstrTable, context, shouldBold) => {
-    let contextPromises = [];
-    for (const cell of subtotalCells) {
-      const subtotalRowRange = this.getSubtotalRange(startCell, cell, mstrTable);
-      if (subtotalRowRange) (subtotalRowRange.format.font.bold = shouldBold);
-      contextPromises.push(context.sync());
-      if (contextPromises.length % CONTEXT_LIMIT === 0) {
-        await Promise.all(contextPromises);
-        contextPromises = [];
-      }
-    }
   }
 
   /**
