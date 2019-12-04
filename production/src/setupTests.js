dfdb316b3da4ecb9a83def183afa8b3908cd429b
Oleksandr Lisovyk <olisovyk@microstrategy.com>
2019-12-04 14:26:42 +0100

    US207979; Fix unit-tests

--- production/src/setupTests.js
+++ production/src/setupTests.js
@@ -4,4 +4,11 @@ import Adapter from 'enzyme-adapter-react-16';
 import 'jest-localstorage-mock';
 import 'regenerator-runtime/runtime';
 
+jest.mock('@mstr/rc', () => ({
+  // eslint-disable-next-line react/react-in-jsx-scope
+  ObjectTable: () => <div />,
+  // eslint-disable-next-line react/react-in-jsx-scope,react/no-multi-comp
+  TopFilterPanel: () => <div />,
+}));
+
 configure({ adapter: new Adapter() });
