8b6d8da8c20d7a4aef972c5a9605b726e3edc4c3
Hryhorii Havryliuk <hhavryliuk@microstrategy.com>
2019-12-04 15:44:54 +0100

    documentation + tests for the queue

--- production/src/mstr-object/queue.js
+++ production/src/mstr-object/queue.js
@@ -1,21 +1,44 @@
-export default class Queue {
+/**
+ * Helper class to add mstr-objects to cache sequentially
+ * @export
+ * @class AsyncQueue
+ */
+
+export default class AsyncQueue {
+  /**
+   * Creates an instance of AsyncQueue.
+   * @param {Function} callback - Function called when dequeueing elements from the queue
+   * @memberof AsyncQueue
+   */
   constructor(callback) {
     this.queue = [];
-    this.isCaching = false;
-    this.cacheFunction = callback;
+    this.busy = false;
+    this.callback = callback;
   }
 
-  enqueue(objectArray) {
-    this.queue.push(objectArray);
-    return this.dequeue();
+  /**
+ * Adds array of objects to the queue
+ * @param {Array} object - array of mstr objects
+ * @returns
+ * @memberof AsyncQueue
+ */
+  enqueue(object) {
+    this.queue.unshift(object);
+    if (this.callback) return this.dequeue();
   }
 
+  /**
+ * Removes the last element from the queue and runs the callback with it as an argument
+ * @memberof AsyncQueue
+ */
   async dequeue() {
-    if (this.isCaching || !this.queue.length) return false;
-    this.isCaching = true;
-    while (this.queue.length && this.queue.length > 0) {
-      await this.cacheFunction(this.queue.shift());
+    if (!this.busy) {
+      this.busy = true;
+      while (this.queue.length && this.queue.length > 0) {
+        const object = this.queue.pop();
+        await this.callback(object);
+      }
+      this.busy = false;
     }
-    this.isCaching = false;
   }
 }
