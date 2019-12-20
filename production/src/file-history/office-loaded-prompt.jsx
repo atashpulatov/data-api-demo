734ea0c0074a50e3fac3b22d435dc98b365a2567
Zawadzki <azawadzki@microstrategy.com>
2019-12-20 14:15:30 +0100

    Added styles

--- production/src/file-history/office-loaded-prompt.jsx
+++ production/src/file-history/office-loaded-prompt.jsx
@@ -1,36 +1,10 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { Radio, Button, Divider } from 'antd';
+import { Radio, Button } from 'antd';
+import './office-loaded-prompt.scss';
 
 export default function OfficeLoadedPrompt({ answerHandler, closeHandler }) {
   const [answer, onAnswerChange] = useState(false);
-
-  const wrapperStyle = {
-    color: 'red',
-    position: 'absolute',
-    border: 'solid 1px black',
-    top: '66px',
-    left: '20px',
-    right: '20px',
-    background: 'white',
-    zIndex: '2',
-    display: 'flex',
-    flexDirection: 'column'
-  };
-  const buttonWrapperStyle = {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-  };
-  const overlayStyle = {
-    position:'fixed',
-    top: '0',
-    bottom: '0',
-    left: '0',
-    right: '0',
-    background: 'rgba(255,255,255,0.5)',
-    zIndex: '1'
-  };
 
   const worksheetOptions = [
     {
@@ -45,17 +19,17 @@ export default function OfficeLoadedPrompt({ answerHandler, closeHandler }) {
 
   return (
     ReactDOM.createPortal(
-      <div style={overlayStyle}>
-        <div style={wrapperStyle}>
-          <h3>Select destination</h3>
-          <Divider />
+      <div className="component-overlay">
+        <div className="component-wrapper">
+          <div className="title">Select destination</div>
+          <div className="divider" />
           <Radio.Group onChange={(e) => { onAnswerChange(e.target.value); }} value={answer} autofocus>
             {worksheetOptions.map((option) => <Radio style={{ display: 'block' }} value={option.value} key={option.label}>{option.label}</Radio>)}
           </Radio.Group>
-          <Divider />
-          <div style={buttonWrapperStyle}>
-            <Button type="button" onClick={(e) => { e.preventDefault(); answerHandler(answer); }}>OK</Button>
-            <Button type="button" onClick={(e) => { e.preventDefault(); closeHandler(); }}>Cancel</Button>
+          <div className="divider" />
+          <div className="buttons-row">
+            <Button type="primary" onClick={(e) => { e.preventDefault(); answerHandler(answer); }}>OK</Button>
+            <Button onClick={(e) => { e.preventDefault(); closeHandler(); }}>Cancel</Button>
           </div>
         </div>
       </div>,
