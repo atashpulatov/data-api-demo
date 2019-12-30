701b119483ff28c37600e7803b24d859974aa432
writeriks <eozturk@microstrategy.com>
2019-12-30 14:05:47 +0100

    DE146128;-Fix for internet connection appear issue and position on popup

--- production/src/popup/internet-connection-error.jsx
+++ production/src/popup/internet-connection-error.jsx
@@ -4,7 +4,7 @@ import { Icon } from 'antd';
 import { withTranslation } from 'react-i18next';
 
 const InternetConnectionError = ({ t }) => {
-  const [status, setStatus] = useState(false);
+  const [status, setStatus] = useState(window.navigator.onLine);
 
   useEffect(() => {
     const handleConnectionChange = () => (window.navigator.onLine ? setStatus(true) : setStatus(false));
@@ -16,28 +16,28 @@ const InternetConnectionError = ({ t }) => {
   }, [status]);
 
   const renderOfflineMessage = () => (status ? null : (
-    <div className="overlay">
+    <div className="internet-connection-overlay">
       <div
         role="dialog"
         aria-modal="true"
         aria-describedby="description"
         aria-labelledby="title"
-        className="dialog">
-        <div className="row">
-          <div className="column icon">
+        className="internet-connection-dialog">
+        <div className="internet-connection-row">
+          <div className="internet-connection-column internet-connection-icon">
             <Icon type="warning" theme="filled" style={{ color: '#faad14', fontSize:'18px' }} />
           </div>
-          <div className="column infoText">
-            <div id="title" className="row" style={{ fontWeight:500 }}>
+          <div className="internet-connection-column internet-connection-infoText">
+            <div id="title" className="internet-connection-row" style={{ fontWeight:500 }}>
               {t('The internet connection appears to be offline.')}
             </div>
-            <div id="description" className="row">
+            <div id="description" className="internet-connection-row">
               {t('Please check your internet connection.')}
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="loading-container">
+        <div className="internet-connection-row">
+          <div className="internet-connection-loading-container">
             <img style={{ width:'1.8em', height:'1.8em' }} src="./assets/small_loading.gif" alt="Loading icon" />
             <span>{t('Trying to connect...')}</span>
           </div>
