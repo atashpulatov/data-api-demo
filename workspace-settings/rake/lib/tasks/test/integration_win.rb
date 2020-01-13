26f194a1dd9435222226b91a06106432d3c5afdc
yzhai <yzhai@microstrategy.com>
2020-01-13 14:36:16 +0100

    US221583; update tomcat config path in windows

--- workspace-settings/rake/lib/tasks/test/integration_win.rb
+++ workspace-settings/rake/lib/tasks/test/integration_win.rb
@@ -17,8 +17,8 @@ task :deploy_tester_server,[:build_no] do | t, args|
   web_dossier_war = "#{tomcat_dir}\\webapps\\web-dossier.war"
   FileUtils.rm_rf web_dossier_dir if Dir.exists? web_dossier_dir
   FileUtils.rm web_dossier_war if File.exists? web_dossier_war
-  tomcat_config_dir = "#{$WORKSPACE_SETTINGS[:paths][:project][:tests][:home]}\\integration\\e2e-webserver"
-  tomcat_config_win_dir = "#{$WORKSPACE_SETTINGS[:paths][:project][:tests][:home]}\\integration\\e2e-webserver-windows"
+  tomcat_config_dir = "#{$WORKSPACE_SETTINGS[:paths][:project][:tests][:home].gsub("/","\\")}\\integration\\e2e-webserver"
+  tomcat_config_win_dir = "#{$WORKSPACE_SETTINGS[:paths][:project][:tests][:home]].gsub("/","\\")}\\integration\\e2e-webserver-windows"
   FileUtils.cp("#{tomcat_config_win_dir}\\server.xml","#{tomcat_dir}\\conf\\server.xml")
   FileUtils.cp("#{tomcat_config_dir}\\mstr-office.jks","#{tomcat_dir}\\conf\\mstr-office.jks")
   FileUtils.cp("#{tomcat_config_dir}\\tomcat-users.xml","#{tomcat_dir}\\conf\\tomcat-users.xml")
