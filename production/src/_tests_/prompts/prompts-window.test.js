db89ed93542c421c0a2e693bc94a1de015bfc0b6
Ryszard Raszplewicz <rraszplewicz@microstrategy.com>
2020-01-22 12:00:52 +0100

    US216823; popup-view-selector.test.js, dossier-window.test.js, popup-reducer.test.js, prompts-window.test.js, popup.test.js fixed.

--- production/src/_tests_/prompts/prompts-window.test.js
+++ production/src/_tests_/prompts/prompts-window.test.js
@@ -3,22 +3,30 @@ import { shallow } from 'enzyme';
 import { _PromptsWindow } from '../../prompts/prompts-window';
 import { notificationService } from '../../notification/notification-service';
 import { authenticationHelper } from '../../authentication/authentication-helper';
+import { popupHelper } from '../../popup/popup-helper';
 
-describe.skip('_PromptsWindow', () => {
+jest.mock('../../popup/popup-helper');
+
+describe('_PromptsWindow', () => {
+  const mstrData = {
+    envUrl: 'env',
+    token: 'token',
+    projectId: 'projectId',
+    chosenObjectId: 'chosenObjectId',
+  };
+
+  const popupState = {
+    isReprompt: false
+  };
+
   afterEach(() => {
     jest.restoreAllMocks();
   });
 
   it('should render with props given', () => {
     // given
-    const mstrData = {
-      envUrl: 'env',
-      token: 'token',
-      projectId: 'projectId',
-      chosenObjectId: 'chosenObjectId',
-    };
     // when
-    const wrappedComponent = shallow(<_PromptsWindow mstrData={mstrData} />);
+    const wrappedComponent = shallow(<_PromptsWindow mstrData={mstrData} popupState={popupState} />);
     // then
     expect(wrappedComponent.instance()).toBeDefined();
     expect(wrappedComponent.find('PromptsContainer').get(0)).toBeDefined();
@@ -26,30 +34,18 @@ describe.skip('_PromptsWindow', () => {
 
   it('addEventListener should be called on mount', () => {
     // given
-    const mstrData = {
-      envUrl: 'env',
-      token: 'token',
-      projectId: 'projectId',
-      chosenObjectId: 'chosenObjectId',
-    };
     const addEventListener = jest.spyOn(window, 'addEventListener');
     // when
-    shallow(<_PromptsWindow mstrData={mstrData} />);
+    shallow(<_PromptsWindow mstrData={mstrData} popupState={popupState} />);
     // then
     expect(addEventListener).toHaveBeenCalled();
   });
 
   it('removeEventListener should be called on unmount', () => {
     // given
-    const mstrData = {
-      envUrl: 'env',
-      token: 'token',
-      projectId: 'projectId',
-      chosenObjectId: 'chosenObjectId',
-    };
     const removeEventListener = jest.spyOn(window, 'removeEventListener');
     // when
-    const wrappedComponent = shallow(<_PromptsWindow mstrData={mstrData} />);
+    const wrappedComponent = shallow(<_PromptsWindow mstrData={mstrData} popupState={popupState} />);
     wrappedComponent.unmount();
     // then
     expect(removeEventListener).toHaveBeenCalled();
@@ -57,15 +53,9 @@ describe.skip('_PromptsWindow', () => {
 
   it('watchForIframeAddition,loadEmbeddedDossier should be called on onPromptsContainerMount with proper params', () => {
     // given
-    const mstrData = {
-      envUrl: 'env',
-      token: 'token',
-      projectId: 'projectId',
-      chosenObjectId: 'chosenObjectId',
-    };
     const ref = React.createRef();
     // when
-    const wrappedComponent = shallow(<_PromptsWindow mstrData={mstrData} />);
+    const wrappedComponent = shallow(<_PromptsWindow mstrData={mstrData} popupState={popupState} />);
     const watchForIframeAddition = jest.spyOn(wrappedComponent.instance(), 'watchForIframeAddition').mockImplementation(() => true);
     const loadEmbeddedDossier = jest.spyOn(wrappedComponent.instance(), 'loadEmbeddedDossier').mockImplementation(() => true);
     const onIframeLoad = jest.spyOn(wrappedComponent.instance(), 'onIframeLoad').mockImplementation(() => true);
@@ -77,33 +67,25 @@ describe.skip('_PromptsWindow', () => {
 
   it('displayNotification should be called on proper messageReceived', () => {
     // given
-    const mstrData = {
-      envUrl: 'env',
-      token: 'token',
-      projectId: 'projectId',
-      chosenObjectId: 'chosenObjectId',
-    };
     const givenMessage = { data: { value: { iServerErrorCode: 1234, message: 'test' } } };
     const displayNotificationSpy = jest.spyOn(notificationService, 'displayNotification');
     // when
-    const wrappedComponent = shallow(<_PromptsWindow mstrData={mstrData} />);
+    const wrappedComponent = shallow(<_PromptsWindow mstrData={mstrData} popupState={popupState} />);
     wrappedComponent.instance().messageReceived(givenMessage);
     // then
-    const expectedObject = { type: 'warning', content: 'This object cannot be imported.', details: givenMessage.data.value.message };
+    const expectedObject = {
+      type: 'warning',
+      content: 'This object cannot be imported.',
+      details: givenMessage.data.value.message
+    };
     expect(displayNotificationSpy).toBeCalledWith(expectedObject);
   });
 
   it('displayNotification should not be called on diffrent messageReceived', () => {
     // given
-    const mstrData = {
-      envUrl: 'env',
-      token: 'token',
-      projectId: 'projectId',
-      chosenObjectId: 'chosenObjectId',
-    };
     const displayNotificationSpy = jest.spyOn(notificationService, 'displayNotification');
     // when
-    const wrappedComponent = shallow(<_PromptsWindow mstrData={mstrData} />);
+    const wrappedComponent = shallow(<_PromptsWindow mstrData={mstrData} popupState={popupState} />);
     wrappedComponent.instance().messageReceived();
     // then
     expect(displayNotificationSpy).not.toBeCalled();
@@ -111,13 +93,7 @@ describe.skip('_PromptsWindow', () => {
 
   it('handleRun should call handlePopupErrors on not valid auth token ', async () => {
     // given
-    const mstrData = {
-      envUrl: 'env',
-      token: 'token',
-      projectId: 'projectId',
-      chosenObjectId: 'chosenObjectId',
-    };
-    const handlePopupErrors = jest.fn();
+    popupHelper.handlePopupErrors = jest.fn();
     authenticationHelper.validateAuthToken = jest
       .fn()
       .mockImplementation(() => {
@@ -126,11 +102,11 @@ describe.skip('_PromptsWindow', () => {
     // when
     const wrappedComponent = shallow(<_PromptsWindow
       mstrData={mstrData}
-      handlePopupErrors={handlePopupErrors}
+      popupState={popupState}
     />);
     await wrappedComponent.instance().handleRun();
     // then
     expect(authenticationHelper.validateAuthToken).toBeCalled();
-    expect(handlePopupErrors).toBeCalled();
+    expect(popupHelper.handlePopupErrors).toBeCalled();
   });
 });
