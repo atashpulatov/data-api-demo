6d878ce0b444bd8287f78ac097bcdd67f841883f
Piotr Nastaj <pnastaj@microstrategy.com>
2020-02-13 14:19:31 +0100

    US227905; Automation for DE157988; Update TC59464

--- tests/integration/test-driver-browser/test/specs/release-validation/TF7862-regression/TC59464-binding-imported-report.spec.js
+++ tests/integration/test-driver-browser/test/specs/release-validation/TF7862-regression/TC59464-binding-imported-report.spec.js
@@ -2,13 +2,8 @@ import OfficeLogin from '../../../helpers/office/office.login';
 import OfficeWorksheet from '../../../helpers/office/office.worksheet';
 import PluginRightPanel from '../../../helpers/plugin/plugin.right-panel';
 import PluginPopup from '../../../helpers/plugin/plugin.popup';
-import { switchToPluginFrame, switchToExcelFrame, switchToPopupFrame } from '../../../helpers/utils/iframe-helper';
-import { writeDataIntoFile, getJsonData } from '../../../helpers/utils/benchmark-helper';
-import { objects as o } from '../../../constants/objects-list';
-import { waitForNotification, waitForPopup } from '../../../helpers/utils/wait-helper';
-import { selectors as se } from '../../../constants/selectors/plugin.right-panel-selectors';
-import { dictionary } from '../../../constants/dictionaries/dictionary';
-import { excelSelectors as es } from '../../../constants/selectors/office-selectors';
+import { switchToPluginFrame, switchToExcelFrame } from '../../../helpers/utils/iframe-helper';
+import { waitForNotification } from '../../../helpers/utils/wait-helper';
 import { waitAndClick } from '../../../helpers/utils/click-helper';
 import settings from '../../../config';
 
@@ -34,15 +29,15 @@ describe('F28550 - Excel Connector Hardening: Rename Excel table without losing 
   it('[TC59464] - Checking binding for newly imported report', () => {
     OfficeWorksheet.selectCell('A3');
     PluginRightPanel.clickImportDataButton();
-    browser.pause(5000);
+    // browser.pause(5000); // usually it works without these two pauses, but sometimes they help...
     switchToPluginFrame();
     PluginPopup.importObject('01. • !#$%&\'()*+,-:;<=>@^`{|}~¢£¥¬«» Report for testing binding and special characters . • !#$%&\'()*+,-:;<=>@^`{|}~¢£¥¬«». • !#$%&\'()*+,-:;<=>@^`{|}~¢£¥¬«» Report for testing binding and special characters . • !#$%&\'()*+,-:;<=>@/`testtesttes/km123456', false);
+    // browser.pause(5000); // usually it works without these two pauses, but sometimes they help...
     waitForNotification();
     switchToExcelFrame();
     waitAndClick($('#m_excelWebRenderer_ewaCtl_NameBox-Medium > a'), 4000);
-    // browser.keys('\uE05B');
     const importedTableName = $('[id^=_01___________________________________Report_for_testing_binding_and_special_characters]> span').getText();
-    const normalizedTableName = importedTableName.replace(/\d{13}\b/, 'TIMESTAMP'); // searches for 13 digits at the end of the string and replaces them with "TIMESTAMP" - this is in order make the string universal for testing
+    const normalizedTableName = importedTableName.replace(/\d{13}\b/, 'TIMESTAMP'); // searches for 13 digits at the end of the string and replaces them with "TIMESTAMP" - this is to make the string universal for testing purposes
     expect(normalizedTableName).toEqual('_01___________________________________Report_for_testing_binding_and_special_characters______________________________________________________________________Report_for_testing_binding_and_special_characters_________________________testtestt_TIMESTAMP');
   });
 });
