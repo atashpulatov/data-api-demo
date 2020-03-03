587c73813ca148824d75d090043ec65eba368002
Pawel Laskowski <plaskowski@microstrategy.com>
2020-03-03 16:41:20 +0100

    US230052; TC54976; Extract const for number of pages to scroll

--- tests/integration/test-driver-browser/test/specs/release-validation/F24087-improve-scrolling-performance/TC54976-E2E-scenario.spec.js
+++ tests/integration/test-driver-browser/test/specs/release-validation/F24087-improve-scrolling-performance/TC54976-E2E-scenario.spec.js
@@ -3,7 +3,7 @@ import OfficeWorksheet from '../../../helpers/office/office.worksheet';
 import PluginRightPanel from '../../../helpers/plugin/plugin.right-panel';
 import PluginPopup from '../../../helpers/plugin/plugin.popup';
 import { waitForNotification } from '../../../helpers/utils/wait-helper';
-import { rightPanelSelectors as se } from '../../../constants/selectors/plugin.right-panel-selectors';
+import { rightPanelSelectors as selectors } from '../../../constants/selectors/plugin.right-panel-selectors';
 import { dictionary } from '../../../constants/dictionaries/dictionary';
 
 describe('F24087 - Improve performance of scrolling through the object list', () => {
@@ -18,19 +18,20 @@ describe('F24087 - Improve performance of scrolling through the object list', ()
   });
 
   it('[TC54976][Object Table] End to End scenario on Excel implementation | E2E', () => {
+    const pageScrollCount = 200;
     OfficeWorksheet.selectCell('A1');
     PluginRightPanel.clickImportDataButton();
     PluginPopup.switchLibrary(false);
-    PluginPopup.scrollTableDownByPages(200);
+    PluginPopup.scrollTableDownByPages(pageScrollCount);
     PluginPopup.clickHeader('Owner');
-    PluginPopup.scrollTableDownByPages(200);
+    PluginPopup.scrollTableDownByPages(pageScrollCount);
     browser.setWindowSize(1000, 800);
-    PluginPopup.scrollTableDownByPages(200);
+    PluginPopup.scrollTableDownByPages(pageScrollCount);
     PluginPopup.searchForObject('Supplier Sales Report');
     browser.pause(999); // waiting for search to filter the ObjectTable
     PluginPopup.selectFirstObject();
     PluginPopup.clickImport();
     waitForNotification();
-    expect($(se.notificationPopUp).getAttribute('textContent')).toContain(dictionary.en.importSuccess);
+    expect($(selectors.notificationPopUp).getAttribute('textContent')).toContain(dictionary.en.importSuccess);
   });
 });
