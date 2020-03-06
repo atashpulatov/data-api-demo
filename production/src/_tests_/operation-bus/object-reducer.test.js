7f9fbea6c42309456dd7d86f370f3f4e84f7ef88
pfilipp <pfilipp@microstrategy.com>
2020-03-06 13:59:22 +0100

    Added spacing in tests

--- production/src/_tests_/operation-bus/object-reducer.test.js
+++ production/src/_tests_/operation-bus/object-reducer.test.js
@@ -35,22 +35,29 @@ describe('objectReducer', () => {
       }]
     }
   };
+
   it('should have default state if provided undefined', () => {
     // given
     const unhandledAction = { type: 'some action' };
+
     // when
     const resultState = objectReducer(undefined, unhandledAction);
+
     // then
     expect(resultState).toEqual({ objects: [] });
   });
+
   it('should return the same state if action not handled by reducer', () => {
     // given
     const unhandledAction = { type: 'some action' };
+
     // when
     const resultState = objectReducer(initialState.multipleObjects, unhandledAction);
+
     // then
     expect(resultState).toBe(initialState.multipleObjects);
   });
+
   describe('importRequested', () => {
     it('should add first object to array and return new array', () => {
       // given
@@ -58,35 +65,45 @@ describe('objectReducer', () => {
         type: IMPORT_REQUESTED,
         payload: { object: initialObject, }
       };
+
       // when
       const resultState = objectReducer(initialState.empty, action);
+
       // then
       expect(resultState).toEqual({ objects: [initialObject] });
     });
+
     it('should add object to array and return new array', () => {
       // given
       const action = {
         type: IMPORT_REQUESTED,
         payload: { object: initialObject, }
       };
+
       // when
       const resultState = objectReducer(initialState.singleObject, action);
+
       // then
       expect(resultState).toEqual({ objects: [...initialState.singleObject.objects, initialObject] });
     });
   });
+
   describe('updateObject', () => {
     it('should return same array if element not found', () => {
+      // given
       const objectName = 'someName';
       const action = {
         type: UPDATE_OBJECT,
         payload: { objectWorkingId: 'someOtherString23', objectName },
       };
+
       // when
       const resultState = objectReducer(initialState.singleObject, action);
+
       // then
       expect(resultState).toEqual(initialState.singleObject);
     });
+
     it('should add one property to object on single element array', () => {
       // given
       const objectName = 'someName';
@@ -94,11 +111,14 @@ describe('objectReducer', () => {
         type: UPDATE_OBJECT,
         payload: { objectWorkingId: 'someOtherString234', objectName },
       };
+
       // when
       const resultState = objectReducer(initialState.singleObject, action);
+
       // then
       expect(resultState.objects[0]).toEqual({ ...initialState.singleObject.objects[0], objectName });
     });
+
     it('should add two properties to object on single element array', () => {
       // given
       const objectName = 'someName';
@@ -107,11 +127,14 @@ describe('objectReducer', () => {
         type: UPDATE_OBJECT,
         payload: { objectWorkingId: 'someOtherString234', objectName, someProp },
       };
+
       // when
       const resultState = objectReducer(initialState.singleObject, action);
+
       // then
       expect(resultState.objects[0]).toEqual({ ...initialState.singleObject.objects[0], objectName, someProp });
     });
+
     it('should add one property to object on multi element array', () => {
       // given
       const objectName = 'someName';
@@ -119,11 +142,14 @@ describe('objectReducer', () => {
         type: UPDATE_OBJECT,
         payload: { objectWorkingId: 'someOtherString23', objectName },
       };
+
       // when
       const resultState = objectReducer(initialState.multipleObjects, action);
+
       // then
       expect(resultState.objects[1]).toEqual({ ...initialState.multipleObjects.objects[1], objectName });
     });
+
     it('should add two properties to object on multi element array', () => {
       // given
       const objectName = 'someName';
@@ -132,12 +158,15 @@ describe('objectReducer', () => {
         type: UPDATE_OBJECT,
         payload: { objectWorkingId: 'someOtherString23', objectName, someProp },
       };
+
       // when
       const resultState = objectReducer(initialState.multipleObjects, action);
+
       // then
       expect(resultState.objects[1]).toEqual({ ...initialState.multipleObjects.objects[1], objectName, someProp });
     });
   });
+
   describe('deleteObject', () => {
     it('should not remove any objects if array is empty', () => {
       // given
@@ -146,11 +175,14 @@ describe('objectReducer', () => {
         type: DELETE_OBJECT,
         payload: someId,
       };
+
       // when
       const resultState = objectReducer(initialState.empty, action);
+
       // then
       expect(resultState).toEqual(initialState.empty);
     });
+
     it('should not remove any objects if id doesnt exist in array', () => {
       // given
       const someId = 'some id';
@@ -158,11 +190,14 @@ describe('objectReducer', () => {
         type: DELETE_OBJECT,
         payload: someId,
       };
+
       // when
       const resultState = objectReducer(initialState.multipleObjects, action);
+
       // then
       expect(resultState).toEqual(initialState.multipleObjects);
     });
+
     it('should remove object if id exists in array', () => {
       // given
       const someId = 'someOtherString23';
@@ -170,11 +205,14 @@ describe('objectReducer', () => {
         type: DELETE_OBJECT,
         payload: someId,
       };
+
       // when
       const resultState = objectReducer(initialState.multipleObjects, action);
+
       // then
       expect(resultState.objects).toHaveLength(2);
     });
+
     it('should remove last object if id exists in array', () => {
       // given
       const someId = 'someOtherString234';
@@ -182,8 +220,10 @@ describe('objectReducer', () => {
         type: DELETE_OBJECT,
         payload: someId,
       };
+
       // when
       const resultState = objectReducer(initialState.singleObject, action);
+
       // then
       expect(resultState.objects).toHaveLength(0);
     });
