69b9f24fd5a6f20eeb91efdebcf4e5a269bbc4be
asedano <asedano@microstrategy.com>
2020-03-16 12:34:39 +0100

    Remove embeddinglib.js and add readme

---
+++ production/public/javascript/README.md
@@ -0,0 +1,11 @@
+# embeddinglib.js for local development
+
+In order to use functionality such as prompts and dossier `embeddinglib.js` is required. This file is part of [embeded-bi-sdk](https://github.microstrategy.com/Kiai/embeded-bi-sdk).
+
+This file is part of MicroStrategyLibrary and we just reference it in the code `../../javascript/embeddinglib.js`.
+
+## Local development
+
+To use dossier functionality in localhost (outside of MicroStrategyLibrary) create embeddinglib.js in `/production/public/javascript/`, and copy the code from a working environment (ideally same environment you are going to connect to).
+
+`https://env-000000.customer.cloud.microstrategy.com/MicroStrategyLibrary/javascript/embeddinglib.js'`
--- production/public/javascript/embeddinglib.js
+++
@@ -1,8 +0,0 @@
-// Placeholder of embeddinglib.js for local development
-console.group('Using Add-in outside MicroStrategyLibrary, prompts not available');
-console.log('To be able to deploy this locally, please do the following:');
-console.log('1) Temporarily run a new instance of Chrome with CORS turned off with the following command: '
-  + 'open -n -a "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --args --user-data-dir="/tmp/chrome_dev_test" --disable-web-security '
-  + 'in your terminal');
-console.log('2) Replace the embeddinglib.js placeholder in /production/public/javascript with actual, up-to-date library, ie. from https://demo.microstrategy.com/MicroStrategyLibrary/javascript/embeddinglib.js');
-console.groupEnd();
