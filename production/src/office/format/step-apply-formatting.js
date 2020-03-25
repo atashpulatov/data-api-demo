df913a2af2a0dd8b1f4ff6bedf6f86a0c3fd89f3
Ryszard Raszplewicz <rraszplewicz@microstrategy.com>
2020-03-25 15:01:16 +0100

    step-apply-formatting unit tests.

--- production/src/office/format/step-apply-formatting.js
+++ production/src/office/format/step-apply-formatting.js
@@ -17,55 +17,86 @@ class StepApplyFormatting {
    * @param {Office} operationData.excelContext Reference to Excel Context used by Excel API functions
    */
   applyFormatting = async (objectData, operationData) => {
+    console.time('Apply formatting');
+
     try {
-      console.time('Apply formatting');
       const {
         objectWorkingId, excelContext, instanceDefinition, officeTable,
       } = operationData;
       const { columnInformation, isCrosstab } = instanceDefinition.mstrTable;
 
       const filteredColumnInformation = this.filterColumnInformation(columnInformation, isCrosstab);
-      let attributeColumnNumber = 0; // this is number of all atrribute/consolidations columns in Excel
-      let offset = 0;
 
-      columnInformation.forEach(element => {
-        if (element.isAttribute) {
-          attributeColumnNumber += (element.forms && element.forms.length) ? element.forms.length : 1;
-        } else {
-          attributeColumnNumber++;
-        }
-      });
+      const attributeColumnNumber = this.calculateAttributeColumnNumber(columnInformation);
 
-      if (isCrosstab) {
-        offset = columnInformation.length - filteredColumnInformation.length;
-      } else {
-        offset = attributeColumnNumber - columnInformation.length;
-      }
+      const offset = this.calculateOffset(
+        isCrosstab,
+        columnInformation.length,
+        filteredColumnInformation.length,
+        attributeColumnNumber
+      );
 
-      for (let i = filteredColumnInformation.length - 1; i >= 0; i--) {
-        const object = filteredColumnInformation[i];
-        const objectIndex = isCrosstab ? object.index - offset : object.index + offset;
-        const columnRange = officeTable.columns.getItemAt(objectIndex).getDataBodyRange();
-
-        if (!object.isAttribute) {
-          columnRange.numberFormat = this.getFormat(object);
-        } else {
-          columnRange.numberFormat = '';
-        }
-      }
+      this.setupFormatting(filteredColumnInformation, isCrosstab, offset, officeTable);
 
       await excelContext.sync();
+
       operationStepDispatcher.completeFormatData(objectWorkingId);
     } catch (error) {
       console.error(error);
       console.log('Cannot apply formatting, skipping');
-    } finally {
-      console.timeEnd('Apply formatting');
+    }
+
+    console.timeEnd('Apply formatting');
+  };
+
+  calculateAttributeColumnNumber = (columnInformation) => {
+    let attributeColumnNumber = 0; // this is number of all atrribute/consolidations columns in Excel
+
+    columnInformation.forEach(element => {
+      if (element.isAttribute) {
+        attributeColumnNumber += (element.forms && element.forms.length) ? element.forms.length : 1;
+      } else {
+        attributeColumnNumber++;
+      }
+    });
+
+    return attributeColumnNumber;
+  };
+
+  calculateOffset = (isCrosstab, columnInformationLength, filteredColumnInformationLength, attributeColumnNumber) => {
+    let offset = 0;
+
+    if (isCrosstab) {
+      offset = columnInformationLength - filteredColumnInformationLength;
+    } else {
+      offset = attributeColumnNumber - columnInformationLength;
+    }
+
+    return offset;
+  };
+
+  setupFormatting = (filteredColumnInformation, isCrosstab, offset, officeTable) => {
+    for (let i = filteredColumnInformation.length - 1; i >= 0; i--) {
+      const object = filteredColumnInformation[i];
+
+      const columnRange = this.getColumnRangeForFormatting(object.index, isCrosstab, offset, officeTable);
+
+      if (object.isAttribute) {
+        columnRange.numberFormat = '';
+      } else {
+        columnRange.numberFormat = this.getFormat(object);
+      }
     }
   };
 
+  getColumnRangeForFormatting = (index, isCrosstab, offset, officeTable) => {
+    const objectIndex = isCrosstab ? index - offset : index + offset;
+
+    return officeTable.columns.getItemAt(objectIndex).getDataBodyRange();
+  };
+
   /**
-   * Return filtered column information
+   * Returns filtered column information.
    *
    * @param columnInformation
    * @param isCrosstab
@@ -79,23 +110,32 @@ class StepApplyFormatting {
   };
 
   /**
-   * Return parsed format string
+   * Returns parsed format string.
    *
    * @param {String} format given by MicroStrategy
    * @return {String} parsed format
    */
   getFormat = ({ formatString, category }) => {
-    if (category === 9) { return 'General'; }
+    if (category === 9) {
+      return 'General';
+    }
+
     // For fractions set General format
-    if (formatString.match(/# \?+?\/\?+?/)) { return 'General'; }
+    if (formatString.match(/# \?+?\/\?+?/)) {
+      return 'General';
+    }
+
+    // Normalizing formatString from MicroStrategy when locale codes are used [$-\d+]
     if (formatString.indexOf('$') !== -1) {
       return formatString.replace(/\[\$-/g, '[$$$$-')
         .replace(/\$/g, '\\$')
         .replace(/\\\$\\\$/g, '$')
         .replace(/"/g, '');
     }
+
     return formatString;
   };
 }
+
 const stepApplyFormatting = new StepApplyFormatting();
 export default stepApplyFormatting;
