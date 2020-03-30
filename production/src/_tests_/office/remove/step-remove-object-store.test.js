1a1b5e30f4ac3d9d15ee24baeecb72b91f1878a5
Ryszard Raszplewicz <rraszplewicz@microstrategy.com>
2020-03-30 16:12:50 +0200

    step-remove-object-binding, step-remove-object-store, step-remove-object-table.test unit tests.

---
+++ production/src/_tests_/office/remove/step-remove-object-store.test.js
@@ -0,0 +1,33 @@
+import operationStepDispatcher from '../../../operation/operation-step-dispatcher';
+import stepRemoveObjectStore from '../../../office/remove/step-remove-object-store';
+import { officeStoreService } from '../../../office/store/office-store-service';
+
+describe('StepRemoveObjectStore', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('stepRemoveObjectStore should work as expected', async () => {
+    // given
+    const removeObjectFromStoreMock = jest.spyOn(officeStoreService, 'removeObjectFromStore').mockImplementation();
+
+    const completeRemoveObjectStoreMock = jest.spyOn(
+      operationStepDispatcher, 'completeRemoveObjectStore'
+    ).mockImplementation();
+
+    const objectData = {
+      objectWorkingId: 'objectWorkingIdTest',
+      bindId: 'bindIdTest',
+    };
+
+    // when
+    await stepRemoveObjectStore.removeObjectStore(objectData, {});
+
+    // then
+    expect(removeObjectFromStoreMock).toBeCalledTimes(1);
+    expect(removeObjectFromStoreMock).toBeCalledWith('bindIdTest', 'objectWorkingIdTest');
+
+    expect(completeRemoveObjectStoreMock).toBeCalledTimes(1);
+    expect(completeRemoveObjectStoreMock).toBeCalledWith('objectWorkingIdTest');
+  });
+});
