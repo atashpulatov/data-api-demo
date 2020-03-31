3194a7e3428c0d562972313faaa6901aa18f4107
mblachnio <mblachnio@microstrategy.com>
2020-03-31 11:53:21 +0200

    Updated script to remove ifs.

--- tests/integration/test-driver-browser/test/specs/release-validation/F24087-improve-scrolling-performance/TC55132-Date-format-I18N.spec.js
+++ tests/integration/test-driver-browser/test/specs/release-validation/F24087-improve-scrolling-performance/TC55132-Date-format-I18N.spec.js
@@ -25,25 +25,32 @@ describe('F24087 - Improve performance of scrolling through the object list', ()
     //Selecting which format should be used for assertion based on
     //the object 'Eastern Region Average Revenue per Customer' on MicroStrategy Tutorial modified date
     //and the Language and Region property value
-    let expectedDateFormat = '8/12/2016 9:32 PM'; //Defaul and fallback - English US
-    if (browser.config.languageRegion === 'de-de') expectedDateFormat = '12.8.2016 21:32'; //German
-    else if (browser.config.languageRegion === 'da-dk') expectedDateFormat = '12.8.2016 21:32'; //Danish
-    else if (browser.config.languageRegion === 'es-es') expectedDateFormat = '12/8/2016 21:32'; //Spanish
-    else if (browser.config.languageRegion === 'fr-fr') expectedDateFormat = '12/8/2016 21:32'; //French
-    else if (browser.config.languageRegion === 'it-it') expectedDateFormat = '12/8/2016 21:32'; //Italian
-    else if (browser.config.languageRegion === 'pt-br') expectedDateFormat = '12/8/2016 21:32'; //Portugese
-    else if (browser.config.languageRegion === 'sv-se') expectedDateFormat = '2016-8-12 21:32'; //Swedish
-    else if (browser.config.languageRegion === 'nl-nl') expectedDateFormat = '2016-8-12 21:32'; //Dutch
-    else if (browser.config.languageRegion === 'zh-cn') expectedDateFormat = '2016/8/12 21:32'; //Chinese Simplified
-    else if (browser.config.languageRegion === 'zh-tw') expectedDateFormat = '2016/8/12 21:32'; //Chinese Traditional
-    else if (browser.config.languageRegion === 'ja-jp') expectedDateFormat = '2016/08/12 21:32'; //Japanese
-    else if (browser.config.languageRegion === 'ko-kr') expectedDateFormat = '2016.8.12 오후 9:32'; //Korean
+    const REGION_TO_DATE = {
+      'de-de': '12.8.2016 21:32', //German
+      'da-dk': '12.8.2016 21:32', //Danish
+      'es-es': '12/8/2016 21:32', //Spanish
+      'fr-fr': '12/8/2016 21:32', //French
+      'it-it': '12/8/2016 21:32', //Italian
+      'pt-br': '12/8/2016 21:32', //Portugese
+      'sv-se': '2016-8-12 21:32', //Swedish
+      'nl-nl': '2016-8-12 21:32', //Dutch
+      'zh-cn': '2016/8/12 21:32', //Chinese Simplified
+      'zh-tw': '2016/8/12 21:32', //Chinese Traditional
+      'ja-jp': '2016/08/12 21:32', //Japanese
+      'ko-kr': '2016.8.12 오후 9:32', //Korean
+    };
+    const selectDateFormatByLanguageRegion = (languageRegion) => {
+      if (languageRegion in REGION_TO_DATE) {
+        return REGION_TO_DATE[languageRegion];
+      }
+      return '8/12/2016 9:32 PM'; //Defaul and fallback - English US
+    };
 
     //Get date for the object 'Eastern Region Average Revenue per Customer' on MicroStrategy Tutorial
     //Expected date format '8/12/2016 9:32 PM' on en-us locale
     PluginPopup.searchForObject('Eastern Region Average Revenue per Customer');
     browser.pause(999); // waiting for search to filter the ObjectTable
     PluginPopup.selectFirstObject();
-    expect(PluginPopup.getFirstRowDate() === expectedDateFormat).toBe(true);
+    expect(PluginPopup.getFirstRowDate() === selectDateFormatByLanguageRegion(browser.config.languageRegion)).toBe(true);
   });
 });
