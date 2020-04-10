95d2d9a2f1837d8be22fef4a34e9b4ed0a995528
Ryszard Raszplewicz <rraszplewicz@microstrategy.com>
2020-04-10 11:48:27 +0200

    office-actions tests fixes.

--- production/src/_tests_/office/office-actions.test.js
+++ production/src/_tests_/office/office-actions.test.js
@@ -1,22 +1,30 @@
-
 import * as actions from '../../redux-reducer/office-reducer/office-actions';
 import { officeProperties } from '../../redux-reducer/office-reducer/office-properties';
+import officeStoreHelper from '../../office/store/office-store-helper';
 
 describe('Office Actions', () => {
   it('should dispatch proper toggleStoreSecuredFlag action', () => {
     // given
+    const setFileSecuredFlagMock = jest.spyOn(officeStoreHelper, 'setFileSecuredFlag').mockImplementation();
     const listener = jest.fn();
+
     // when
     actions.toggleSecuredFlag(true)(listener);
+
     // then
     expect(listener).toHaveBeenCalledWith({ type: officeProperties.actions.toggleSecuredFlag, isSecured: true });
+
+    expect(setFileSecuredFlagMock).toBeCalledTimes(1);
+    expect(setFileSecuredFlagMock).toBeCalledWith(true);
   });
 
   it('should dispatch proper toggleIsSettingsFlag action', () => {
     // given
     const listener = jest.fn();
+
     // when
     actions.toggleIsSettingsFlag(true)(listener);
+
     // then
     expect(listener).toHaveBeenCalledWith({ type: officeProperties.actions.toggleIsSettingsFlag, isSettings: true });
   });
@@ -24,8 +32,10 @@ describe('Office Actions', () => {
   it('should dispatch proper toggleIsConfirmFlag action', () => {
     // given
     const listener = jest.fn();
+
     // when
     actions.toggleIsConfirmFlag(true)(listener);
+
     // then
     expect(listener).toHaveBeenCalledWith({ type: officeProperties.actions.toggleIsConfirmFlag, isConfirm: true });
   });
@@ -33,23 +43,31 @@ describe('Office Actions', () => {
   it('should dispatch proper toggleRenderSettingsFlag action', () => {
     // given
     const listener = jest.fn();
+
     // when
     actions.toggleRenderSettingsFlag()(listener);
+
     // then
     expect(listener).toHaveBeenCalledWith({ type: officeProperties.actions.toggleRenderSettingsFlag });
   });
 
   it('should dispatch proper toggleIsClearDataFailedFlag action', () => {
     // given
+    const setIsClearDataFailedMock = jest.spyOn(officeStoreHelper, 'setIsClearDataFailed').mockImplementation();
     const listener = jest.fn();
+
     // when
     actions.toggleIsClearDataFailedFlag(true)(listener);
+
     // then
     expect(listener).toHaveBeenCalledWith(
       {
         type: officeProperties.actions.toggleIsClearDataFailedFlag,
-        IsClearDataFailed: true
+        isClearDataFailed: true
       }
     );
+
+    expect(setIsClearDataFailedMock).toBeCalledTimes(1);
+    expect(setIsClearDataFailedMock).toBeCalledWith(true);
   });
 });
