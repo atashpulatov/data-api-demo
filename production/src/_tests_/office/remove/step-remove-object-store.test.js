e2c39a673d1c558e5300c891587f1cb6406c6fa2
Ryszard Raszplewicz <rraszplewicz@microstrategy.com>
2020-04-14 12:38:47 +0200

    step-remove-object-store unit tests.

--- production/src/_tests_/office/remove/step-remove-object-store.test.js
+++ production/src/_tests_/office/remove/step-remove-object-store.test.js
@@ -1,19 +1,39 @@
 import operationStepDispatcher from '../../../operation/operation-step-dispatcher';
 import stepRemoveObjectStore from '../../../office/remove/step-remove-object-store';
 import officeStoreObject from '../../../office/store/office-store-object';
+import operationErrorHandler from '../../../operation/operation-error-handler';
 
 describe('StepRemoveObjectStore', () => {
   afterEach(() => {
     jest.restoreAllMocks();
   });
 
+  it('stepRemoveObjectStore should handle exception', async () => {
+    // given
+    console.error = jest.fn();
+
+    jest.spyOn(officeStoreObject, 'removeObjectFromStore').mockImplementation(() => {
+      throw new Error('errorTest');
+    });
+
+    jest.spyOn(operationErrorHandler, 'handleOperationError').mockImplementation();
+
+    // when
+    await stepRemoveObjectStore.removeObjectStore({}, {});
+
+    // then
+    expect(console.error).toBeCalledTimes(1);
+    expect(console.error).toBeCalledWith(new Error('errorTest'));
+
+    expect(operationErrorHandler.handleOperationError).toBeCalledTimes(1);
+    expect(operationErrorHandler.handleOperationError).toBeCalledWith({}, {}, new Error('errorTest'));
+  });
+
   it('stepRemoveObjectStore should work as expected', async () => {
     // given
-    const removeObjectFromStoreMock = jest.spyOn(officeStoreObject, 'removeObjectFromStore').mockImplementation();
+    jest.spyOn(officeStoreObject, 'removeObjectFromStore').mockImplementation();
 
-    const completeRemoveObjectStoreMock = jest.spyOn(
-      operationStepDispatcher, 'completeRemoveObjectStore'
-    ).mockImplementation();
+    jest.spyOn(operationStepDispatcher, 'completeRemoveObjectStore').mockImplementation();
 
     const objectData = {
       objectWorkingId: 'objectWorkingIdTest',
@@ -24,10 +44,10 @@ describe('StepRemoveObjectStore', () => {
     await stepRemoveObjectStore.removeObjectStore(objectData, {});
 
     // then
-    expect(removeObjectFromStoreMock).toBeCalledTimes(1);
-    expect(removeObjectFromStoreMock).toBeCalledWith('objectWorkingIdTest');
+    expect(officeStoreObject.removeObjectFromStore).toBeCalledTimes(1);
+    expect(officeStoreObject.removeObjectFromStore).toBeCalledWith('objectWorkingIdTest');
 
-    expect(completeRemoveObjectStoreMock).toBeCalledTimes(1);
-    expect(completeRemoveObjectStoreMock).toBeCalledWith('objectWorkingIdTest');
+    expect(operationStepDispatcher.completeRemoveObjectStore).toBeCalledTimes(1);
+    expect(operationStepDispatcher.completeRemoveObjectStore).toBeCalledWith('objectWorkingIdTest');
   });
 });
