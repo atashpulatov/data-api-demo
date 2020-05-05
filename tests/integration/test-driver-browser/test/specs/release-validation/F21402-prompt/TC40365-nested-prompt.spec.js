ef65e2deb12774c92b25a5d258b38a70ceca3528
mgraczyk <mgraczyk@microstrategy.com>
2020-05-05 11:40:34 +0200

    Refactored TC40359 and TC40365

---
+++ tests/integration/test-driver-browser/test/specs/release-validation/F21402-prompt/TC40365-nested-prompt.spec.js
@@ -0,0 +1,29 @@
+import OfficeLogin from '../../../helpers/office/office.login';
+import PluginRightPanel from '../../../helpers/plugin/plugin.right-panel';
+import PluginPopup from '../../../helpers/plugin/plugin.popup';
+import { waitForNotification } from '../../../helpers/utils/wait-helper';
+import { changeBrowserTab } from '../../../helpers/utils/iframe-helper';
+import { dictionary } from '../../../constants/dictionaries/dictionary';
+import { objectsList } from '../../../constants/objects-list';
+import { rightPanelSelectors } from '../../../constants/selectors/plugin.right-panel-selectors';
+
+describe('F21402 - Handle Prompted Object', () => {
+  beforeEach(() => {
+    // OfficeLogin.openExcelAndLoginToPlugin();
+  });
+
+  afterEach(() => {
+    // browser.closeWindow();
+    // changeBrowserTab(0);
+  });
+
+  it('[Import prompt] Importing prompted reports functionality, for nested prompts without Prepare Data', () => {
+    // should import a report
+    PluginRightPanel.clickImportDataButton();
+    PluginPopup.switchLibrary(false);
+    console.log('Should import nested prompt');
+    PluginPopup.importPromptDefaultNested(objectsList.reports.nestedPrompt);
+    waitForNotification();
+    expect($(rightPanelSelectors.notificationPopUp).getAttribute('textContent')).toContain(dictionary.en.importSuccess);
+  });
+});
