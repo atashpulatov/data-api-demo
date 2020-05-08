931fb0b15b4f45f319d7e587453cfb86d9f190dd
jkowalczyk <jkowalczyk@microstrategy.com>
2020-05-08 14:22:35 +0200

    refactored spec

--- tests/integration/test-driver-browser/test/specs/release-validation/F21409-refresh-all/TC41093-right-panel-refresh-all.spec.js
+++ tests/integration/test-driver-browser/test/specs/release-validation/F21409-refresh-all/TC41093-right-panel-refresh-all.spec.js
@@ -16,29 +16,53 @@ describe('F21409 - Refresh All - ', () => {
 
 
   it('[TC41094] - Refreshing at least 10 already imported objects with very long names', () => {
-    PluginPopUp.importObjectToCellAndAssertSuccess('A1', objectsList.reports.reportXML, 'First object should be imported');
-    PluginPopUp.importObjectToCellAndAssertSuccess('I1', objectsList.reports.reportXML, 'Second object should be imported', true);
-    PluginPopUp.importObjectToCellAndAssertSuccess('A21', objectsList.datasets.datasetSQL, 'Fourth object should be imported', true);
-    PluginPopUp.importObjectToCellAndAssertSuccess('G21', objectsList.datasets.datasetSQL, 'Fifth object should be imported', true);
-    PluginPopUp.importObjectToCellAndAssertSuccess('M21', objectsList.datasets.datasetSQL, 'Sixth object should be imported', true);
-    PluginPopUp.importObjectToCellAndAssertSuccess('A41', objectsList.reports.reportWithLongName, 'Seventh object should be imported', true);
-    PluginPopUp.importObjectToCellAndAssertSuccess('F41', objectsList.reports.longReportWithInvalidCharacters.sourceName, 'Eighth object should be imported', true);
-    PluginPopUp.importObjectToCellAndAssertSuccess('L41', objectsList.reports.longReportWithInvalidCharacters.sourceName, 'Ninth object should be imported', true);
-    PluginPopUp.importObjectToCellAndAssertSuccess('S41', objectsList.reports.reportWithLongName, 'Tenth object should be imported', true);
+    const objects = [
+      {
+        cellValue: 'A1', object: objectsList.reports.reportXML, message: 'First object should be imported', add: false
+      },
+      {
+        cellValue: 'I1', object: objectsList.reports.reportXML, message: 'Second object should be imported', add: true
+      },
+      {
+        cellValue: 'Q1', object: objectsList.reports.reportXML, message: 'Third object should be imported', add: true
+      },
+      {
+        cellValue: 'A21', object: objectsList.datasets.datasetSQL, message: 'Fourth object should be imported', add: true
+      },
+      {
+        cellValue: 'G21', object: objectsList.datasets.datasetSQL, message: 'Fifth object should be imported', add: true
+      },
+      {
+        cellValue: 'M21', object: objectsList.datasets.datasetSQL, message: 'Sixth object should be imported', add: true
+      },
+      {
+        cellValue: 'A41', object: objectsList.reports.reportWithLongName, message: 'Seventh object should be imported', add: true
+      },
+      {
+        cellValue: 'F41', object: objectsList.reports.longReportWithInvalidCharacters.sourceName, message: 'Eighth object should be imported', add: true
+      },
+      {
+        cellValue: 'L41', object: objectsList.reports.longReportWithInvalidCharacters.sourceName, message: 'Ninth object should be imported', add: true
+      },
+      {
+        cellValue: 'S41', object: objectsList.reports.reportWithLongName, message: 'Tenth object should be imported', add: true
+      }
+    ];
 
-    console.log('Should refresh all');
+    objects.forEach((obj) => {
+      const {
+        cellValue, object, message, add
+      } = obj;
+      PluginPopUp.importObjectToCellAndAssertSuccess(cellValue, object, message, add);
+    });
+
+
     PluginRightPanel.refreshAll();
 
     console.log('Should close notifications and assert the "Refresh complete" message');
-    PluginRightPanel.waitAndCloseNotification(dictionary.en.reportRefreshed);
-    PluginRightPanel.waitAndCloseNotification(dictionary.en.reportRefreshed);
-    PluginRightPanel.waitAndCloseNotification(dictionary.en.reportRefreshed);
-    PluginRightPanel.waitAndCloseNotification(dictionary.en.reportRefreshed);
-    PluginRightPanel.waitAndCloseNotification(dictionary.en.reportRefreshed);
-    PluginRightPanel.waitAndCloseNotification(dictionary.en.reportRefreshed);
-    PluginRightPanel.waitAndCloseNotification(dictionary.en.reportRefreshed);
-    PluginRightPanel.waitAndCloseNotification(dictionary.en.reportRefreshed);
-    PluginRightPanel.waitAndCloseNotification(dictionary.en.reportRefreshed);
-    PluginRightPanel.waitAndCloseNotification(dictionary.en.reportRefreshed);
+    for (let i = 0; i < 10; i++) {
+      const { reportRefreshed } = dictionary.en;
+      PluginRightPanel.waitAndCloseNotification(reportRefreshed);
+    }
   });
 });
