3a39f351eb3f095744cf1847f163e0f939f8aaa1
eozturk <eozturk@microstrategy.com>
2020-05-11 12:30:22 +0200

    TC64702;-Add spec to acceptance

--- tests/integration/test-driver-browser/wdio.conf.js
+++ tests/integration/test-driver-browser/wdio.conf.js
@@ -93,6 +93,7 @@ exports.config = {
       './test/specs/release-validation/F25931-duplicate-object/TC64626-duplicate-all-types.spec.js',
       './test/specs/release-validation/F25931-duplicate-object/TC64700-Duplicating-and-editing-all-types-of-objects.spec.js',
       './test/specs/release-validation/F25931-duplicate-object/TC65014-side-panel-functionality-for-duplicated-object.spec.js',
+      './test/specs/release-validation/F25931-duplicate-object/TC64702-duplicate-same-object.spec.js',
     ],
     'F12910-import-dataset': [
       './test/specs/release-validation/F12910-import-dataset/TC34506-import-not-supported-cube.spec.js',
