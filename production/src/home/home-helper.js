b239b452548e0c070dfc75b9aa13e68eaef4a89d
Otreba, Dominik <dotreba@microstrategy.com>
2020-05-13 13:55:50 +0200

    added return to jsdocs

--- production/src/home/home-helper.js
+++ production/src/home/home-helper.js
@@ -78,6 +78,7 @@ export class HomeHelper {
    * Checking only for webkit or safari is not enought:
    * https://security.stackexchange.com/questions/126407/why-does-chrome-send-four-browsers-in-the-user-agent-header
    *
+   * @returns {boolean} true if user agent is instance of mac desktop or safari
    */
   isMacAndSafariBased = () => {
     const userAgent = navigator.userAgent.toLowerCase();
