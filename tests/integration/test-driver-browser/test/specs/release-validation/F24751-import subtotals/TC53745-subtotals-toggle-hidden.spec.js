78bd3bff793442e4e8aac64fd480de903db98185
Sonia Lukaszewicz <slukaszewicz@microstrategy.com>
2020-05-14 09:58:54 +0200

    testing

--- tests/integration/test-driver-browser/test/specs/release-validation/F24751-import subtotals/TC53745-subtotals-toggle-hidden.spec.js
+++ tests/integration/test-driver-browser/test/specs/release-validation/F24751-import subtotals/TC53745-subtotals-toggle-hidden.spec.js
@@ -7,63 +7,69 @@ import { popupSelectors } from '../../../constants/selectors/popup-selectors';
 
 describe('[F24751] Import report with or without subtotals', () => {
   beforeEach(() => {
-    officeLogin.openExcelAndLoginToPlugin();
+    // officeLogin.openExcelAndLoginToPlugin();
   });
 
   afterEach(() => {
-    browser.closeWindow();
-    const handles = browser.getWindowHandles();
-    browser.switchToWindow(handles[0]);
+    // browser.closeWindow();
+    // const handles = browser.getWindowHandles();
+    // browser.switchToWindow(handles[0]);
   });
 
-  it('[TC53745] Checking if subtotals toggle is hidden while importing report/dataset without subtotals', () => {
-    // should check if the toggle is hidden for basic report without subtotals
-    const basicReport = objectsList.reports.withoutSubtotals.basicReport;
-    PluginRightPanel.clickImportDataButton();
-    PluginPopup.openPrepareData(basicReport, false);
-    expect($(popupSelectors.subtotalToggler).isExisting(false));
-    PluginPopup.clickBack();
+  it('[TC53745] Checking if subtotals toggle is hidden while importing objects without subtotals and prompted report with attributes with and without subtotals', () => {
+    const { basicReport } = objectsList.reports.withoutSubtotals;
+    const { reportBasedOnIntelligentCube } = objectsList.reports.withoutSubtotals;
+    const { promptedReport } = objectsList.reports.withoutSubtotals;
+    const { reportWithCrosstabs } = objectsList.reports.withoutSubtotals;
+    const { promptedReportWithCrosstabs } = objectsList.reports.withoutSubtotals;
+    const { dataset } = objectsList.reports.withoutSubtotals;
+    const objects = [basicReport, reportBasedOnIntelligentCube, promptedReport, reportWithCrosstabs, promptedReportWithCrosstabs, promptedReportWithCrosstabs, dataset];
 
-    // should check if the toggle is hidden for report without subtotals based on intelligent cube
-    const reportBasedOnIntelligentCube = objectsList.reports.withoutSubtotals.reportBasedOnIntelligentCube;
-    PluginPopup.openPrepareData(reportBasedOnIntelligentCube, false);
-    expect($(popupSelectors.subtotalToggler).isExisting(false));
-    PluginPopup.clickBack();
 
-    // should check if the toggle is hidden for prompted report without subtotals
-    const promptedReportWithoutSubtotals =
-      objectsList.reports.withoutSubtotals.promptedReport;
-    PluginPopup.preparePrompt(promptedReportWithoutSubtotals, false);
-    PluginPopup.clickRun();
-    console.log('swich to prepare data');
-    switchToDialogFrame();
-    expect($(popupSelectors.subtotalToggler).isExisting(false));
-    PluginPopup.clickBack();
 
-    //should check if the toggle is hidden for  report with crosstabs without subtotals
-    const reportWithCrosstabsWithoutSubtotals =
-      objectsList.reports.withoutSubtotals.reportWithCrosstabs;
-    PluginPopup.openPrepareData(reportWithCrosstabsWithoutSubtotals, false);
-    expect($(popupSelectors.subtotalToggler).isExisting(false));
-    PluginPopup.clickBack();
+    objects.forEach((obj) => {
+      
+    })
 
-    // should check if the toggle is hidden for prompted report with crosstabs and without subtotals
-    const promptedReportWithCrosstabsWithoutSubtotals =
-      objectsList.reports.withoutSubtotals.promptedReportWithCrosstabs;
-    PluginPopup.preparePrompt(
-      promptedReportWithCrosstabsWithoutSubtotals,
-      false
-    );
-    PluginPopup.clickRun();
-    console.log('swich to prepare data');
-    switchToDialogFrame();
-    expect($(popupSelectors.subtotalToggler).isExisting(false));
-    PluginPopup.clickBack();
+    // // should check if the toggle is hidden for basic report without subtotals
+    // PluginRightPanel.clickImportDataButton();
+    // PluginPopup.openPrepareData(basicReport, false);
+    // expect($(popupSelectors.subtotalToggler).isExisting(false));
+    // PluginPopup.clickBack();
 
-    // should  check if the toggle is hidden for dataset
-    const dataset = objectsList.reports.withoutSubtotals.dataset;
-    PluginPopup.openPrepareData(dataset, false);
-    expect($(popupSelectors.subtotalToggler).isExisting(false));
-    PluginPopup.clickBack();
+    // // should check if the toggle is hidden for report without subtotals based on intelligent cube
+    // PluginPopup.openPrepareData(reportBasedOnIntelligentCube, false);
+    // expect($(popupSelectors.subtotalToggler).isExisting(false));
+    // PluginPopup.clickBack();
+
+    // // should check if the toggle is hidden for prompted report without subtotals
+
+    // PluginPopup.preparePrompt(promptedReport, false);
+    // PluginPopup.clickRun();
+    // console.log('swich to prepare data');
+    // switchToDialogFrame();
+    // expect($(popupSelectors.subtotalToggler).isExisting(false));
+    // PluginPopup.clickBack();
+
+    // // should check if the toggle is hidden for  report with crosstabs without subtotals
+
+    // PluginPopup.openPrepareData(reportWithCrosstabs, false);
+    // expect($(popupSelectors.subtotalToggler).isExisting(false));
+    // PluginPopup.clickBack();
+
+    // // should check if the toggle is hidden for prompted report with crosstabs and without subtotals
+
+    // PluginPopup.preparePrompt(promptedReportWithCrosstabs, false);
+    // PluginPopup.clickRun();
+    // console.log('swich to prepare data');
+    // switchToDialogFrame();
+    // expect($(popupSelectors.subtotalToggler).isExisting(false));
+    // PluginPopup.clickBack();
+
+    // // should  check if the toggle is hidden for dataset
+    
+    // PluginPopup.openPrepareData(dataset, false);
+    // expect($(popupSelectors.subtotalToggler).isExisting(false));
+    // PluginPopup.clickBack();
   });
 });
