1262a6ab231a8b37dcd99bc01ab710271713ba51
msabatura <msabatura@microstrategy.com>
2020-05-18 10:01:56 +0200

    US278097; remove unused imports

--- tests/integration/test-driver-browser/test/specs/release-validation/F25946-details-panel/TC59675-exanding-many-rows.spec.js
+++ tests/integration/test-driver-browser/test/specs/release-validation/F25946-details-panel/TC59675-exanding-many-rows.spec.js
@@ -2,11 +2,6 @@ import OfficeLogin from '../../../helpers/office/office.login';
 import PluginRightPanel from '../../../helpers/plugin/plugin.right-panel';
 import PluginPopup from '../../../helpers/plugin/plugin.popup';
 import { changeBrowserTab, switchToDialogFrame } from '../../../helpers/utils/iframe-helper';
-import { objectsList } from '../../../constants/objects-list';
-import { waitForNotification } from '../../../helpers/utils/wait-helper';
-import { rightPanelSelectors } from '../../../constants/selectors/plugin.right-panel-selectors';
-import { dictionary } from '../../../constants/dictionaries/dictionary';
-import { pressEnter, pressDownArrow } from '../../../helpers/utils/keyboard-actions';
 
 describe('F25946 - details panel', () => {
   beforeEach(() => {
