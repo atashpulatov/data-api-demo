06f485bc91e5a2b9d5711dfe6d48b64f7443bde3
jkowalczyk <jkowalczyk@microstrategy.com>
2020-05-20 17:37:09 +0200

    Added strings file

---
+++ tests/integration/test-driver-browser/rally/strings.js
@@ -0,0 +1,17 @@
+module.exports = {
+  osCMD: {
+    mac13: 'mac13',
+    mac14: 'mac14',
+    mac15: 'mac15',
+    win10: 'win10',
+    win19: 'win19',
+  },
+  OS: {
+    macOS13: 'Mac OS 10.13 High Sierra',
+    macOS14: 'Mac OS 10.14 Mojave',
+    macOS15: 'Mac OS 10.15 Catalina',
+    msWin10: 'Microsoft Windows 10',
+    msWin19: 'Microsoft Windows Server 2019',
+  }
+
+};
