774eea4b90c415ae8e6a9ea047716604128f58b6
Dominik Maliszewski <dmaliszewski@microstrategy.com>
2020-05-21 10:56:32 +0200

    Uncommented steps

--- tests/integration/test-driver-browser/test/specs/release-validation/AQDT-Tests/TC65783-E2E-import-and-edit-of-Aqueduct-dossiers.spec.js
+++ tests/integration/test-driver-browser/test/specs/release-validation/AQDT-Tests/TC65783-E2E-import-and-edit-of-Aqueduct-dossiers.spec.js
@@ -20,54 +20,56 @@ describe('IMPORT diferent types of vizualizations', () => {
 
   // Create test for each visType defined in visualizations
   it(`[TC61046] E2E import and edit of Aqueduct Dossiers`, () => {
-    // switchToExcelFrame();
-    // OfficeWorksheet.selectCell('A1');
-    // PluginRightPanel.clickImportDataButton();
-    // PluginPopup.switchLibrary();
-    // PluginPopup.clickFilterButton();
-    // PluginPopup.tickFilterCheckBox('Certified Status', 'Certified');
-    // PluginPopup.searchForObject(objectList.dossiers.aqueductTECQA.name);
-    // switchToDialogFrame();
-    // PluginPopup.selectFirstObjectWithoutSearch();
-    // PluginPopup.clickImport();
+    switchToExcelFrame();
+    OfficeWorksheet.selectCell('A1');
+    PluginRightPanel.clickImportDataButton();
+    PluginPopup.switchLibrary();
+    PluginPopup.clickFilterButton();
+    PluginPopup.tickFilterCheckBox('Certified Status', 'Certified');
+    PluginPopup.searchForObject(objectList.dossiers.aqueductTECQA.name);
+    switchToDialogFrame();
+    PluginPopup.selectFirstObjectWithoutSearch();
+    PluginPopup.clickImport();
 
-    // console.log('Importing TEC.QA');
-    // browser.pause(5000);
-    // switchToPromptFrame();
-    // PluginPopup.goToDossierPageOrChapter(7);
-    // PluginPopup.selectAndImportVizualiation(objectsList.dossiers.aqueductTECQA.visualizations.automationProgress);
-    // waitForNotification();
-    // PluginRightPanel.closeNotificationOnHover();
-    // browser.pause(6000);
+    console.log('Importing TEC.QA');
+    browser.pause(5000);
+    switchToPromptFrame();
+    PluginPopup.goToDossierPageOrChapter(7);
+    PluginPopup.selectAndImportVizualiation(objectsList.dossiers.aqueductTECQA.visualizations.automationProgress);
+    waitForNotification();
+    PluginRightPanel.closeNotificationOnHover();
+    browser.pause(6000);
 
-    // console.log('Editing imported TEC.QA visualization')
-    // PluginRightPanel.editObject(1);
-    // browser.pause(2000);
-    // PluginPopup.selectAndImportVizualiation(objectsList.dossiers.aqueductTECQA.visualizations.testSets);
-    // waitForNotification();
-    // PluginRightPanel.closeNotificationOnHover();
+    console.log('Editing imported TEC.QA visualization');
+    PluginRightPanel.editObject(1);
+    browser.pause(2000);
+    PluginPopup.selectAndImportVizualiation(objectsList.dossiers.aqueductTECQA.visualizations.testSets);
+    waitForNotification();
+    PluginRightPanel.closeNotificationOnHover();
 
-    // console.log('Importing TEC.PD visualization');
-    // switchToExcelFrame();
-    // OfficeWorksheet.selectCell('I1');
-    // PluginRightPanel.clickAddDataButton();
-    // switchToDialogFrame();
-    // PluginPopup.searchForObject(objectsList.dossiers.aqueductTECPD.name);
-    // PluginPopup.selectFirstObjectWithoutSearch();
-    // PluginPopup.clickImport();
-    // browser.pause(5000);
-    // switchToPromptFrame();
-    // PluginPopup.goToDossierPageOrChapter(8);
-    // PluginPopup.selectAndImportVizualiation(objectsList.dossiers.aqueductTECPD.visualization);
-    // waitForNotification();
-    // PluginRightPanel.closeNotificationOnHover();
+    console.log('Importing TEC.PD visualization');
+    switchToExcelFrame();
+    OfficeWorksheet.selectCell('I1');
+    PluginRightPanel.clickAddDataButton();
+    switchToDialogFrame();
+    PluginPopup.searchForObject(objectsList.dossiers.aqueductTECPD.name);
+    PluginPopup.selectFirstObjectWithoutSearch();
+    PluginPopup.clickImport();
+    browser.pause(5000);
+    switchToPromptFrame();
+    PluginPopup.goToDossierPageOrChapter(8);
+    PluginPopup.selectAndImportVizualiation(objectsList.dossiers.aqueductTECPD.visualization);
+    waitForNotification();
+    PluginRightPanel.closeNotificationOnHover();
+
+    console.log('Clearing data');
     switchToPluginFrame();
     PluginRightPanel.clickSettings();
     PluginRightPanel.clearData();
     PluginRightPanel.viewDataBtn();
-    // console.log('Loggin out');
-    // switchToPluginFrame();
-    // PluginRightPanel.logout();
+    console.log('Loggin out');
+    switchToPluginFrame();
+    PluginRightPanel.logout();
     browser.pause(3000);
   });
 });
