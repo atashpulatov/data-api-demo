a02c50ef99396204a51212977fc836c14f4f7a05
Dominik Maliszewski <dmaliszewski@microstrategy.com>
2020-05-21 15:26:25 +0200

    Added locale users and adjusted helpers

--- tests/integration/test-driver-browser/test/config.js
+++ tests/integration/test-driver-browser/test/config.js
@@ -2,13 +2,67 @@ import { parse } from '@babel/core';
 
 const settings = {
   officeOnline: {
-    username: 'test3@mstrtesting.onmicrosoft.com',
-    password: 'FordFocus2019',
     url: 'https://www.office.com/launch/excel?auth=2',
+    en: {
+      username: 'test3@mstrtesting.onmicrosoft.com',
+      password: 'FordFocus2019',
+    },
+    sp: {
+      username: 'testes@mstrtesting.onmicrosoft.com',
+      password: 'Testing1212'
+    },
+    fr: {
+      username: 'testfr@mstrtesting.onmicrosoft.com',
+      password: 'Testing1212'
+    },
+    it: {
+      username: 'testit@mstrtesting.onmicrosoft.com',
+      password: 'Testing1212'
+    },
+    cn: {
+      username: 'testcn@mstrtesting.onmicrosoft.com',
+      password: 'Testing1212'
+    },
+    cn2: {
+      username: 'testcn2@mstrtesting.onmicrosoft.com',
+      password: 'Testing1212'
+    },
+    de: {
+      username: 'testde@mstrtesting.onmicrosoft.com',
+      password: 'Testing1212'
+    },
+    nl: {
+      username: 'testnl@mstrtesting.onmicrosoft.com',
+      password: 'Testing1212'
+    },
+    dk: {
+      username: 'testdk@mstrtesting.onmicrosoft.com',
+      password: 'Testing1212'
+    },
+    se: {
+      username: 'testse@mstrtesting.onmicrosoft.com',
+      password: 'Testing1212'
+    },
+    pt: {
+      username: 'testpt@mstrtesting.onmicrosoft.com',
+      password: 'Testing1212'
+    },
+    jp: {
+      username: 'testjp@mstrtesting.onmicrosoft.com',
+      password: 'Testing1212'
+    },
+    kr: {
+      username: 'testkr@mstrtesting.onmicrosoft.com',
+      password: 'Testing1212'
+    },
+    pl: {
+      username: 'testpl@mstrtesting.onmicrosoft.com',
+      password: 'Testing1212'
+    },
   },
   args: parseArgs(),
   env: {
-    hostname: getEnvironment(),
+    // hostname: getEnvironment(),
     username: 'a',
     password: '',
     chineseSimplifiedUser: 'User_SChinese',
@@ -16,9 +70,17 @@ const settings = {
   }
 };
 
+/**
+ * Parses arguments passed to terminal
+ * syntax of passing arguments: argument="value"
+ * @return {Object} arguments
+ */
 function parseArgs() {
   const { argv } = process;
-  const args = {};
+  const args = { // passing default values when user doesn't provide
+    env: '127.0.0.1',
+    lang: 'en',
+  };
   argv.forEach(argument => {
     if (argument.includes('=')) {
       const [key, value] = argument.split('=');
@@ -26,20 +88,6 @@ function parseArgs() {
     }
   });
   return args;
-}
-
-/**
- * It checks if last parameter contains "env-#######" if not it returns localhost
- * "npm test env-000000"
- *
- * @returns {string}
- */
-function getEnvironment() {
-  const env = process.argv[process.argv.length - 1];
-  if (env.includes('env-')) {
-    return env;
-  }
-  return '127.0.0.1';
 }
 
 export default settings;
