5c9ef8225eaaf244fd79fa4f26552fa6d5c68ae1
jkowalczyk <jkowalczyk@microstrategy.com>
2020-05-22 13:06:39 +0200

    Corrected async await

--- tests/integration/test-driver-browser/rally/helpers.js
+++ tests/integration/test-driver-browser/rally/helpers.js
@@ -1,13 +1,14 @@
+/* eslint-disable no-underscore-dangle */
 const getDataFromRally = require('./getDataFromRally');
 const rallyConfig = require('./rallyconfig');
 
 /**
- * Returns Test Set endpoint using Formatted Test Set ID (e.g. 'TS23156')
- *
- * @param {String} testSetId Formatted Test Set ID (e.g. 'TS23156')
- * @returns {String} URL to the Test Set endpoint
- */
-async function getTestSet(testSetId) {
+* Returns Test Set endpoint using Formatted Test Set ID (e.g. 'TS23156')
+*
+* @param {String} testSetId Formatted Test Set ID (e.g. 'TS23156')
+* @returns {String} URL to the Test Set endpoint
+*/
+function getTestSet(testSetId) {
   const formattedIDUrl = `${rallyConfig.apiUrl}/testset/?query=(FormattedID%20%3D%20"${testSetId}")`;
   return getDataFromRally(formattedIDUrl)
     .then(({ QueryResult }) => QueryResult.Results[0]._ref)
@@ -15,25 +16,24 @@ async function getTestSet(testSetId) {
 }
 
 /**
- * Returns Test Case owner endpoint using Formatted Test Case ID (e.g. 'TC12567')
- *
- * @param {String} testCaseId Formatted Test Case ID (e.g. 'TC12567')
- * @returns {String} URL to the Test Case endpoint
- */
+* Returns Test Case owner endpoint using Formatted Test Case ID (e.g. 'TC12567')
+*
+* @param {String} testCaseId Formatted Test Case ID (e.g. 'TC12567')
+* @returns {String} URL to the Test Case endpoint
+*/
 async function getOwner(testCaseId) {
-  const tcUrl = await getRallyTCUrl(testCaseId);
-  return getDataFromRally(tcUrl)
-    .then(({ TestCase }) => TestCase.Owner._ref)
+  return getRallyTCUrl(testCaseId)
+    .then(tcUrl => getDataFromRally(tcUrl).then(({ TestCase }) => TestCase.Owner._ref))
     .catch(() => { throw Error(`Couldn't get ${testCaseId} owner`); });
 }
 
 /**
- * Returns Tester endpoint using tester email (e.g. 'stasiu@microstrategy.com')
- *
- * @param {String} testerEmail Tester email (e.g. 'stasiu@microstrategy.com')
- * @returns {String} URL to tester endpoint
- */
-async function getTesterUrl(testerEmail) {
+* Returns Tester endpoint using tester email (e.g. 'stasiu@microstrategy.com')
+*
+* @param {String} testerEmail Tester email (e.g. 'stasiu@microstrategy.com')
+* @returns {String} URL to tester endpoint
+*/
+function getTesterUrl(testerEmail) {
   const emailUrl = `${rallyConfig.apiUrl}/user/?query=(EmailAddress%20%3D%20"${testerEmail}")`;
   return getDataFromRally(emailUrl)
     .then(({ QueryResult }) => QueryResult.Results[0]._ref)
@@ -41,12 +41,12 @@ async function getTesterUrl(testerEmail) {
 }
 
 /**
- * Returns Test Case endpoint using Formatted Test Case ID (e.g. 'TC12567')
- *
- * @param {String} formattedID Formatted Test Case ID (e.g. 'TC12567')
- * @returns {String} URL to Test Case endpoint
- */
-async function getRallyTCUrl(formattedID) {
+* Returns Test Case endpoint using Formatted Test Case ID (e.g. 'TC12567')
+*
+* @param {String} formattedID Formatted Test Case ID (e.g. 'TC12567')
+* @returns {String} URL to Test Case endpoint
+*/
+function getRallyTCUrl(formattedID) {
   const formattedIDUrl = `${rallyConfig.apiUrl}/testcase/?query=(FormattedID%20%3D%20%22${formattedID}%22)`;
   return getDataFromRally(formattedIDUrl)
     .then(({ QueryResult }) => QueryResult.Results[0]._ref)
@@ -54,18 +54,16 @@ async function getRallyTCUrl(formattedID) {
 }
 
 /**
- * Returns Test Case details from Rally using Formatted Test Case ID (e.g. 'TC12567')
- *
- * @param {String} formattedID Formatted Test Case ID (e.g. 'TC12567')
- * @returns {Object} Object contatining Test Case details
- */
+* Returns Test Case details from Rally using Formatted Test Case ID (e.g. 'TC12567')
+*
+* @param {String} formattedID Formatted Test Case ID (e.g. 'TC12567')
+* @returns {Object} Object contatining Test Case details
+*/
 async function getTCDetailsFromRally(formattedID) {
-  const tcUrl = await getRallyTCUrl(formattedID);
-  return getDataFromRally(tcUrl)
-    .then((tcDetails) => tcDetails)
+  return getRallyTCUrl(formattedID)
+    .then(tcUrl => getDataFromRally(tcUrl))
     .catch(() => { throw Error(`Couldn't get ${formattedID} details`); });
 }
-
 
 exports.getTestSet = getTestSet;
 exports.getTesterUrl = getTesterUrl;
