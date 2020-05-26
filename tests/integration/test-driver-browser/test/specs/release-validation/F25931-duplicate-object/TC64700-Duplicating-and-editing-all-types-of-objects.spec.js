0b430bc0ca7988fc23bafd54fcd82ac90ac167e0
mgraczyk <mgraczyk@microstrategy.com>
2020-05-26 14:05:56 +0200

    Mistake

--- tests/integration/test-driver-browser/test/specs/release-validation/F25931-duplicate-object/TC64700-Duplicating-and-editing-all-types-of-objects.spec.js
+++ tests/integration/test-driver-browser/test/specs/release-validation/F25931-duplicate-object/TC64700-Duplicating-and-editing-all-types-of-objects.spec.js
@@ -38,7 +38,7 @@ describe('F25931 - Duplicate object', () => {
     browser.pause(3333);
     const dossier = objectsList.dossiers.interactiveDossier;
     PluginPopup.openDossier(dossier.name);
-    PluginPopup.selectAndImportVizualiation(dossier.vis2);
+    PluginPopup.selectAndImportVizualization(dossier.vis2);
     PluginRightPanel.waitAndCloseNotification(dictionary.en.importSuccess);
     browser.pause(1000);
 
