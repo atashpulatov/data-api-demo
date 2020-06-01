28d02690e44e070d178de7aa343d533fc6e0aba8
azawadzki <azawadzki@microstrategy.com>
2020-06-01 12:51:58 +0200

    use it.each in test for composeFilter

--- production/src/_tests_/popup/popup-view-selector-helper.test.js
+++ production/src/_tests_/popup/popup-view-selector-helper.test.js
@@ -8,89 +8,86 @@ describe('PopupViewSelectorHelper', () => {
     const filter4 = 'filter4';
     const filter4Element3 = `${filter4}:3`;
     const filter4Element9 = `${filter4}:9`;
-    it('should return expected value when called with 1 filter with 1 element', () => {
-      // given
-      const givenFilters = {
-        filter1: [filter1Element1],
-      };
-      const expectedResult = {
-        operator: 'In',
-        operands: [
-          { type: 'attribute', id: [filter1Element1] },
-          { type: 'elements', elements: [{ id: filter1Element1 }] }
-        ]
-      };
-      // when
-      const result = popupViewSelectorHelper.composeFilter(givenFilters);
-      // then
-      expect(result).toStrictEqual(expectedResult);
-    });
 
-    it('should return expected value when called with 2 filters, both with 1 element', () => {
-      // given
-      const givenFilters = {
-        filter1: [filter1Element1],
-        filter4: [filter4Element3]
-      };
-      const expectedResult = {
-        operator: 'And',
-        operands: [
-          { operator: 'In',
-            operands: [
-              { type: 'attribute', id: [filter1Element1] },
-              { type: 'elements', elements: [{ id: filter1Element1 }] }
-            ]
-          },
-          { operator: 'In',
-            operands: [
-              { type: 'attribute', id: [filter4Element3] },
-              { type: 'elements', elements: [{ id: filter4Element3 }] }
-            ]
-          }
-        ]
-      };
-      // when
-      const result = popupViewSelectorHelper.composeFilter(givenFilters);
-      // then
-      expect(result).toStrictEqual(expectedResult);
-    });
+    const givenFilter1 = {
+      filter1: [filter1Element1],
+    };
+    const expectedResult1 = {
+      operator: 'In',
+      operands: [
+        { type: 'attribute', id: [filter1Element1] },
+        { type: 'elements', elements: [{ id: filter1Element1 }] }
+      ]
+    };
 
-    it('should return expected value when called with 2 filters, both with 2 elements', () => {
-      // given
-      const givenFilters = {
-        filter1: [filter1Element1, filter1Element7],
-        filter4: [filter4Element3, filter4Element9]
-      };
-      const expectedResult = {
-        operator: 'And',
-        operands: [
-          { operator: 'In',
-            operands: [
-              { type: 'attribute', id: [filter1Element1, filter1Element7] },
-              { type: 'elements',
-                elements: [
-                  { id: filter1Element1 },
-                  { id: filter1Element7 }
-                ]
-              }
-            ]
-          },
-          {
-            operator: 'In',
-            operands: [
-              { type: 'attribute', id: [filter4Element3, filter4Element9] },
-              { type: 'elements',
-                elements: [
-                  { id: filter4Element3 },
-                  { id: filter4Element9 }
-                ]
-              }
-            ]
-          }
-        ]
-      };
+    const givenFilter2 = {
+      filter1: [filter1Element1],
+      filter4: [filter4Element3]
+    };
+    const expectedResult2 = {
+      operator: 'And',
+      operands: [
+        {
+          operator: 'In',
+          operands: [
+            { type: 'attribute', id: [filter1Element1] },
+            { type: 'elements', elements: [{ id: filter1Element1 }] }
+          ]
+        },
+        {
+          operator: 'In',
+          operands: [
+            { type: 'attribute', id: [filter4Element3] },
+            { type: 'elements', elements: [{ id: filter4Element3 }] }
+          ]
+        }
+      ]
+    };
+
+    const givenFilter3 = {
+      filter1: [filter1Element1, filter1Element7],
+      filter4: [filter4Element3, filter4Element9]
+    };
+    const expectedResult3 = {
+      operator: 'And',
+      operands: [
+        {
+          operator: 'In',
+          operands: [
+            { type: 'attribute', id: [filter1Element1, filter1Element7] },
+            {
+              type: 'elements',
+              elements: [
+                { id: filter1Element1 },
+                { id: filter1Element7 }
+              ]
+            }
+          ]
+        },
+        {
+          operator: 'In',
+          operands: [
+            { type: 'attribute', id: [filter4Element3, filter4Element9] },
+            {
+              type: 'elements',
+              elements: [
+                { id: filter4Element3 },
+                { id: filter4Element9 }
+              ]
+            }
+          ]
+        }
+      ]
+    };
+
+    it.each`
+    givenFilter           | expectedResult
+    ${givenFilter1}       | ${expectedResult1}
+    ${givenFilter2}       | ${expectedResult2}
+    ${givenFilter3}       | ${expectedResult3}
+    `('should return $expectedResult when composeFilter was called with $givenFilter', ({ givenFilter, expectedResult }) => {
       // when
-      const result = popupViewSelectorHelper.composeFilter(givenFilters);
+      const result = popupViewSelectorHelper.composeFilter(givenFilter);
       // then
       expect(result).toStrictEqual(expectedResult);
     });
