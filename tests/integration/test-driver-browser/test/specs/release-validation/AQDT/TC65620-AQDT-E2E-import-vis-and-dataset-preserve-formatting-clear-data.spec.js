0f12f02bac6f8581fb387a55a3a87a1ed21d657d
jkowalczyk <jkowalczyk@microstrategy.com>
2020-06-19 10:58:12 +0200

    Added TC65620

--- tests/integration/test-driver-browser/test/specs/release-validation/AQDT/TC65620-AQDT-E2E-import-vis-and-dataset-preserve-formatting-clear-data.spec.js
+++ tests/integration/test-driver-browser/test/specs/release-validation/AQDT/TC65620-AQDT-E2E-import-vis-and-dataset-preserve-formatting-clear-data.spec.js
@@ -6,54 +6,81 @@ import { waitForNotification } from '../../../helpers/utils/wait-helper';
 import { rightPanelSelectors } from '../../../constants/selectors/plugin.right-panel-selectors';
 import { dictionary } from '../../../constants/dictionaries/dictionary';
 import { logStep, logFirstStep } from '../../../helpers/utils/allure-helper';
-import {
-  switchToPromptFrameForImportDossier, switchToPromptFrame, switchToDialogFrame, switchToPluginFrame, switchToPopupFrame, switchToExcelFrame
-} from '../../../helpers/utils/iframe-helper';
-import { waitAndClick } from '../../../helpers/utils/click-helper';
+import { switchToDialogFrame, switchToPluginFrame, switchToExcelFrame } from '../../../helpers/utils/iframe-helper';
 import { popupSelectors } from '../../../constants/selectors/popup-selectors';
-
+import { excelSelectors } from '../../../constants/selectors/office-selectors';
 
 describe('US262640: E2E Test Case Automation for AQDT Environment', () => {
   it('[TC65620] - AQDT E2E - Import vis, import dataset, preserve formatting, secure data', () => {
-    // logFirstStep('+ should import a visualisation from TEC.QA dossier owned by Andrew Smith');
-    // OfficeWorksheet.selectCell('A1');
-    // PluginRightPanel.clickImportDataButton();
-    // PluginPopup.switchLibrary(false);
-    // switchToDialogFrame();
-    // PluginPopup.clickFilterButton();
-    // PluginPopup.tickFilterCheckBox('Certified Status', 'Certified');
-
     const { tecQa } = objectsList.aqdtMirror2Objects;
     const vis1 = tecQa.visualizations.automationByUnitVis1;
-    const vis2 = tecQa.visualizations.workDetailLedger;
+    const vis2 = tecQa.visualizations.useCaseDetail;
+    const page = popupSelectors.dossierWindow.getPageAt(18);
+    const { reportFormatting } = objectsList.aqdtMirror2Objects;
 
-    // PluginPopup.importAnyObject(tecQa.name);
-    // PluginPopup.selectVisualization(vis1);
-    // switchToPluginFrame();
-    // PluginPopup.clickImport();
-    // waitForNotification();
-    // expect($(rightPanelSelectors.notificationPopUp).getAttribute('textContent')).toContain(dictionary.en.importSuccess);
-    // PluginRightPanel.closeNotificationOnHover();
-
-    logStep('+ should edit the imported visualisation and import a different one');
-
-    PluginRightPanel.editObject(1);
+    logFirstStep('+ should import a visualisation from TEC.QA dossier owned by Andrew Smith');
+    OfficeWorksheet.selectCell('A1');
+    PluginRightPanel.clickImportDataButton();
+    PluginPopup.switchLibrary(false);
     switchToDialogFrame();
-    browser.pause(4000);
-    PluginPopup.goToDossierPageOrChapter(16);
-    switchToExcelFrame();
-    PluginPopup.selectVisualization(vis2);
+    PluginPopup.clickFilterButton();
+    PluginPopup.tickFilterCheckBox('Certified Status', 'Certified');
+
+    PluginPopup.importAnyObject(tecQa.name);
+    PluginPopup.selectVisualization(vis1);
     switchToPluginFrame();
     PluginPopup.clickImport();
     waitForNotification();
     expect($(rightPanelSelectors.notificationPopUp).getAttribute('textContent')).toContain(dictionary.en.importSuccess);
     PluginRightPanel.closeNotificationOnHover();
 
-    // logStep('+ should clear data');
-    // switchToPluginFrame();
-    // PluginRightPanel.clearData();
+    logStep('+ should edit the imported visualisation and import a different one');
+    PluginRightPanel.editObject(1);
+    switchToDialogFrame();
+    browser.pause(4000);
+    PluginPopup.goToDossierPageOrChapter(23);
+    switchToDialogFrame();
+    PluginPopup.selectVisualizationOnPage(page, vis2);
+    switchToPluginFrame();
+    PluginPopup.clickImport();
+    waitForNotification();
+    expect($(rightPanelSelectors.notificationPopUp).getAttribute('textContent')).toContain(dictionary.en.importSuccess);
+    PluginRightPanel.closeNotificationOnHover();
 
-    // logStep('+ should logout');
-    // PluginRightPanel.logout();
+    logStep('+ should import a report with MSTR formatting');
+    switchToExcelFrame();
+    OfficeWorksheet.selectCell('M1');
+    PluginRightPanel.clickAddDataButton();
+    switchToDialogFrame();
+    PluginPopup.clickFilterButton();
+    PluginPopup.tickFilterCheckBox('Certified Status', 'Certified');
+    PluginPopup.importAnyObject(reportFormatting);
+    waitForNotification();
+    expect($(rightPanelSelectors.notificationPopUp).getAttribute('textContent')).toContain(dictionary.en.importSuccess);
+    PluginRightPanel.closeNotificationOnHover();
+
+    PluginRightPanel.clickObjectInRightPanel(1);
+    browser.pause(4000);
+    OfficeWorksheet.formatTable();
+    browser.pause(4000);
+
+    logStep('+ should clear data');
+    switchToPluginFrame();
+    PluginRightPanel.clickSettings();
+    PluginRightPanel.clearData();
+
+    logStep('+ should click View Data');
+    switchToPluginFrame();
+    PluginRightPanel.viewDataBtn();
+
+    switchToExcelFrame();
+    OfficeWorksheet.selectCell('A1');
+    browser.pause(1000);
+    const a1 = excelSelectors.getCell(1, 1);
+    const a1Value = $(a1).getText();
+    browser.waitUntil(() => a1Value === 'ITEM', { timeout: 20000 });
+
+    logStep('+ should logout');
+    PluginRightPanel.logout();
   });
 });
