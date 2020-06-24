072cc8a2aa4e88dd278754825a3c42e658c1086f
bkoziel <bkoziel@microstrategy.com>
2020-06-24 16:21:29 +0200

    added unit tests for clear data steps

--- production/src/_tests_/office/highlight/step-highlight-object.test.js
+++ production/src/_tests_/office/highlight/step-highlight-object.test.js
@@ -1,7 +1,7 @@
-const { default: stepHighlightObject } = require('../../../office/highlight/step-highlight-object');
-const { officeApiHelper } = require('../../../office/api/office-api-helper');
-const { default: operationStepDispatcher } = require('../../../operation/operation-step-dispatcher');
-const { default: operationErrorHandler } = require('../../../operation/operation-error-handler');
+import operationErrorHandler from '../../../operation/operation-error-handler';
+import stepHighlightObject from '../../../office/highlight/step-highlight-object';
+import operationStepDispatcher from '../../../operation/operation-step-dispatcher';
+import { officeApiHelper } from '../../../office/api/office-api-helper';
 
 describe('StepHighlightObject', () => {
   afterEach(() => {
@@ -31,6 +31,7 @@ describe('StepHighlightObject', () => {
     const operationData = {};
     const error = new Error('error');
 
+    jest.spyOn(console, 'error').mockImplementation();
     const mockedOnObjectClick = jest.spyOn(officeApiHelper, 'onBindingObjectClick').mockImplementation(() => { throw error; });
     const mockedHandleError = jest.spyOn(operationErrorHandler, 'handleOperationError').mockImplementation();
 
