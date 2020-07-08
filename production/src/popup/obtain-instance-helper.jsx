eda710610baa07f137359900d5c7914209bae262
azawadzki <azawadzki@microstrategy.com>
2020-07-08 13:04:15 +0200

    Add useDispatch to ObtainInstanceHelper

--- production/src/popup/obtain-instance-helper.jsx
+++ production/src/popup/obtain-instance-helper.jsx
@@ -1,36 +1,36 @@
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { popupViewSelectorHelper } from './popup-view-selector-helper';
 import mstrObjectEnum from '../mstr-object/mstr-object-type-enum';
 import { popupHelper } from './popup-helper';
+import { popupActions } from '../redux-reducer/popup-reducer/popup-actions';
 
 export const ObtainInstanceHelper = () => {
   const props = useSelector(state => {
     const {
       navigationTree,
-      popupReducer: { editedObject, preparedInstance },
-      sessionReducer: { attrFormPrivilege, authToken },
+      popupReducer: { editedObject },
+      sessionReducer: { attrFormPrivilege },
       officeReducer,
-      popupStateReducer
     } = state;
     const { promptsAnswers } = navigationTree;
     const { supportForms } = officeReducer;
-    const { popupType } = popupStateReducer;
     const isReport = editedObject && editedObject.mstrObjectType.name === mstrObjectEnum.mstrObjectType.report.name;
     const formsPrivilege = supportForms && attrFormPrivilege && isReport;
     return {
       ...navigationTree,
-      authToken,
       editedObject: { ...(popupHelper.parsePopupState(editedObject, promptsAnswers, formsPrivilege)) },
-      preparedInstance,
-      propsToPass: { ...popupStateReducer },
-      popupType,
-      formsPrivilege
     };
   });
 
+  const dispatch = useDispatch();
+
+  const preparePromptedReport = () => {
+    dispatch(popupActions.preparePromptedReport);
+  };
+
   useEffect(() => {
-    popupViewSelectorHelper.obtainInstanceWithPromptsAnswers(props);
+    popupViewSelectorHelper.obtainInstanceWithPromptsAnswers({ ...props, preparePromptedReport });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return <div />;
