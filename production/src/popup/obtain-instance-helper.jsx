9128c941993bdb34397905c5a5b99f982a1d036d
azawadzki <azawadzki@microstrategy.com>
2020-07-08 13:37:03 +0200

    Convert ObtainInstanceHelper to class component

--- production/src/popup/obtain-instance-helper.jsx
+++ production/src/popup/obtain-instance-helper.jsx
@@ -1,37 +1,37 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { connect } from 'react-redux';
+import { popupActions } from '../redux-reducer/popup-reducer/popup-actions';
 import { popupViewSelectorHelper } from './popup-view-selector-helper';
 import mstrObjectEnum from '../mstr-object/mstr-object-type-enum';
 import { popupHelper } from './popup-helper';
-import { popupActions } from '../redux-reducer/popup-reducer/popup-actions';
 
-export const ObtainInstanceHelper = () => {
-  const props = useSelector(state => {
-    const {
-      navigationTree,
-      popupReducer: { editedObject },
-      sessionReducer: { attrFormPrivilege },
-      officeReducer,
-    } = state;
-    const { promptsAnswers } = navigationTree;
-    const { supportForms } = officeReducer;
-    const isReport = editedObject && editedObject.mstrObjectType.name === mstrObjectEnum.mstrObjectType.report.name;
-    const formsPrivilege = supportForms && attrFormPrivilege && isReport;
-    return {
-      ...navigationTree,
-      editedObject: { ...(popupHelper.parsePopupState(editedObject, promptsAnswers, formsPrivilege)) },
-    };
-  });
+class ObtainInstanceHelperNotConnected extends React.Component {
+  componentDidMount() {
+    popupViewSelectorHelper.obtainInstanceWithPromptsAnswers(this.props);
+  }
 
-  const dispatch = useDispatch();
+  render() {
+    return <div />;
+  }
+}
 
-  const preparePromptedReport = () => {
-    dispatch(popupActions.preparePromptedReport);
+function mapStateToProps(state) {
+  const {
+    navigationTree,
+    popupReducer: { editedObject },
+    sessionReducer: { attrFormPrivilege },
+    officeReducer,
+  } = state;
+  const { promptsAnswers } = navigationTree;
+  const { supportForms } = officeReducer;
+  const isReport = editedObject && editedObject.mstrObjectType.name === mstrObjectEnum.mstrObjectType.report.name;
+  const formsPrivilege = supportForms && attrFormPrivilege && isReport;
+  return {
+    ...navigationTree,
+    editedObject: { ...(popupHelper.parsePopupState(editedObject, promptsAnswers, formsPrivilege)) },
   };
+}
 
-  useEffect(() => {
-    popupViewSelectorHelper.obtainInstanceWithPromptsAnswers({ ...props, preparePromptedReport });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  return <div />;
-};
+const mapDispatchToProps = { preparePromptedReport: popupActions.preparePromptedReport, };
+
+export const ObtainInstanceHelper = connect(mapStateToProps, mapDispatchToProps)(ObtainInstanceHelperNotConnected);
