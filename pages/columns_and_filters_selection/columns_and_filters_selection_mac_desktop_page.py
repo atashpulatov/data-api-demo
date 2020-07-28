3f817c71ce1e083af17401e1a8ebfc5cc9e520d2
atashpulatov <atashpulatov@microstrategy.com>
2020-07-28 11:22:52 +0200

    US297296; Formatted code

--- pages/columns_and_filters_selection/columns_and_filters_selection_mac_desktop_page.py
+++ pages/columns_and_filters_selection/columns_and_filters_selection_mac_desktop_page.py
@@ -2,11 +2,19 @@ from pages.base_mac_desktop_page import BaseMacDesktopPage
 
 
 class ColumnsAndFiltersSelectionMacDesktopPage(BaseMacDesktopPage):
-    ITEM_ALL_ATTRIBUTES = "{prefix}/AXGroup[5]/AXGroup[0]".format(prefix=BaseMacDesktopPage.POPUP_WRAPPER_ELEM)
-    ITEM_ALL_METRICS = "{prefix}/AXGroup[7]/AXGroup[0]".format(prefix=BaseMacDesktopPage.POPUP_WRAPPER_ELEM)
+    ITEM_ALL_ATTRIBUTES = "{prefix}/AXGroup[5]/AXGroup[0]".format(
+        prefix=BaseMacDesktopPage.POPUP_WRAPPER_ELEM
+    )
+    ITEM_ALL_METRICS = "{prefix}/AXGroup[7]/AXGroup[0]".format(
+        prefix=BaseMacDesktopPage.POPUP_WRAPPER_ELEM
+    )
     ATTRIBUTE_CHECKBOX = "{prefix}/AXOutline[0]/AXRow[@AXDescription='{attribute}']"
-    METRIC_CHECKBOX = "{prefix}/AXTable[0]/AXUnknown[0]/AXGroup/AXGroup[@AXDescription='{attribute}']"
-    IMPORT_BUTTON = "{prefix}/AXGroup[13]/AXButton[@AXTitle='Import']".format(prefix=BaseMacDesktopPage.POPUP_WRAPPER_ELEM)
+    METRIC_CHECKBOX = (
+        "{prefix}/AXTable[0]/AXUnknown[0]/AXGroup/AXGroup[@AXDescription='{attribute}']"
+    )
+    IMPORT_BUTTON = "{prefix}/AXGroup[13]/AXButton[@AXTitle='Import']".format(
+        prefix=BaseMacDesktopPage.POPUP_WRAPPER_ELEM
+    )
     OFFSET_X = 22
     OFFSET_Y = 15
 
@@ -18,17 +26,21 @@ class ColumnsAndFiltersSelectionMacDesktopPage(BaseMacDesktopPage):
 
     def unselect_all_metrics(self):
         self.get_element_by_xpath(self.ITEM_ALL_METRICS).click()
-    
+
     def click_attribute(self, attribute_name):
         self._click_element(self.ATTRIBUTE_CHECKBOX, attribute_name)
 
     def click_metric(self, metric_name):
         self._click_element(self.METRIC_CHECKBOX, metric_name)
-    
+
     def _click_element(self, selector, element_name):
-       formatted_selector = selector.format(prefix=BaseMacDesktopPage.POPUP_WRAPPER_ELEM, attribute=element_name)
-        
-       self.get_element_by_xpath(formatted_selector).click(offset_x=self.OFFSET_X, offset_y=self.OFFSET_Y)
-    
+        formatted_selector = selector.format(
+            prefix=BaseMacDesktopPage.POPUP_WRAPPER_ELEM, attribute=element_name
+        )
+
+        self.get_element_by_xpath(formatted_selector).click(
+            offset_x=self.OFFSET_X, offset_y=self.OFFSET_Y
+        )
+
     def click_import_button(self):
         self.get_element_by_xpath(self.IMPORT_BUTTON).click()
--- pages_set/pages_set_mac_desktop.py
+++ pages_set/pages_set_mac_desktop.py
@@ -1,15 +1,31 @@
 from pages.add_in_login.add_in_login_mac_desktop_page import AddInLoginMacDesktopPage
-from pages.duplicate_object_popup.duplicate_object_popup_mac_desktop_page import DuplicateObjectPopupMacDesktopPage
+from pages.duplicate_object_popup.duplicate_object_popup_mac_desktop_page import (
+    DuplicateObjectPopupMacDesktopPage,
+)
 from pages.excel.cleanup.cleanup_mac_desktop_page import CleanupMacDesktopPage
 from pages.excel.excel_menu.excel_menu_mac_desktop_page import ExcelMenuMacDesktopPage
-from pages.excel.excel_sheet.excel_sheet_mac_desktop_page import ExcelSheetMacDesktopPage
-from pages.excel.start_excel.start_excel_mac_desktop_page import StartExcelMacDesktopPage
-from pages.import_data_popup.import_data_popup_mac_desktop_page import ImportDataPopupMacDesktopPage
-from pages.not_logged_right_panel.not_logged_right_panel_mac_desktop_page import NotLoggedRightPanelMacDesktopPage
-from pages.right_panel.right_panel.right_panel_mac_desktop_page import RightPanelMacDesktopPage
-from pages.right_panel.right_panel_tile.right_panel_tile_mac_desktop_page import RightPanelTileMacDesktopPage
+from pages.excel.excel_sheet.excel_sheet_mac_desktop_page import (
+    ExcelSheetMacDesktopPage,
+)
+from pages.excel.start_excel.start_excel_mac_desktop_page import (
+    StartExcelMacDesktopPage,
+)
+from pages.import_data_popup.import_data_popup_mac_desktop_page import (
+    ImportDataPopupMacDesktopPage,
+)
+from pages.not_logged_right_panel.not_logged_right_panel_mac_desktop_page import (
+    NotLoggedRightPanelMacDesktopPage,
+)
+from pages.right_panel.right_panel.right_panel_mac_desktop_page import (
+    RightPanelMacDesktopPage,
+)
+from pages.right_panel.right_panel_tile.right_panel_tile_mac_desktop_page import (
+    RightPanelTileMacDesktopPage,
+)
 from pages_set.abstract_pages_set import AbstractPagesSet
-from pages.columns_and_filters_selection.columns_and_filters_selection_mac_desktop_page import ColumnsAndFiltersSelectionMacDesktopPage
+from pages.columns_and_filters_selection.columns_and_filters_selection_mac_desktop_page import (
+    ColumnsAndFiltersSelectionMacDesktopPage,
+)
 
 
 class PagesSetMacDesktop(AbstractPagesSet):
@@ -24,9 +40,15 @@ class PagesSetMacDesktop(AbstractPagesSet):
         self.right_panel_tile_mac_desktop_page = RightPanelTileMacDesktopPage()
         self.import_data_popup_mac_desktop_page = ImportDataPopupMacDesktopPage()
         self.excel_sheet_mac_desktop_page = ExcelSheetMacDesktopPage()
-        self.duplicate_object_popup_mac_desktop_page = DuplicateObjectPopupMacDesktopPage()
-        self.not_logged_right_panel_mac_desktop_page = NotLoggedRightPanelMacDesktopPage()
-        self.columns_and_filters_selection_mac_desktop_page = ColumnsAndFiltersSelectionMacDesktopPage()
+        self.duplicate_object_popup_mac_desktop_page = (
+            DuplicateObjectPopupMacDesktopPage()
+        )
+        self.not_logged_right_panel_mac_desktop_page = (
+            NotLoggedRightPanelMacDesktopPage()
+        )
+        self.columns_and_filters_selection_mac_desktop_page = (
+            ColumnsAndFiltersSelectionMacDesktopPage()
+        )
 
     def start_excel_page(self):
         return self.start_excel_mac_desktop_page
