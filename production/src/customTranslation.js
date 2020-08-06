d99d78e6737f60897101231595b8d2f6a4bf63b5
dhornos <dhornos@microstrategy.com>
2020-08-06 10:01:19 +0200

    DE164565;Fixed Eslint warnings

--- production/src/customTranslation.js
+++ production/src/customTranslation.js
@@ -3,7 +3,8 @@ import i18n from './i18n';
 /**
   * It gets the translation of the string (if available)
   *
-  * It is using the translation stored in json file from i18n to get the translated version of the string for the currently used language
+  * It is using the translation stored in json file from i18n
+  * to get the translated version of the string for the currently used language
   *
   * @param {String} str - String to be translated
   * @returns {String} Translation of the string for the currently used language
