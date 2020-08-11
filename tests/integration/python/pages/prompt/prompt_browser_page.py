84d0b5f24a626ed1c301cad410bd4d26a3c3cd7f
Sonia Lukaszewicz <slukaszewicz@microstrategy.com>
2020-08-11 15:07:32 +0200

    selecting prompt from the list

--- tests/integration/python/pages/prompt/prompt_browser_page.py
+++ tests/integration/python/pages/prompt/prompt_browser_page.py
@@ -3,9 +3,7 @@ from framework.pages_base.base_browser_page import BaseBrowserPage
 
 class PromptBrowserPage(BaseBrowserPage):
     PROMPT_RUN_BUTTON = '#run'
-    PROMPT_INDEX_TABLE = '.mstrPromptTOCHeader'
-
-
+    PROMPT_LIST_ELEM = '.mstrPromptTOCListItemIndex'
 
     def __init__(self):
         super().__init__()
@@ -13,4 +11,9 @@ class PromptBrowserPage(BaseBrowserPage):
     def select_prompt_from_list(self, index):
         self.focus_on_prompt_frame()
 
+        prompt_list = self.find_element_by_text_in_elements_list_by_css_safe(
+            PromptBrowserPage.PROMPT_LIST_ELEM,
+            index,
+        )
 
+        prompt_list.click()
