a677c0f148ffd5924184c4920eee78524286de31
Pawel Laskowski <plaskowski@microstrategy.com>
2020-08-13 14:50:47 +0200

    US299439; TC55013; Add TC steps and selectors

--- tests/integration/python/pages/filter_panel/filter_panel_browser_page.py
+++ tests/integration/python/pages/filter_panel/filter_panel_browser_page.py
@@ -2,21 +2,46 @@ from framework.pages_base.base_browser_page import BaseBrowserPage
 
 
 class FilterPanelBrowserPage(BaseBrowserPage):
-    OWNER_ALL_PANEL = 'div.category-list-wrapper:nth-child(4) > button:nth-child(2)'
     SELECT_ALL_WITHIN_ALL_PANEL = 'button.all-panel__button:nth-child(1)'
-    ELEMENT_FROM_CATEGORY = '.category-list-header[aria-label="%s"] + .category-list-table label[title="%s"]'
     ALL_PANEL_EMPTY_ELEMENT = '.all-panel__content .category-list-row.disabled .checkbox-cell'
-    # FIRST_CATEGORY_FIRST_CHECKBOX = 'div.category-list-wrapper:nth-child(1) input'
-    # FIRST_CATEGORY_FIRST_CHECKBOX = 'div.category-list-wrapper:nth-child(1) input:nth-child(1)'
-    APPLICATIONS_CATEGORY_CHECKBOXES = 'div.category-list-wrapper:nth-child(1) input'
-    TYPES_CATEGORY_CHECKBOXES = 'div.category-list-wrapper:nth-child(2) input'
-    VIEW_SELECTED_SWITCH = '.all-panel .mstr-switch'
+    VIEW_SELECTED_SWITCH_ALL_PANEL = '.all-panel .mstr-switch'
+    SEARCH_INPUT = '.all-panel input.search-field__input'
+    ALL_PANEL_FIRST_CHECKBOX = '.all-panel .ReactVirtualized__Grid__innerScrollContainer > div:first-child input'
 
+    APPLICATIONS_FIRST_CHECKBOX = '.filter-panel .category-list-wrapper:nth-child(1) ' \
+                                  '.category-list-row:nth-child(1) input:nth-of-type(1)'
+    APPLICATIONS_ALL_PANEL = '.filter-panel div.category-list-wrapper:nth-child(1) > button:nth-child(2)'
+
+    TYPES_REPORT_CHECKBOX = '.filter-panel .category-list-wrapper:nth-child(2) ' \
+                            '.category-list-row:nth-child(1) input'
+    TYPES_DATASET_CHECKBOX = '.filter-panel .category-list-wrapper:nth-child(2) ' \
+                             '.category-list-row:nth-child(2) input'
+    TYPES_DOSSIER_CHECKBOX = '.filter-panel .category-list-wrapper:nth-child(2) ' \
+                             '.category-list-row:nth-child(3) input'
+
+    CERTIFIED_CHECKBOX = '.filter-panel .category-list-wrapper:nth-child(3) ' \
+                         '.category-list-row:nth-child(1) input:nth-of-type(1)'
+
+    OWNERS_FIRST_CHECKBOX = '.filter-panel .category-list-wrapper:nth-child(4) ' \
+                            '.category-list-row:nth-child(1) input:nth-of-type(1)'
+    OWNER_ALL_PANEL = '.filter-panel div.category-list-wrapper:nth-child(4) > button:nth-child(2)'
+
+    ELEMENT_FROM_CATEGORY = '.category-list-header[aria-label="%s"] + .category-list-table label[title="%s"]'
     ELEMENT_CHECKED = 'input:checked'
 
     MAP_ELEMENT_NAME_TO_SELECTOR = {
-        'View Selected switch': '.all-panel .mstr-switch',
-        'first checkbox in Applications category': 'div.category-list-table:nth-of-type(1) input:nth-of-type(1)'
+        'View Selected switch': VIEW_SELECTED_SWITCH_ALL_PANEL,
+        'Select All button in All Panel': SELECT_ALL_WITHIN_ALL_PANEL,
+        'Search input': SEARCH_INPUT,
+        'first checkbox in All Panel': ALL_PANEL_FIRST_CHECKBOX,
+        'first checkbox in Applications': APPLICATIONS_FIRST_CHECKBOX,
+        'Applications All button': APPLICATIONS_ALL_PANEL,
+        'checkbox for Report': TYPES_REPORT_CHECKBOX,
+        'checkbox for Dataset': TYPES_DATASET_CHECKBOX,
+        'checkbox for Dossier': TYPES_DOSSIER_CHECKBOX,
+        'checkbox for Certified': CERTIFIED_CHECKBOX,
+        'first checkbox in Owners': OWNERS_FIRST_CHECKBOX,
+        'Owners All button': OWNER_ALL_PANEL,
     }
 
     def __init__(self):
@@ -46,5 +71,6 @@ class FilterPanelBrowserPage(BaseBrowserPage):
         return element.check_if_child_element_exists_by_css(FilterPanelBrowserPage.ELEMENT_CHECKED)
 
     def examine_if_element_has_focus(self, element_name):
-        return (self.get_element_by_css(FilterPanelBrowserPage.MAP_ELEMENT_NAME_TO_SELECTOR[element_name])
+        return (self.get_element_by_css_no_visibility_checked(FilterPanelBrowserPage
+                .MAP_ELEMENT_NAME_TO_SELECTOR[element_name])
                 == self.get_element_with_focus())
--- tests/integration/python/tests/features/F32725_data_source_browsing_and_management_improvements/TC55013_object_filtering_filter_panel_accessibility.feature
+++ tests/integration/python/tests/features/F32725_data_source_browsing_and_management_improvements/TC55013_object_filtering_filter_panel_accessibility.feature
@@ -1,14 +1,14 @@
 @mac_chrome
 Feature: F32725 - Data source browsing and management improvements
 
-  Scenario: [TC55013] - [Object filtering] Filter panel Accessibility
+  Scenario: [TC55013] - Filter panel Accessibility
     Given I logged in as default user
       And I clicked Import Data button
       And MyLibrary Switch is OFF
       And I clicked Filters button
 
      When I pressed key Tab
-     Then element "first checkbox in Applications category" has focus
+     Then element "first checkbox in Applications" has focus
 
      When I pressed key Tab
      Then element "Applications All button" has focus
@@ -17,12 +17,45 @@ Feature: F32725 - Data source browsing and management improvements
      Then element "View Selected switch" has focus
 
      When I pressed key Tab
-     Then element "Search" has focus
+     Then element "Search input" has focus
 
      When I pressed key Tab
      Then element "first checkbox in All Panel" has focus
 
+     When I pressed key Tab
+     Then element "Select All button in All Panel" has focus
+
      When I pressed key Arrow Right
      Then element "Applications All button" has focus
+
+     When I pressed key Tab
+     Then element "checkbox for Report" has focus
+
+     When I pressed key Arrow Down
+     Then element "checkbox for Dataset" has focus
+
+     When I pressed key Arrow Down
+     Then element "checkbox for Dossier" has focus
+
+     When I pressed key Arrow Up
+     Then element "checkbox for Dataset" has focus
+
+     When I pressed key Arrow Left
+     Then element "Select All button in All Panel" has focus
+
+     When I pressed key Arrow Right
+     Then element "checkbox for Dataset" has focus
+
+     When I pressed key Tab
+     Then element "checkbox for Certified" has focus
+
+     When I pressed key Tab
+     Then element "first checkbox in Owners" has focus
+
+     When I pressed key Tab
+     Then element "Owners All button" has focus
+
+     When I pressed key Arrow Left
+     Then element "View Selected switch" has focus
 
       And I log out
