366758336fe15af39816b5205595bb1078cd414d
Ryszard Raszplewicz <rraszplewicz@microstrategy.com>
2020-08-19 09:27:15 +0200

    Tests. Opening New blank workbook improved.

--- tests/integration/python/pages/excel/excel_main/excel_main_browser_page.py
+++ tests/integration/python/pages/excel/excel_main/excel_main_browser_page.py
@@ -4,13 +4,13 @@ from framework.pages_base.base_browser_page import BasePage
 class ExcelMainBrowserPage(BasePage):
     APP_LAUNCHER_ELEM = "[title^='App launcher']"
 
-    WELCOME_PAGE_EXCEL_ARIA_ELEM = "[aria-label^='Excel']"
+    APP_LAUNCHER_EXCEL_ELEM = 'O365_AppTile_ExcelOnline'
 
     NEW_BLANK_WORKBOOK_ELEM = "[title^='New blank workbook']"
 
     def open_new_work_book(self):
         self.get_element_by_css(ExcelMainBrowserPage.APP_LAUNCHER_ELEM).click()
 
-        self.get_element_by_css(ExcelMainBrowserPage.WELCOME_PAGE_EXCEL_ARIA_ELEM).click()
+        self.get_element_by_id(ExcelMainBrowserPage.APP_LAUNCHER_EXCEL_ELEM).click()
 
         self.get_element_by_css(ExcelMainBrowserPage.NEW_BLANK_WORKBOOK_ELEM).click()
