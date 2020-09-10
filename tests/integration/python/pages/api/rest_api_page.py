c53d2748a6b107cfdc4a31906d495b974e4827a6
Ryszard Raszplewicz <rraszplewicz@microstrategy.com>
2020-09-10 16:02:28 +0200

    Rest API calls fixed.

--- tests/integration/python/pages/api/rest_api_page.py
+++ tests/integration/python/pages/api/rest_api_page.py
@@ -14,10 +14,8 @@ class RestApiPage(BasePage):
 
     REST_API_LOGIN_ENDPOINT = '/auth/login'
 
-    REST_API_OBJECT_INFO_ENDPOINT = '/object/%s/type=3'
-    REST_API_OBJECT_CERTIFY_ENDPOINT = '/object/%s/certify?type=3&certify=%s'
-
-    HTTP_RESPONSE_CODE_OK = 200
+    REST_API_OBJECT_INFO_ENDPOINT = '/objects/%s/?type=3'
+    REST_API_OBJECT_CERTIFY_ENDPOINT = '/objects/%s/certify?type=3&certify=%s'
 
     def __init__(self):
         super().__init__()
@@ -41,9 +39,9 @@ class RestApiPage(BasePage):
             cookies=cookies
         )
 
-        if response.status_code != RestApiPage.HTTP_RESPONSE_CODE_OK:
+        if not response.ok:
             raise MstrException(f'Error while accessing url: {url}, headers: {custom_headers}, '
-                                f'cookies: {cookies}, status: {response.status_code}')
+                                f'cookies: {cookies}, response: {response.status_code}')
 
     def is_object_certified(self, object_id, project_id=TUTORIAL_PROJECT_ID):
         cookies, custom_headers = self._get_cookies_and_headers(project_id)
@@ -56,7 +54,7 @@ class RestApiPage(BasePage):
             cookies=cookies
         )
 
-        if response.status_code != RestApiPage.HTTP_RESPONSE_CODE_OK:
+        if not response.ok:
             raise MstrException(f'Error while accessing url: {url}, headers: {custom_headers}, '
                                 f'cookies: {cookies}, status: {response.status_code}')
 
@@ -88,7 +86,7 @@ class RestApiPage(BasePage):
             data=payload
         )
 
-        if response.status_code != RestApiPage.HTTP_RESPONSE_CODE_OK:
+        if not response.ok:
             raise MstrException(f'Error while accessing url: {url}, payload: {payload}, status: {response.status_code}')
 
         auth_token = response.headers[RestApiPage.HEADER_NAME_AUTH_TOKEN]
