5ecf70230d784d098b1ee02cd96adb60a3c3e7b5
Ryszard Raszplewicz <rraszplewicz@microstrategy.com>
2020-09-18 12:08:26 +0200

    Possibility to launch AppiumForMac automatically added, on Mac Desktop, fixes. Added possibility to execute code before driver initialization.

--- tests/integration/python/framework/util/test_util.py
+++ tests/integration/python/framework/util/test_util.py
@@ -4,6 +4,13 @@ from framework.util.config_util import ConfigUtil
 
 class TestUtil:
     @staticmethod
+    def global_test_startup():
+        driver_type = ConfigUtil.get_driver_type()
+        before_driver_startup = DriverFactory().get_before_driver_startup(driver_type)
+
+        before_driver_startup()
+
+    @staticmethod
     def global_test_cleanup():
         driver_type = ConfigUtil.get_driver_type()
         driver = DriverFactory().get_driver(driver_type)
