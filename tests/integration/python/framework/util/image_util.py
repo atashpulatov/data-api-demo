fb01f53d15bfabda9148ba8805fd786f53d35bc0
Ryszard Raszplewicz <rraszplewicz@microstrategy.com>
2020-09-21 13:06:58 +0200

    CI config, Windows Desktop. image_recognition_screenshots_folder config param introduced.

--- tests/integration/python/framework/util/image_util.py
+++ tests/integration/python/framework/util/image_util.py
@@ -4,11 +4,13 @@ from io import BytesIO
 
 from PIL import Image
 
+from framework.util.config_util import ConfigUtil
+from framework.util.exception.MstrException import MstrException
+
 
 class ImageUtil:
     CURRENT_SCREENSHOT_FILE_NAME = 'current_screenshot'
 
-    SCREENSHOT_FOLDER = 'framework/screenshots'
     SCREENSHOT_FILE_EXTENSION = '.png'
 
     TO_ALPHA_REGEX = re.compile(r'\W')
@@ -46,8 +48,22 @@ class ImageUtil:
         return left, top, right, bottom
 
     def prepare_file_path(self, file_name_prefix):
+        screenshots_folder = self._get_screenshots_folder()
+
         file_name_prefix_alpha = ImageUtil.TO_ALPHA_REGEX.sub('_', file_name_prefix)
         file_name = ''.join((file_name_prefix_alpha.lower(), ImageUtil.SCREENSHOT_FILE_EXTENSION))
-        file_path = os.path.join(ImageUtil.SCREENSHOT_FOLDER, file_name)
+
+        file_path = os.path.join(screenshots_folder, file_name)
 
         return file_path
+
+    def _get_screenshots_folder(self):
+        screenshots_folder = ConfigUtil.get_image_recognition_screenshots_folder()
+
+        if not os.path.exists(screenshots_folder):
+            raise MstrException(
+                'Invalid image recognition screenshots folder name, folder does not exist '
+                'or has wrong permissions: %s' % screenshots_folder
+            )
+
+        return screenshots_folder
