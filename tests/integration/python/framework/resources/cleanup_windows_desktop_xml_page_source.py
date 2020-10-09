4cf0ca9c33c7595de0941143221a9f22da0b05f0
Ryszard Raszplewicz <rraszplewicz@microstrategy.com>
2020-10-09 15:10:03 +0200

    cleanup_windows_desktop_xml_page_source.py script for cleaning up XML representing Windows Desktop application added.

--- tests/integration/python/framework/resources/cleanup_windows_desktop_xml_page_source.py
+++ tests/integration/python/framework/resources/cleanup_windows_desktop_xml_page_source.py
@@ -28,6 +28,37 @@ a) first line contains full path to Python interpreter (#!/usr/local/bin/python3
 b) it's executable (chmod a+x cleanup_windows_desktop_xml_page_source.py).
 
 4. Open page_source_cleaned.xml in your favorite XML viewer and format it.
+
+For example, before:
+
+<?xml version="1.0" encoding="utf-16"?>
+<Window AcceleratorKey="" AccessKey="" AutomationId="" ClassName="XLMAIN" FrameworkId="Win32" HasKeyboardFocus="False"
+        HelpText="" IsContentElement="True" IsControlElement="True" IsEnabled="True" IsKeyboardFocusable="True"
+        IsOffscreen="False" IsPassword="False" IsRequiredForForm="False" ItemStatus="" ItemType=""
+        LocalizedControlType="window" Name="Book1 - Excel" Orientation="None" ProcessId="2244" RuntimeId="42.4261146"
+        x="0" y="0" width="1936" height="1056" CanMaximize="True" CanMinimize="True" IsModal="False"
+        WindowVisualState="Maximized" WindowInteractionState="ReadyForUserInteraction" IsTopmost="False"
+        CanRotate="False" CanResize="False" CanMove="False" IsAvailable="True">
+    <Window AcceleratorKey="" AccessKey="" AutomationId="" ClassName="NUIDialog" FrameworkId="Win32"
+            HasKeyboardFocus="False" HelpText="" IsContentElement="True" IsControlElement="True" IsEnabled="True"
+            IsKeyboardFocusable="True" IsOffscreen="False" IsPassword="False" IsRequiredForForm="False" ItemStatus=""
+            ItemType="" LocalizedControlType="window" Name="env-227052-MicroStrategy for Office" Orientation="None"
+            ProcessId="2244" RuntimeId="42.1705950" x="207" y="96" width="1522" height="857" CanMaximize="False"
+            CanMinimize="False" IsModal="False" WindowVisualState="Normal"
+            WindowInteractionState="ReadyForUserInteraction" IsTopmost="False" CanRotate="False" CanResize="True"
+            CanMove="True" IsAvailable="True">
+        <Window AcceleratorKey="" AccessKey="" AutomationId="" ClassName="NetUIHWNDElement" FrameworkId="Win32"
+                HasKeyboardFocus="False" HelpText="" IsContentElement="True" IsControlElement="True" IsEnabled="True"
+                IsKeyboardFocusable="False" IsOffscreen="False" IsPassword="False" IsRequiredForForm="False"
+                ItemStatus="" ItemType="" LocalizedControlType="Window" Name="env-227052-MicroStrategy for Office"
+                Orientation="None" ProcessId="2244" RuntimeId="42.2098840" x="208" y="127" width="1520" height="825">
+
+and after:
+
+<?xml version="1.0" encoding="utf-16"?>
+<Window Name="Book1 - Excel" x="0" y="0" width="1440" height="1040">
+    <Window Name="11.3 RC GA Validation (env-228690)-MicroStrategy for Office" x="-41" y="88" width="1522" height="857">
+        <Window Name="11.3 RC GA Validation (env-228690)-MicroStrategy for Office" x="-40" y="119" width="1520" height="825">
 """
 
 import fileinput
