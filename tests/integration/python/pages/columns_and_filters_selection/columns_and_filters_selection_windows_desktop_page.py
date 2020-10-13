0dc8710fed531b2cc30b19e09b5447ec95640143
Wojtaszko, Pawel <pwojtaszko@microstrategy.com>
2020-10-13 14:41:03 +0200

    Implemented steps for Windows Desktop: "I clicked Back button", "I clicked Cancel button",  "I clicked Data Preview button", "I clicked Close Preview button", "I clicked Include Subtotals and Totals switch"

--- tests/integration/python/pages/columns_and_filters_selection/columns_and_filters_selection_windows_desktop_page.py
+++ tests/integration/python/pages/columns_and_filters_selection/columns_and_filters_selection_windows_desktop_page.py
@@ -4,7 +4,11 @@ from framework.util.exception.MstrException import MstrException
 
 class ColumnsAndFiltersSelectionWindowsDesktopPage(BaseWindowsDesktopPage):
     IMPORT_BUTTON = 'import'
-
+    BACK_BUTTON = 'back'
+    DATA_PREVIEW_BUTTON = 'data-preview'
+    CANCEL_BUTTON = 'cancel'
+    CLOSE_PREVIEW_BUTTON = 'Close Preview'
+    TOTALS_AND_SUBTOTALS_SWITCH = '//Text[@Name="Include Subtotals and Totals"]'
     COLUMNS_AND_FILTERS_SELECTION_OPEN_TEXT = 'Columns & Filters Selection'
 
     def click_import_button(self):
@@ -28,3 +32,33 @@ class ColumnsAndFiltersSelectionWindowsDesktopPage(BaseWindowsDesktopPage):
 
         if not element_coordinates:
             raise MstrException('Error while opening Attributes Metrics Filters')
+
+    def click_back_button(self):
+        self.get_element_by_accessibility_id(
+            ColumnsAndFiltersSelectionWindowsDesktopPage.BACK_BUTTON,
+            image_name=self.prepare_image_name(ColumnsAndFiltersSelectionWindowsDesktopPage.BACK_BUTTON)
+        ).click()
+
+    def click_data_preview(self):
+        self.get_element_by_accessibility_id(
+            ColumnsAndFiltersSelectionWindowsDesktopPage.DATA_PREVIEW_BUTTON,
+            image_name=self.prepare_image_name(ColumnsAndFiltersSelectionWindowsDesktopPage.DATA_PREVIEW_BUTTON)
+        ).click()
+
+    def click_cancel_button(self):
+        self.get_element_by_accessibility_id(
+            ColumnsAndFiltersSelectionWindowsDesktopPage.CANCEL_BUTTON,
+            image_name=self.prepare_image_name(ColumnsAndFiltersSelectionWindowsDesktopPage.CANCEL_BUTTON)
+        ).click()
+    
+    def click_close_preview(self):
+        self.get_element_by_name(
+            ColumnsAndFiltersSelectionWindowsDesktopPage.CLOSE_PREVIEW_BUTTON,
+            image_name=self.prepare_image_name(ColumnsAndFiltersSelectionWindowsDesktopPage.CLOSE_PREVIEW_BUTTON)
+        ).click()
+
+    def click_include_totals_and_subtotals(self):
+        self.get_element_by_xpath(
+            ColumnsAndFiltersSelectionWindowsDesktopPage.TOTALS_AND_SUBTOTALS_SWITCH,
+            image_name=self.prepare_image_name(ColumnsAndFiltersSelectionWindowsDesktopPage.TOTALS_AND_SUBTOTALS_SWITCH)
+        ).move_to_and_click(offset_x=90, offset_y=2) # need to move and click beacuse switch element xpath is the same like for View selected switch
