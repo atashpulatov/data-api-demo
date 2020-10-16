31b67a2486ebfac1413140638d744b34f68ae0dc
Suchanecki, Maciej <msuchanecki@microstrategy.com>
2020-10-16 14:58:23 +0200

    Changed import of MstrException

--- tests/integration/python/pages/filter_panel/filter_panel_windows_desktop_page.py
+++ tests/integration/python/pages/filter_panel/filter_panel_windows_desktop_page.py
@@ -1,5 +1,5 @@
 from framework.pages_base.base_windows_desktop_page import BaseWindowsDesktopPage
-from framework.util.exception import MstrException
+from framework.util.exception.MstrException import MstrException
 
 
 class FilterPanelWindowsDesktopPage(BaseWindowsDesktopPage):
