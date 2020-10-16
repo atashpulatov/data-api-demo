a3e9d9cabf29d643f716ef74f350e7958b350f52
Thomas Cunningham <tcunningham@microstrategy.com>
2020-10-16 12:29:49 +0200

    Added steps for TC49134 in its current state. Subject to change once it is updated

--- tests/integration/python/pages/excel/excel_sheet/excel_sheet_windows_desktop_page.py
+++ tests/integration/python/pages/excel/excel_sheet/excel_sheet_windows_desktop_page.py
@@ -5,14 +5,7 @@ from framework.pages_base.image_element import ImageElement
 from framework.util.excel_util import ExcelUtil
 from framework.util.exception.MstrException import MstrException
 
-
 class ExcelSheetWindowsDesktopPage(BaseWindowsDesktopPage):
-    NAME_BOX_ELEM = 'Name Box'
-    HOME_MENU_ITEM = 'Home'
-    NUMBER_FORMAT_ELEM = 'Number Format'
-    MORE_NUMBER_FORMATS_ELEM = 'More Number Formats...'
-    SAMPLE_ELEM = 'Sample'
-
     VALUE_ATTRIBUTE = 'Value.Value'
 
     BOOK_ELEM = 'Book1'
@@ -20,6 +13,10 @@ class ExcelSheetWindowsDesktopPage(BaseWindowsDesktopPage):
     NAME_ATTRIBUTE = 'Name'
     SHEET_TAB_NAME = 'Sheet Tab'
     ADD_SHEET_BUTTON = 'Sheet Tab Add Sheet'
+    
+    GREEN_TABLE_STYLE_XPATH = 'Window/Pane/ToolBar/Pane/Pane/Pane/Pane/Pane/Group/Group/Group/MenuItem/Pane/DataGrid/ListItem[@Name="Light Green, Table Style Light 21"]'
+
+    PAUSE_IN_SECONDS = 2
 
     def get_cells_values(self, cells):
         result = []
@@ -38,10 +35,20 @@ class ExcelSheetWindowsDesktopPage(BaseWindowsDesktopPage):
 
     def go_to_cell(self, cell):
         cell_upper = cell.upper()
+        #Keys required to launch Find & Select menu in the Home tab.
+        self._navigate_to_home_tab_using_accessibility_keys()
 
-        self.get_elements_by_name(ExcelSheetWindowsDesktopPage.NAME_BOX_ELEM)[1].click()
+        ImageElement.excel_element.send_keys(("F", "D", "G"))
 
-        ImageElement.excel_element.send_keys((cell_upper, Keys.ENTER))
+        self.pause(ExcelSheetWindowsDesktopPage.PAUSE_IN_SECONDS)
+
+        ImageElement.excel_element.send_keys(cell_upper)
+
+        self.pause(ExcelSheetWindowsDesktopPage.PAUSE_IN_SECONDS)
+
+        ImageElement.excel_element.send_keys(Keys.ENTER)
+
+        self.pause(ExcelSheetWindowsDesktopPage.PAUSE_IN_SECONDS)
 
     def _get_selected_cell_value(self, cell):
         cell_selector_name = self._get_selector_name(cell)
@@ -66,6 +73,19 @@ class ExcelSheetWindowsDesktopPage(BaseWindowsDesktopPage):
                 result.append(char)
 
         return ''.join(result)
+
+    def _navigate_to_home_tab_using_accessibility_keys(self):
+        self._navigate_using_accessibility_keys()
+
+        ImageElement.excel_element.send_keys("H")
+
+        self.pause(ExcelSheetWindowsDesktopPage.PAUSE_IN_SECONDS)
+
+    def _navigate_using_accessibility_keys(self):
+        ImageElement.excel_element.send_keys(Keys.ALT)
+
+        self.pause(ExcelSheetWindowsDesktopPage.PAUSE_IN_SECONDS)
+
 
     def get_number_of_worksheets(self):
         book_element = self.get_element_by_name(ExcelSheetWindowsDesktopPage.BOOK_ELEM)
@@ -101,3 +121,91 @@ class ExcelSheetWindowsDesktopPage(BaseWindowsDesktopPage):
                     return
 
         raise MstrException('Cannot open worksheet number: %s.' % worksheet_number)
+
+    def click_table_design_tab(self):
+
+        ImageElement.excel_element.send_keys(Keys.ALT)
+
+        ImageElement.excel_element.send_keys(("J", "T"))
+
+        ImageElement.excel_element.send_keys(Keys.ALT)
+
+        self.pause(ExcelSheetWindowsDesktopPage.PAUSE_IN_SECONDS)
+    
+    def click_green_table_style(self):
+        self.get_elements_by_xpath(ExcelSheetWindowsDesktopPage.GREEN_TABLE_STYLE_XPATH)[0].click()
+
+    def click_home_tab(self):
+        self._navigate_to_home_tab_using_accessibility_keys()
+        #Turn off accessibility selectors
+        ImageElement.excel_element.send_keys(Keys.ALT)
+
+        self.pause(ExcelSheetWindowsDesktopPage.PAUSE_IN_SECONDS)
+
+    def click_percentage_button(self):
+        self._navigate_to_home_tab_using_accessibility_keys()
+
+        ImageElement.excel_element.send_keys("P")
+
+        self.pause(ExcelSheetWindowsDesktopPage.PAUSE_IN_SECONDS)
+
+    def click_comma_style_button(self):
+        self._navigate_to_home_tab_using_accessibility_keys()
+
+        ImageElement.excel_element.send_keys("K")
+
+        self.pause(ExcelSheetWindowsDesktopPage.PAUSE_IN_SECONDS)
+
+    def click_align_middle_button(self):
+        self._navigate_to_home_tab_using_accessibility_keys()
+
+        ImageElement.excel_element.send_keys(("A", "M"))
+
+        self.pause(ExcelSheetWindowsDesktopPage.PAUSE_IN_SECONDS)
+
+    def click_align_left_button(self):
+        self._navigate_to_home_tab_using_accessibility_keys()
+
+        ImageElement.excel_element.send_keys(("A", "L"))
+
+        self.pause(ExcelSheetWindowsDesktopPage.PAUSE_IN_SECONDS)
+        
+    def click_bold_button(self):
+        self._navigate_to_home_tab_using_accessibility_keys()
+
+        ImageElement.excel_element.send_keys("1")
+
+        self.pause(ExcelSheetWindowsDesktopPage.PAUSE_IN_SECONDS)
+        
+    def click_font_color_button(self):
+        # TODO Select a specific font color. For now, just select the first
+        self._navigate_to_home_tab_using_accessibility_keys()
+
+        ImageElement.excel_element.send_keys(("F", "C", Keys.ENTER))
+
+        self.pause(ExcelSheetWindowsDesktopPage.PAUSE_IN_SECONDS)
+
+    def click_fill_color_button(self):
+        # TODO Select a specific fill color. For now, just select the first
+        self._navigate_to_home_tab_using_accessibility_keys()
+
+        ImageElement.excel_element.send_keys(("H", Keys.ENTER))
+
+        self.pause(ExcelSheetWindowsDesktopPage.PAUSE_IN_SECONDS)
+
+    def change_font_name_of_cell(self, cell_name, font_name):
+        self.go_to_cell(cell_name)
+
+        self._navigate_to_home_tab_using_accessibility_keys()
+
+        ImageElement.excel_element.send_keys(("F", "F"))
+
+        self.pause(ExcelSheetWindowsDesktopPage.PAUSE_IN_SECONDS)
+
+        ImageElement.excel_element.send_keys(font_name)
+
+        self.pause(ExcelSheetWindowsDesktopPage.PAUSE_IN_SECONDS)
+
+        ImageElement.excel_element.send_keys(Keys.ENTER)
+
+        self.pause(ExcelSheetWindowsDesktopPage.PAUSE_IN_SECONDS)
