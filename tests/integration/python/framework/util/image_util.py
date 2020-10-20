8bcadf0d49378e6c0871e311aa44e18f60f52892
pobrebski <pobrebski@microstrategy.com>
2020-10-20 15:09:23 +0200

    Moved logic of object_name trimming to ImageUtil class.

--- tests/integration/python/framework/util/image_util.py
+++ tests/integration/python/framework/util/image_util.py
@@ -51,11 +51,27 @@ class ImageUtil:
         screenshots_folder = self._get_screenshots_folder()
 
         file_name_prefix_alpha = ImageUtil.TO_ALPHA_REGEX.sub('_', file_name_prefix)
+        file_name_prefix_alpha = self.prepare_file_name_string(file_name_prefix_alpha)
         file_name = ''.join((file_name_prefix_alpha.lower(), ImageUtil.SCREENSHOT_FILE_EXTENSION))
 
         file_path = os.path.join(screenshots_folder, file_name)
 
         return file_path
+
+    def prepare_file_name_string(self, file_name):
+        """
+        Checks if the file_name is longer than MAX_LENGTH_OF_OBJECT_NAME and if so hash the ending.
+        """
+        MAX_LENGTH_OF_OBJECT_NAME = 100
+
+        file_name_return = file_name
+
+        if len(file_name) > MAX_LENGTH_OF_OBJECT_NAME:
+            file_name_hashed = '_' + str(hash(file_name))
+            file_name_trimmed = file_name[0:(MAX_LENGTH_OF_OBJECT_NAME-len(file_name_hashed))]
+            file_name_return = file_name_trimmed + file_name_hashed
+
+        return file_name_return
 
     def _get_screenshots_folder(self):
         screenshots_folder = ConfigUtil.get_image_recognition_screenshots_folder()
