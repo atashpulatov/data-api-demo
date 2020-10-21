7c9280f60e97ac207d7db2523562db980cff7b5c
Ryszard Raszplewicz <rraszplewicz@microstrategy.com>
2020-10-21 12:58:41 +0200

    Review.

--- tests/integration/python/pages/excel/excel_sheet/excel_sheet_windows_desktop_page.py
+++ tests/integration/python/pages/excel/excel_sheet/excel_sheet_windows_desktop_page.py
@@ -1,11 +1,10 @@
-from selenium.webdriver.common.keys import Keys
 from pyperclip import paste
+from selenium.webdriver.common.keys import Keys
 
 from framework.pages_base.base_windows_desktop_page import BaseWindowsDesktopPage
-from framework.pages_base.image_element import ImageElement
+from framework.util.const import AFTER_OPERATION_WAIT_TIME
 from framework.util.excel_util import ExcelUtil
 from framework.util.exception.MstrException import MstrException
-from framework.util.const import AFTER_OPERATION_WAIT_TIME
 
 
 class ExcelSheetWindowsDesktopPage(BaseWindowsDesktopPage):
@@ -17,10 +16,10 @@ class ExcelSheetWindowsDesktopPage(BaseWindowsDesktopPage):
     SHEET_TAB_NAME = 'Sheet Tab'
     ADD_SHEET_BUTTON = 'Sheet Tab Add Sheet'
 
-    TABLE_STYLE_XPATH = '//DataGrid[@Name="Quick Styles"]/Group/ListItem[@Name="{}"]'
+    TABLE_STYLE_XPATH = '//DataGrid[@Name="Quick Styles"]/Group/ListItem[@Name="%s"]'
 
-    FONT_COLOR_XPATH = '//DataGrid[@Name="Font Color"]/Group/ListItem[@Name="{}"]'
-    FILL_COLOR_XPATH = '//DataGrid[@Name="Fill Color"]/Group/ListItem[@Name="{}"]'
+    FONT_COLOR_XPATH = '//DataGrid[@Name="Font Color"]/Group/ListItem[@Name="%s"]'
+    FILL_COLOR_XPATH = '//DataGrid[@Name="Fill Color"]/Group/ListItem[@Name="%s"]'
 
     ALIGN_MIDDLE_BUTTON = "Middle Align"
     ALIGN_LEFT_BUTTON = "Align Left"
@@ -37,7 +36,7 @@ class ExcelSheetWindowsDesktopPage(BaseWindowsDesktopPage):
         return result
 
     def _get_cell_value(self, cell):
-        # first go to cell to ensure it's visible (scrolled to)
+        # First go to cell to ensure it's visible (scrolled to).
         self.go_to_cell(cell)
 
         value = self._get_selected_cell_value(cell)
@@ -46,31 +45,21 @@ class ExcelSheetWindowsDesktopPage(BaseWindowsDesktopPage):
 
     def go_to_cell(self, cell):
         cell_upper = cell.upper()
-        # Keys required to launch Find & Select menu in the Home tab.
-        self._navigate_to_home_tab_using_accessibility_keys()
 
-        ImageElement.excel_element.send_keys(("F", "D", "G"))
+        self._navigate_to_home_tab_and_press('fdg')
 
-        self.pause(AFTER_OPERATION_WAIT_TIME)
+        self.send_keys(cell_upper)
 
-        ImageElement.excel_element.send_keys(cell_upper)
-
-        self.pause(AFTER_OPERATION_WAIT_TIME)
-
-        ImageElement.excel_element.send_keys(Keys.ENTER)
-
-        self.pause(AFTER_OPERATION_WAIT_TIME)
+        self.press_enter()
 
     def _get_selected_cell_value(self, cell):
         cell_selector_name = self._get_selector_name(cell)
 
         cell_elem = self.get_element_by_name(cell_selector_name)
 
-        cell_value = cell_elem.get_attribute(
-            ExcelSheetWindowsDesktopPage.VALUE_ATTRIBUTE)
+        cell_value = cell_elem.get_attribute(ExcelSheetWindowsDesktopPage.VALUE_ATTRIBUTE)
 
-        formatted_cell_value = ExcelUtil.format_cell_value(
-            cell_value) if cell_value else ''
+        formatted_cell_value = ExcelUtil.format_cell_value(cell_value) if cell_value else ''
 
         return formatted_cell_value
 
@@ -87,45 +76,31 @@ class ExcelSheetWindowsDesktopPage(BaseWindowsDesktopPage):
 
         return ''.join(result)
 
-    def _navigate_to_home_tab_using_accessibility_keys(self):
-        self._navigate_using_accessibility_keys()
-
-        ImageElement.excel_element.send_keys("H")
-
-        self.pause(AFTER_OPERATION_WAIT_TIME)
-
-    def _navigate_using_accessibility_keys(self):
-        ImageElement.excel_element.send_keys(Keys.ALT)
-
-        self.pause(AFTER_OPERATION_WAIT_TIME)
-
     def get_number_of_worksheets(self):
-        book_element = self.get_element_by_name(
-            ExcelSheetWindowsDesktopPage.BOOK_ELEM)
-        book_children_elements = book_element.get_elements_by_xpath(
-            ExcelSheetWindowsDesktopPage.BOOK_CHILDREN_ELEMS)
+        book_element = self.get_element_by_name(ExcelSheetWindowsDesktopPage.BOOK_ELEM)
+        book_children_elements = book_element.get_elements_by_xpath(ExcelSheetWindowsDesktopPage.BOOK_CHILDREN_ELEMS)
 
         sheet_tab_elements = list(
-            filter(lambda item: item.get_attribute(
-                ExcelSheetWindowsDesktopPage.NAME_ATTRIBUTE).startswith(ExcelSheetWindowsDesktopPage.SHEET_TAB_NAME),
-                book_children_elements))
+            filter(
+                lambda item: item.get_attribute(
+                    ExcelSheetWindowsDesktopPage.NAME_ATTRIBUTE
+                ).startswith(ExcelSheetWindowsDesktopPage.SHEET_TAB_NAME), book_children_elements
+            )
+        )
 
         return len(sheet_tab_elements)
 
     def add_worksheet(self):
         self.get_element_by_name(
             ExcelSheetWindowsDesktopPage.ADD_SHEET_BUTTON,
-            image_name=self.prepare_image_name(
-                ExcelSheetWindowsDesktopPage.ADD_SHEET_BUTTON)
+            image_name=self.prepare_image_name(ExcelSheetWindowsDesktopPage.ADD_SHEET_BUTTON)
         ).click()
 
     def open_worksheet(self, worksheet_number):
         worksheet_number_int = int(worksheet_number)
 
-        book_element = self.get_element_by_name(
-            ExcelSheetWindowsDesktopPage.BOOK_ELEM)
-        book_children_elements = book_element.get_elements_by_xpath(
-            ExcelSheetWindowsDesktopPage.BOOK_CHILDREN_ELEMS)
+        book_element = self.get_element_by_name(ExcelSheetWindowsDesktopPage.BOOK_ELEM)
+        book_children_elements = book_element.get_elements_by_xpath(ExcelSheetWindowsDesktopPage.BOOK_CHILDREN_ELEMS)
 
         i = 0
         for child in book_children_elements:
@@ -137,98 +112,60 @@ class ExcelSheetWindowsDesktopPage(BaseWindowsDesktopPage):
                     child.click()
                     return
 
-        raise MstrException(
-            'Cannot open worksheet number: %s.' % worksheet_number)
+        raise MstrException('Cannot open worksheet number: %s.' % worksheet_number)
 
     def click_table_design_tab(self):
-
-        ImageElement.excel_element.send_keys(Keys.ALT)
-
-        ImageElement.excel_element.send_keys(("J", "T"))
-
-        ImageElement.excel_element.send_keys(Keys.ALT)
-
-        self.pause(AFTER_OPERATION_WAIT_TIME)
+        self.send_keys_using_excel_element(Keys.ALT)
+        self.send_keys_using_excel_element(('j', 't'))
+        self.send_keys_using_excel_element(Keys.ALT)
 
     def click_green_table_style(self):
-
-        ImageElement.excel_element.send_keys(Keys.ALT)
-
-        ImageElement.excel_element.send_keys(("J", "T", "S"))
+        self.send_keys_using_excel_element(Keys.ALT)
+        self.send_keys_using_excel_element(('j', 't', 's'))
 
         self.get_element_by_xpath(
-            ExcelSheetWindowsDesktopPage.TABLE_STYLE_XPATH.format(ExcelSheetWindowsDesktopPage.LIGHT_GREEN_TABLE)).click()
+            ExcelSheetWindowsDesktopPage.TABLE_STYLE_XPATH % ExcelSheetWindowsDesktopPage.LIGHT_GREEN_TABLE
+        ).click()
 
         self.pause(AFTER_OPERATION_WAIT_TIME)
 
     def click_home_tab(self):
-        self._navigate_to_home_tab_using_accessibility_keys()
-        # Turn off accessibility selectors
-        ImageElement.excel_element.send_keys(Keys.ALT)
-
-        self.pause(AFTER_OPERATION_WAIT_TIME)
+        self.send_keys_using_excel_element(Keys.ALT)
+        self.send_keys_using_excel_element('h')
+        self.send_keys_using_excel_element(Keys.ALT)
 
     def click_percentage_button(self):
-        self._navigate_to_home_tab_using_accessibility_keys()
-
-        ImageElement.excel_element.send_keys("P")
-
-        self.pause(AFTER_OPERATION_WAIT_TIME)
+        self._navigate_to_home_tab_and_press('p')
 
     def click_comma_style_button(self):
-        self._navigate_to_home_tab_using_accessibility_keys()
-
-        ImageElement.excel_element.send_keys("K")
-
-        self.pause(AFTER_OPERATION_WAIT_TIME)
+        self._navigate_to_home_tab_and_press('k')
 
     def click_align_middle_button(self):
-        self._navigate_to_home_tab_using_accessibility_keys()
-
-        ImageElement.excel_element.send_keys(("A", "M"))
-
-        self.pause(AFTER_OPERATION_WAIT_TIME)
+        self._navigate_to_home_tab_and_press('am')
 
     def click_align_left_button(self):
-        self._navigate_to_home_tab_using_accessibility_keys()
-
-        ImageElement.excel_element.send_keys(("A", "L"))
-
-        self.pause(AFTER_OPERATION_WAIT_TIME)
+        self._navigate_to_home_tab_and_press('al')
 
     def click_bold_button(self):
-        self._navigate_to_home_tab_using_accessibility_keys()
-
-        ImageElement.excel_element.send_keys("1")
-
-        self.pause(AFTER_OPERATION_WAIT_TIME)
+        self._navigate_to_home_tab_and_press('1')
 
     def click_font_color_button(self):
         # TODO Select a specific font color. For now, hardcode
-        self._navigate_to_home_tab_using_accessibility_keys()
-
-        ImageElement.excel_element.send_keys(("F", "C"))
+        self._navigate_to_home_tab_and_press('fc')
 
         self.get_element_by_xpath(
-            ExcelSheetWindowsDesktopPage.FONT_COLOR_XPATH.format(ExcelSheetWindowsDesktopPage.LIGHT_GREEN)).click()
+            ExcelSheetWindowsDesktopPage.FONT_COLOR_XPATH % ExcelSheetWindowsDesktopPage.LIGHT_GREEN
+        ).click()
 
         self.pause(AFTER_OPERATION_WAIT_TIME)
 
     def click_fill_color_button(self):
         # TODO Select a specific fill color. For now, hardcode
-        self._navigate_to_home_tab_using_accessibility_keys()
-
-        ImageElement.excel_element.send_keys(("H"))
+        self._navigate_to_home_tab_and_press('h')
 
         self.get_element_by_xpath(
-            ExcelSheetWindowsDesktopPage.FILL_COLOR_XPATH.format(ExcelSheetWindowsDesktopPage.LIGHT_GREEN)).click()
-
-        self.pause(AFTER_OPERATION_WAIT_TIME)
-
-    def _select_font_name_combo_box(self):
-        self._navigate_to_home_tab_using_accessibility_keys()
-
-        ImageElement.excel_element.send_keys(("F", "F"))
+            ExcelSheetWindowsDesktopPage.FILL_COLOR_XPATH % ExcelSheetWindowsDesktopPage.LIGHT_GREEN
+        ).click()
 
         self.pause(AFTER_OPERATION_WAIT_TIME)
 
@@ -237,13 +174,8 @@ class ExcelSheetWindowsDesktopPage(BaseWindowsDesktopPage):
 
         self._select_font_name_combo_box()
 
-        ImageElement.excel_element.send_keys(font_name)
-
-        self.pause(AFTER_OPERATION_WAIT_TIME)
-
-        ImageElement.excel_element.send_keys(Keys.ENTER)
-
-        self.pause(AFTER_OPERATION_WAIT_TIME)
+        self.send_keys(font_name)
+        self.press_enter()
 
     def is_align_middle_button_selected(self, cell_name):
         return self._is_button_selected(cell_name, ExcelSheetWindowsDesktopPage.ALIGN_MIDDLE_BUTTON)
@@ -259,37 +191,37 @@ class ExcelSheetWindowsDesktopPage(BaseWindowsDesktopPage):
 
         return self.get_element_by_name(name).is_selected()
 
-    def is_green_table_selected(self):
-        ImageElement.excel_element.send_keys(Keys.ALT)
-
-        ImageElement.excel_element.send_keys(("J", "T", "S"))
-
-        return self.get_element_by_xpath(
-            ExcelSheetWindowsDesktopPage.TABLE_STYLE_XPATH.format(ExcelSheetWindowsDesktopPage.LIGHT_GREEN_TABLE)).is_selected
-
     def is_font_color_selected(self):
-      # TODO Select a specific font color. For now, hardcode
-        self._navigate_to_home_tab_using_accessibility_keys()
-
-        ImageElement.excel_element.send_keys(("F", "C"))
+        # TODO Select a specific font color. For now, hardcode
+        self._navigate_to_home_tab_and_press('fc')
 
         return self.get_element_by_xpath(
-            ExcelSheetWindowsDesktopPage.FONT_COLOR_XPATH.format(ExcelSheetWindowsDesktopPage.LIGHT_GREEN)).is_selected()
+            ExcelSheetWindowsDesktopPage.FONT_COLOR_XPATH % ExcelSheetWindowsDesktopPage.LIGHT_GREEN
+        ).is_selected()
 
     def is_fill_color_selected(self):
-      # TODO Select a specific fill color. For now, hardcode
-        self._navigate_to_home_tab_using_accessibility_keys()
-
-        ImageElement.excel_element.send_keys(("H"))
+        # TODO Select a specific fill color. For now, hardcode
+        self._navigate_to_home_tab_and_press('h')
 
         return self.get_element_by_xpath(
-            ExcelSheetWindowsDesktopPage.FILL_COLOR_XPATH.format(ExcelSheetWindowsDesktopPage.LIGHT_GREEN)).is_selected()
+            ExcelSheetWindowsDesktopPage.FILL_COLOR_XPATH % ExcelSheetWindowsDesktopPage.LIGHT_GREEN
+        ).is_selected()
 
     def get_font_name_of_cell(self, cell_name):
         self.go_to_cell(cell_name)
 
         self._select_font_name_combo_box()
 
-        ImageElement.excel_element.send_keys(Keys.CONTROL + "c")
+        self.send_keys(Keys.CONTROL + 'c')
+        self.send_keys(Keys.CONTROL)
+        self.press_escape()
 
         return paste()
+
+    def _select_font_name_combo_box(self):
+        self._navigate_to_home_tab_and_press('ff')
+
+    def _navigate_to_home_tab_and_press(self, keys):
+        self.send_keys(Keys.ALT + 'h')
+
+        self.send_keys(Keys.ALT + keys)
--- tests/integration/python/pages/import_data/import_data_windows_desktop_page.py
+++ tests/integration/python/pages/import_data/import_data_windows_desktop_page.py
@@ -1,4 +1,5 @@
 from pyperclip import paste
+from selenium.webdriver.common.keys import Keys
 
 from framework.pages_base.base_windows_desktop_page import BaseWindowsDesktopPage
 from framework.pages_base.windows_desktop_workaround import WindowsDesktopWorkaround
@@ -49,19 +50,16 @@ class ImportDataWindowsDesktopPage(BaseWindowsDesktopPage):
         # TODO check if is on or ignore to have better performance?
         self.get_element_by_name(
             ImportDataWindowsDesktopPage.MY_LIBRARY_SWITCH_ELEM,
-            image_name=self.prepare_image_name(
-                ImportDataWindowsDesktopPage.MY_LIBRARY_SWITCH_ELEM)
+            image_name=self.prepare_image_name(ImportDataWindowsDesktopPage.MY_LIBRARY_SWITCH_ELEM)
         ).click()
 
     def _is_on(self, element):
         aria_properties_value = self._get_element_aria_properties(element)
 
-        aria_properties = aria_properties_value.split(
-            ImportDataWindowsDesktopPage.ARIA_PROPERTIES_SEPARATOR)
+        aria_properties = aria_properties_value.split(ImportDataWindowsDesktopPage.ARIA_PROPERTIES_SEPARATOR)
 
         for property_value in aria_properties:
-            property_on_off = property_value.split(
-                ImportDataWindowsDesktopPage.ARIA_PROPERTY_SEPARATOR)
+            property_on_off = property_value.split(ImportDataWindowsDesktopPage.ARIA_PROPERTY_SEPARATOR)
             if property_on_off[0] == ImportDataWindowsDesktopPage.ARIA_PROPERTY_CHECKED:
                 return property_on_off[1]
 
@@ -80,15 +78,19 @@ class ImportDataWindowsDesktopPage(BaseWindowsDesktopPage):
 
         filters_elem = self.get_element_by_name(
             ImportDataWindowsDesktopPage.FILTERS_BUTTON_ELEM,
-            image_name=self.prepare_image_name(
-                ImportDataWindowsDesktopPage.FILTERS_BUTTON_ELEM)
+            image_name=self.prepare_image_name(ImportDataWindowsDesktopPage.FILTERS_BUTTON_ELEM)
         )
         filters_elem.click(
             offset_x=ImportDataWindowsDesktopPage.SEARCH_ELEM_OFFSET_X,
             offset_y=ImportDataWindowsDesktopPage.SEARCH_ELEM_OFFSET_Y
         )
 
-        self.send_keys(object_name)
+        # Remove search box content.
+        self.send_keys((Keys.CONTROL, 'a', Keys.CONTROL, Keys.DELETE))
+
+        if object_name:
+            # Enter object_name.
+            self.send_keys(object_name)
 
     def find_and_select_object(self, object_name):
         """
@@ -108,8 +110,7 @@ class ImportDataWindowsDesktopPage(BaseWindowsDesktopPage):
 
         Util.pause(4)  # TODO wait when ready
 
-        self.get_element_by_name(
-            object_name, image_name=self.prepare_image_name(object_name)).click()
+        self.get_element_by_name(object_name, image_name=self.prepare_image_name(object_name)).click()
 
     def click_import_button(self):
         self.windows_desktop_workaround.focus_on_popup_window()
@@ -136,8 +137,7 @@ class ImportDataWindowsDesktopPage(BaseWindowsDesktopPage):
 
         self.get_element_by_name(
             ImportDataWindowsDesktopPage.ERROR_MESSAGE_BUTTON_OK,
-            image_name=self.prepare_image_name(
-                ImportDataWindowsDesktopPage.ERROR_MESSAGE_BUTTON_OK)
+            image_name=self.prepare_image_name(ImportDataWindowsDesktopPage.ERROR_MESSAGE_BUTTON_OK)
         ).click()
 
     def click_import_button_to_open_import_dossier(self):
@@ -150,8 +150,7 @@ class ImportDataWindowsDesktopPage(BaseWindowsDesktopPage):
     def _click_import_button(self):
         self.get_element_by_name(
             ImportDataWindowsDesktopPage.IMPORT_BUTTON_ELEM,
-            image_name=self.prepare_image_name(
-                ImportDataWindowsDesktopPage.IMPORT_BUTTON_ELEM)
+            image_name=self.prepare_image_name(ImportDataWindowsDesktopPage.IMPORT_BUTTON_ELEM)
         ).click()
 
     def click_prepare_data_button(self):
@@ -159,8 +158,7 @@ class ImportDataWindowsDesktopPage(BaseWindowsDesktopPage):
 
         self.get_element_by_name(
             ImportDataWindowsDesktopPage.PREPARE_DATA_BUTTON_ELEM,
-            image_name=self.prepare_image_name(
-                ImportDataWindowsDesktopPage.PREPARE_DATA_BUTTON_ELEM)
+            image_name=self.prepare_image_name(ImportDataWindowsDesktopPage.PREPARE_DATA_BUTTON_ELEM)
         ).click()
 
     def show_object_details(self, object_number):
@@ -168,8 +166,7 @@ class ImportDataWindowsDesktopPage(BaseWindowsDesktopPage):
 
         object_index = int(object_number) - 1
 
-        elements = self.get_elements_by_name(
-            ImportDataWindowsDesktopPage.SHOW_DETAILS)
+        elements = self.get_elements_by_name(ImportDataWindowsDesktopPage.SHOW_DETAILS)
 
         elements[object_index].click()
 
@@ -205,27 +202,23 @@ class ImportDataWindowsDesktopPage(BaseWindowsDesktopPage):
     def click_filters_button(self):
         self.get_element_by_name(
             ImportDataWindowsDesktopPage.FILTERS_BUTTON_ELEM,
-            image_name=self.prepare_image_name(
-                ImportDataWindowsDesktopPage.FILTERS_BUTTON_ELEM)
+            image_name=self.prepare_image_name(ImportDataWindowsDesktopPage.FILTERS_BUTTON_ELEM)
         ).click()
 
     def hover_over_first_object_in_list(self):
-        self.get_add_in_main_element().get_element_by_xpath(
-            ImportDataWindowsDesktopPage.FIRST_OBJECT_ROW).move_to()
+        self.get_add_in_main_element().get_element_by_xpath(ImportDataWindowsDesktopPage.FIRST_OBJECT_ROW).move_to()
 
     def select_first_object_from_list(self):
-        self.get_add_in_main_element().get_element_by_xpath(
-            ImportDataWindowsDesktopPage.FIRST_OBJECT_ROW).click()
+        self.get_add_in_main_element().get_element_by_xpath(ImportDataWindowsDesktopPage.FIRST_OBJECT_ROW).click()
 
     def clear_search_box(self):
-        self.find_object("")
+        self.find_object('')
 
     def hover_over_import_button(self):
         self.get_element_by_name(
             ImportDataWindowsDesktopPage.IMPORT_BUTTON_ELEM,
-            image_name=self.prepare_image_name(
-                ImportDataWindowsDesktopPage.IMPORT_BUTTON_ELEM)
+            image_name=self.prepare_image_name(ImportDataWindowsDesktopPage.IMPORT_BUTTON_ELEM)
         ).move_to()
 
     def get_tooltip_message_for_button(self):
-        return self.get_element_by_xpath(ImportDataWindowsDesktopPage.TOOLTIP_XPATH).get_attribute("Name")
+        return self.get_element_by_xpath(ImportDataWindowsDesktopPage.TOOLTIP_XPATH).get_name_by_attribute()
