aba04ff6190a82b2a63723bb1ffe0575197258d5
Wojtaszko, Pawel <pwojtaszko@microstrategy.com>
2020-10-23 14:53:08 +0200

    Added comment

--- tests/integration/python/pages/excel/excel_menu/excel_menu_windows_desktop_page.py
+++ tests/integration/python/pages/excel/excel_menu/excel_menu_windows_desktop_page.py
@@ -16,6 +16,8 @@ class ExcelMenuWindowsDesktopPage(BaseWindowsDesktopPage):
     def click_close_add_in_button(self):
         MENU_BUTTON_WIDTH = 40
         MENU_BUTTONS_MARGIN = 20
+        # Close addin button ('X') is invisible in xpath.
+        # Workaround is to find button next to this element and move to the right
         self.get_element_by_name(
             ExcelMenuWindowsDesktopPage.MENU_BUTTON
         ).move_to_and_click(offset_x=MENU_BUTTON_WIDTH+MENU_BUTTONS_MARGIN, offset_y=10)
