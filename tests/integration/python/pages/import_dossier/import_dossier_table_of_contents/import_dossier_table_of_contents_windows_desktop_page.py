3f1a85960f82b0af7573635856ab09e0edd5e296
Suchanecki, Maciej <msuchanecki@microstrategy.com>
2020-10-23 14:10:42 +0200

    Created ImportDossierTableOfContentsWindowsDesktopPage

---
+++ tests/integration/python/pages/import_dossier/import_dossier_table_of_contents/import_dossier_table_of_contents_windows_desktop_page.py
@@ -0,0 +1,24 @@
+from framework.pages_base.base_windows_desktop_page import BaseWindowsDesktopPage
+from framework.util.exception.MstrException import MstrException
+
+
+class ImportDossierTableOfContentsWindowsDesktopPage(BaseWindowsDesktopPage):
+    TABLE_OF_CONTENT_BUTTON = 'Table of Contents'
+    TABLE_OF_CONTENT_ITEMS = '//Button[@Name="Table of Contents"]/following-sibling::List[1]//ListItem'
+
+    def select_table_of_content_option(self, option_number):
+        self.get_element_by_name(
+            ImportDossierTableOfContentsWindowsDesktopPage.TABLE_OF_CONTENT_BUTTON,
+            image_name=self.prepare_image_name(ImportDossierTableOfContentsWindowsDesktopPage.TABLE_OF_CONTENT_BUTTON)
+        ).click()
+
+        menu_items = self.get_elements_by_xpath(ImportDossierTableOfContentsWindowsDesktopPage.TABLE_OF_CONTENT_ITEMS)
+
+        option_index = int(option_number) - 1
+
+        if option_index > len(menu_items):
+            raise MstrException(
+                ('Not possible to click menu option, given object index is too big', option_index, menu_items)
+            )
+
+        menu_items[option_index].click()
