d1045f59a565cc079c2533aabcb7f7cca5ebec88
Suchanecki, Maciej <msuchanecki@microstrategy.com>
2020-10-23 15:41:40 +0200

    Changed order

--- tests/integration/python/pages/import_dossier/import_dossier_filter/import_dossier_filter_windows_desktop_page.py
+++ tests/integration/python/pages/import_dossier/import_dossier_filter/import_dossier_filter_windows_desktop_page.py
@@ -4,12 +4,10 @@ from framework.util.exception.MstrException import MstrException
 
 class ImportDossierFilterWindowsDesktopPage(BaseWindowsDesktopPage):
     FILTERS_BUTTON = 'Filter'
+    APPLY_FILTER_BUTTON = 'Apply'
 
     DOSSIER_FILTER_VALUE = '//Pane[@Name=\"Filter Data\"]//CheckBox[@Name=\"%s\"]'
-
     DOSSIER_FILTER_YEAR = '//Pane[@Name=\"Filter Data\"]//MenuItem[@Name=\"Year\"]'
-
-    APPLY_FILTER_BUTTON = 'Apply'
 
     def select_year_in_year_filter(self, year):
         self._open_year_filter()
