d96e5ba0fda4571b540a21b193b0bf4e5daa9352
pobrebski <pobrebski@microstrategy.com>
2020-10-27 16:50:07 +0100

    Implement methods in ExcelMenuWindowsDesktopPage class for binding.feature TC.

--- tests/integration/python/pages/excel/excel_menu/excel_menu_windows_desktop_page.py
+++ tests/integration/python/pages/excel/excel_menu/excel_menu_windows_desktop_page.py
@@ -1,8 +1,13 @@
 from framework.pages_base.base_windows_desktop_page import BaseWindowsDesktopPage
 from framework.util.config_util import ConfigUtil
+from selenium.webdriver.common.keys import Keys
+from pyperclip import paste
+import re
 
 
 class ExcelMenuWindowsDesktopPage(BaseWindowsDesktopPage):
+    NAME_BOX_ELEM = 'Name Box'
+
     def click_add_in_elem(self):
         import_data_name = ConfigUtil.get_excel_desktop_add_in_import_data_name()
 
@@ -12,14 +17,67 @@ class ExcelMenuWindowsDesktopPage(BaseWindowsDesktopPage):
         ).click()
 
     def select_object_from_name_box(self, object_number):
-        NAME_BOX_ELEM = 'Name Box'
 
+        self._go_to_object_from_name_box(object_number)
+
+        self.press_enter()
+
+    def is_object_name_in_name_box_correct(self, object_number, object_name):
+        """
+        Navigates to the Name Box element and picks ListItem number object_number.
+        Gets the name string and removes trailing digits from the name string and compares the result with object_name.
+
+        :param object_number(int): Number of element from Name Box dropdown
+        :param object_name(string): Name of object that we expect
+
+        :returns (bloean): Returns true if string from Name box and object_name variable is equal
+        """
+
+        self._go_to_object_from_name_box(object_number)
+
+        name_from_namebox = self._get_highlighted_string()
+        object_name_without_quotes = object_name[1:-1]
+        name_from_namebox_without_timestamp = re.sub(r'\d+$', '', name_from_namebox)
+
+        return object_name_without_quotes == name_from_namebox_without_timestamp
+
+    def are_timstamps_different(self, object_number_1, object_number_2):
+        """
+        Navigates to the Name Box element number object_number_1 and gets the name string.
+        Repeats this step for element number object_number_2.
+
+        Compares the strings and return true if they are not equal.
+
+        :param object_number_1(int): Number of first element from Name Box dropdown
+        :param object_number_2(int): Number of second element from Name Box dropdown
+
+        :returns (bloean): Returns true if names of objects are different
+        """
+
+        self._go_to_object_from_name_box(object_number_1)
+        name_1_from_namebox = self._get_highlighted_string()
+
+        self._go_to_object_from_name_box(object_number_2)
+        name_2_from_namebox = self._get_highlighted_string()
+
+        return name_1_from_namebox != name_2_from_namebox
+
+    def _go_to_object_from_name_box(self, object_number):
         self.get_element_by_name(
-          NAME_BOX_ELEM,
-          image_name=self.prepare_image_name(NAME_BOX_ELEM)
+          ExcelMenuWindowsDesktopPage.NAME_BOX_ELEM
         ).click()
 
         for i in range(int(object_number)):
             self.press_down_arrow()
 
-        self.press_enter()
+    def _get_highlighted_string(self):
+        '''
+        Copy and paste highlighted content. Returns pasted string.
+
+        :returns (str): String containing highlighted text
+        '''
+        self.send_keys(Keys.CONTROL + 'c')
+        self.send_keys(Keys.CONTROL)
+        clipboard_content = paste()
+
+        return clipboard_content
