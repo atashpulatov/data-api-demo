ac7dc6b6ff35cb560afe70c36c4dd86b052a4d6e
pobrebski <pobrebski@microstrategy.com>
2020-10-27 16:51:31 +0100

    Define new steps to execute TC59464 operations.

--- tests/integration/python/tests/steps/excel_menu_steps.py
+++ tests/integration/python/tests/steps/excel_menu_steps.py
@@ -1,4 +1,5 @@
 from behave import *
+from framework.util.assert_util import AssertUtil
 
 
 @step('I clicked close Add-in button')
@@ -14,3 +15,17 @@ def step_impl(context):
 @step('I selected object number {object_number} from Name Box')
 def step_impl(context, object_number):
     context.pages.excel_menu_page().select_object_from_name_box(object_number)
+
+
+@step('item number {object_number} in Name Box, ignoring timestamp at the end, should be called {object_name}')
+def step_impl(context, object_number, object_name):
+    result = context.pages.excel_menu_page().is_object_name_in_name_box_correct(object_number, object_name)
+
+    AssertUtil.assert_simple(result, True)
+
+
+@step('object number {object_number_1} and object number {object_number_2} in Name Box had different timestamps')
+def step_impl(context, object_number_1, object_number_2):
+    result = context.pages.excel_menu_page().are_timstamps_different(object_number_1, object_number_2)
+
+    AssertUtil.assert_simple(result, True)
