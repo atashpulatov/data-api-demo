4e04f6007f15f7a2f5784356e3aba74198ea4fbf
Suchanecki, Maciej <msuchanecki@microstrategy.com>
2020-10-27 14:40:27 +0100

    Updated clicking vis import button steps

--- tests/integration/python/pages/import_dossier/import_dossier_main/import_dossier_main_windows_desktop_page.py
+++ tests/integration/python/pages/import_dossier/import_dossier_main/import_dossier_main_windows_desktop_page.py
@@ -1,5 +1,6 @@
 from framework.pages_base.base_windows_desktop_page import BaseWindowsDesktopPage
 from framework.util.exception.MstrException import MstrException
+from pages.right_panel.right_panel_tile.right_panel_tile_windows_desktop_page import RightPanelTileWindowsDesktopPage
 
 
 class ImportDossierMainWindowsDesktopPage(BaseWindowsDesktopPage):
@@ -11,6 +12,10 @@ class ImportDossierMainWindowsDesktopPage(BaseWindowsDesktopPage):
 
     VISUALIZATION_MENU_BUTTON = 'Menu for %s'
     SHOW_DATA = 'Show Data'
+
+    def __init__(self):
+        super().__init__()
+        self.right_panel_tile_windows_desktop_page = RightPanelTileWindowsDesktopPage()
 
     def import_visualization_by_name(self, visualization_name):
         self.select_visualization_by_name(visualization_name)
@@ -24,12 +29,20 @@ class ImportDossierMainWindowsDesktopPage(BaseWindowsDesktopPage):
         self.pause(5)  # TODO wait when ready
 
     def click_import_visualization(self):
+        self.click_import_visualization_without_waiting_for_results()
+
+        self.right_panel_tile_windows_desktop_page.wait_for_import_object_to_finish_successfully()
+
+    def click_import_visualization_without_waiting_for_results(self):
         self.get_element_by_name(
             ImportDossierMainWindowsDesktopPage.IMPORT_BUTTON,
             image_name=self.prepare_image_name(ImportDossierMainWindowsDesktopPage.IMPORT_BUTTON)
         ).click()
 
-        self.pause(5)  # TODO wait when ready
+    def click_import_visualization_to_duplicate(self):
+        self.click_import_visualization_without_waiting_for_results()
+
+        self.right_panel_tile_windows_desktop_page.wait_for_duplicate_object_to_finish_successfully()
 
     def find_tile_by_name(self, visualization_name):
         popup_main_element = self.get_add_in_main_element()
