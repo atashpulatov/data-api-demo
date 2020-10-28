6e08fc1d962e92be8c44fa9b334b9b6f71c3afb8
Triinu Erik <terik@microstrategy.com>
2020-10-28 12:06:57 +0100

    Implement step "I scrolled into metric element number {object_number}" and refactor

--- tests/integration/python/pages/columns_and_filters_selection/metrics/columns_and_filters_selection_metrics_windows_desktop_page.py
+++ tests/integration/python/pages/columns_and_filters_selection/metrics/columns_and_filters_selection_metrics_windows_desktop_page.py
@@ -1,4 +1,7 @@
 from framework.pages_base.base_windows_desktop_page import BaseWindowsDesktopPage
+from framework.util.exception.MstrException import MstrException
+
+from selenium.webdriver.common.keys import Keys
 
 
 class ColumnsAndFiltersSelectionMetricsWindowsDesktopPage(BaseWindowsDesktopPage):
@@ -9,6 +12,11 @@ class ColumnsAndFiltersSelectionMetricsWindowsDesktopPage(BaseWindowsDesktopPage
     METRICS_XPATH = '//Group/DataGrid/Group/Text'
     METRICS_XPATH_AT = '//Group/DataGrid/Group/Text[%s]'
 
+    METRICS_CONTAINER = '//Group/DataGrid'
+
+    all_metrics = []
+
+    # TODO: refactor in case metric is not visible
     def click_metric(self, metric_name):
         popup_main_element = self.get_add_in_main_element()
 
@@ -29,14 +37,52 @@ class ColumnsAndFiltersSelectionMetricsWindowsDesktopPage(BaseWindowsDesktopPage
         ).click()
 
     def select_metric_by_number(self, object_number):
+        self._find_metric_by_number(object_number).click()
+
+    def _find_metric_by_number(self, object_number):
         popup_main_element = self.get_add_in_main_element()
 
-        popup_main_element.get_element_by_xpath(
-            ColumnsAndFiltersSelectionMetricsWindowsDesktopPage.METRICS_XPATH_AT % object_number
-        ).click()
+        visible_metrics = popup_main_element.get_elements_by_xpath(
+            ColumnsAndFiltersSelectionMetricsWindowsDesktopPage.METRICS_XPATH
+        )
+
+        if int(object_number) <= len(visible_metrics):
+            return visible_metrics[int(object_number) - 1]
+        else:
+            return self._get_all_metrics()[int(object_number) - 1]
+
+    #  Workaround as non-visible metrics are not in page source
+    def _get_all_metrics(self):
+        if self.all_metrics:
+            return self.all_metrics
+
+        popup_main_element = self.get_add_in_main_element()
+
+        metrics_container = popup_main_element.get_element_by_xpath(
+                ColumnsAndFiltersSelectionMetricsWindowsDesktopPage.METRICS_CONTAINER
+            )
+
+        all_metrics = []
+
+        for i in range(10):  # Cap the search for 10 loops
+            visible_metrics = popup_main_element.get_elements_by_xpath(
+                ColumnsAndFiltersSelectionMetricsWindowsDesktopPage.METRICS_XPATH
+            )
+            updated_metrics = all_metrics + list(filter(lambda m: m not in all_metrics, visible_metrics))
+
+            if updated_metrics == all_metrics:
+                self.all_metrics = all_metrics
+                return all_metrics
+            else:
+                all_metrics = updated_metrics
+
+            for j in range(4):  # Scroll at least 4 times before checking if attribute is visible
+                metrics_container.click(metrics_container.size['width'], metrics_container.size['height'])
+
+        raise MstrException('There are too many metrics in this dataset, search has been capped.')
 
     def get_metric_name(self, object_number):
-        popup_main_element = self.get_add_in_main_element()
-        metric_elements = popup_main_element.get_elements_by_xpath(ColumnsAndFiltersSelectionMetricsWindowsDesktopPage.METRICS_XPATH)
-        
-        return metric_elements[int(object_number) - 1].get_name_by_attribute()
+        self._find_metric_by_number(object_number).get_name_by_attribute()
+
+    def scroll_into_metric_by_number(self, object_number):
+        self._get_all_metrics()[int(object_number) - 1].move_to()
