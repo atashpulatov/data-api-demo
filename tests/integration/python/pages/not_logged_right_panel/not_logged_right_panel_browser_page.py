400fe797c47474f8d366755cfe9d7efee8a93063
Piotr Nastaj <pnastaj@microstrategy.com>
2020-11-16 13:52:19 +0100

    US325018; Update NotLoggedRightPanelBrowserPage, first part

--- tests/integration/python/pages/not_logged_right_panel/not_logged_right_panel_browser_page.py
+++ tests/integration/python/pages/not_logged_right_panel/not_logged_right_panel_browser_page.py
@@ -4,7 +4,8 @@ from framework.util.const import SHORT_TIMEOUT
 
 class NotLoggedRightPanelBrowserPage(BaseBrowserPage):
     OPEN_LOGIN_POPUP_BUTTON_ELEM = 'login-btn'
-    ACCEPT_COOKIES_BUTTON_ELEM = 'accept-cookies-btn'
+    ACCEPT_COOKIES_BUTTON_ID = 'accept-cookies-btn'
+    ACCEPT_COOKIES_WRAPPER_ID = 'cookies-needed'
 
     def enable_windows_desktop_workaround_if_needed(self):
         pass
@@ -17,10 +18,17 @@ class NotLoggedRightPanelBrowserPage(BaseBrowserPage):
         self.get_element_by_id(NotLoggedRightPanelBrowserPage.OPEN_LOGIN_POPUP_BUTTON_ELEM).click()
 
     def _enable_accept_cookies(self):
-        accept_cookies_button_exists = self.check_if_element_exists_by_id(
-            NotLoggedRightPanelBrowserPage.ACCEPT_COOKIES_BUTTON_ELEM,
+        self.log_page_source()
+        accept_cookies_wrapper = self.get_element_by_id_no_visibility_checked(
+            NotLoggedRightPanelBrowserPage.ACCEPT_COOKIES_WRAPPER_ID,
+            timeout=SHORT_TIMEOUT
+        )
+        accept_cookies_button = self.get_element_by_id_no_visibility_checked(
+            NotLoggedRightPanelBrowserPage.ACCEPT_COOKIES_BUTTON_ID,
             timeout=SHORT_TIMEOUT
         )
 
-        if accept_cookies_button_exists:
-            self.get_element_by_id(NotLoggedRightPanelBrowserPage.ACCEPT_COOKIES_BUTTON_ELEM).click()
+        accept_cookies_wrapper_display_value = accept_cookies_wrapper.get_attribute("display")
+
+        if accept_cookies_wrapper_display_value != "None":
+            accept_cookies_button.click()
