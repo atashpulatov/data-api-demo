83c3c35bcdc5b60c0218e3d117c218d103c86af8
Ryszard Raszplewicz <rraszplewicz@microstrategy.com>
2020-11-18 16:54:03 +0100

    Windows Desktop, selecting pages and chapters fix.

--- tests/integration/python/pages/import_dossier/import_dossier_table_of_contents/import_dossier_table_of_contents_windows_desktop_page.py
+++ tests/integration/python/pages/import_dossier/import_dossier_table_of_contents/import_dossier_table_of_contents_windows_desktop_page.py
@@ -1,11 +1,10 @@
 from framework.pages_base.base_windows_desktop_page import BaseWindowsDesktopPage
-from framework.util.exception.MstrException import MstrException
 
 
 class ImportDossierTableOfContentsWindowsDesktopPage(BaseWindowsDesktopPage):
     TABLE_OF_CONTENT_BUTTON = 'Table of Contents'
-    # TODO please add parent if possible
-    TABLE_OF_CONTENT_ITEMS = '//Button[@Name="Table of Contents"]/following-sibling::List[1]//ListItem'
+
+    TABLE_OF_CONTENT_ITEMS = '//Button[@Name="Table of Contents"]/following-sibling::List[1]//ListItem[%s]'
 
     def select_table_of_content_option(self, option_number):
         self.get_element_by_name(
@@ -13,13 +12,8 @@ class ImportDossierTableOfContentsWindowsDesktopPage(BaseWindowsDesktopPage):
             image_name=self.prepare_image_name(ImportDossierTableOfContentsWindowsDesktopPage.TABLE_OF_CONTENT_BUTTON)
         ).click()
 
-        menu_items = self.get_elements_by_xpath(ImportDossierTableOfContentsWindowsDesktopPage.TABLE_OF_CONTENT_ITEMS)
+        add_in_main_element = self.get_add_in_main_element()
 
-        option_index = int(option_number) - 1
-
-        if option_index > len(menu_items):
-            raise MstrException(
-                ('Not possible to click menu option, given object index is too big', option_index, menu_items)
-            )
-
-        menu_items[option_index].click()
+        add_in_main_element.get_element_by_xpath(
+            ImportDossierTableOfContentsWindowsDesktopPage.TABLE_OF_CONTENT_ITEMS % option_number
+        ).click()
