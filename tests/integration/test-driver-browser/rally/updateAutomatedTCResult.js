886d0b7ca42c56a83eabb5230ed52fe871b96289
jkowalczyk <jkowalczyk@microstrategy.com>
2020-12-01 14:10:34 +0100

    Refactor logic for updating Rally results

---
+++ tests/integration/test-driver-browser/rally/updateAutomatedTCResult.js
@@ -0,0 +1,16 @@
+
+const updateResultHelpers = require('./helpers/updateResultHelpers');
+
+updateResultHelpers.getAutomatedBatchArray().then(batch => {
+  updateResultHelpers.updateRallyTCResult(batch)
+    .then((result) => {
+      const { Errors: errors } = result.BatchResult;
+      if (errors.length > 0) { throw new Error(errors); }
+      console.log('Rally request completed');
+      process.exit(0);
+    })
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    })
+});
