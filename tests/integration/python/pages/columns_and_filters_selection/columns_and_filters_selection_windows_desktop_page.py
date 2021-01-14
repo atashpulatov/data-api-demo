44ff26d0cfb0f876bda7c40699e9e7f1681c1bd5
Ryszard Raszplewicz <rraszplewicz@microstrategy.com>
2021-01-14 12:01:10 +0100

    Review. Performance improved when Totals And Subtotals is not visible.

--- tests/integration/python/pages/columns_and_filters_selection/columns_and_filters_selection_windows_desktop_page.py
+++ tests/integration/python/pages/columns_and_filters_selection/columns_and_filters_selection_windows_desktop_page.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.keys import Keys
 
 from framework.pages_base.base_windows_desktop_page import BaseWindowsDesktopPage
-from framework.util.const import SHORT_TIMEOUT
+from framework.util.const import SHORT_TIMEOUT, MEDIUM_TIMEOUT
 from framework.util.exception.MstrException import MstrException
 from pages.right_panel.right_panel_tile.right_panel_tile_windows_desktop_page import RightPanelTileWindowsDesktopPage
 
@@ -128,9 +128,9 @@ class ColumnsAndFiltersSelectionWindowsDesktopPage(BaseWindowsDesktopPage):
         ).click()
 
     def is_subtotal_visible(self):
-        is_visible = self.check_if_element_exists_by_name(
+        is_visible = self.get_add_in_main_element().check_if_element_exists_by_name(
             ColumnsAndFiltersSelectionWindowsDesktopPage.TOTALS_AND_SUBTOTALS,
-            image_name=self.prepare_image_name(ColumnsAndFiltersSelectionWindowsDesktopPage.TOTALS_AND_SUBTOTALS)
+            timeout=MEDIUM_TIMEOUT
         )
 
         return is_visible
@@ -158,4 +158,3 @@ class ColumnsAndFiltersSelectionWindowsDesktopPage(BaseWindowsDesktopPage):
             timeout=SHORT_TIMEOUT,
             image_name=self.prepare_image_name(ColumnsAndFiltersSelectionWindowsDesktopPage.BACK_BUTTON)
         )
-
