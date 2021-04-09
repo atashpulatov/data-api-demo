f802da0c84fb0512a67b4b0699324197e0769aa5
mbereszczuk <mbereszczuk@microstrategy.com>
2021-04-09 11:31:18 +0200

    Fixes TC62674 for Mac Chrome

--- tests/integration/python/pages/prompt/prompt_browser_page.py
+++ tests/integration/python/pages/prompt/prompt_browser_page.py
@@ -1,6 +1,7 @@
 from framework.pages_base.base_browser_page import BaseBrowserPage
 from framework.util.const import Const
 from framework.util.exception.mstr_exception import MstrException
+from selenium.webdriver.common.keys import Keys
 from pages.columns_and_filters_selection.columns_and_filters_selection_browser_page import \
     ColumnsAndFiltersSelectionBrowserPage
 
@@ -12,12 +13,13 @@ class PromptBrowserPage(BaseBrowserPage):
     PROMPT_LIST_ELEM = '.mstrPromptTOCListItemIndex'
     PROMPT_OBJECT_SELECTED_ITEM = '.mstrListCartCellSelectedView .mstrListBlockItemName'
     PROMPT_OBJECT_AVAILABLE_ITEM = '.mstrListCartCellAvailableView .mstrListBlockItemName'
+    PROMPT_OBJECT_QUESTION_CONTAINER = '.mstrPromptQuestion'
 
     PROMPT_VALUE_ELEM = '.mstrCalendarAndTimePickerCellTextBox > input'
 
     PROMPT_QUESTION_TITLE_BAR = '.mstrPromptQuestionTitleBar'
     PROMPT_QUESTION_TITLE_INDEX = PROMPT_QUESTION_TITLE_BAR + ' .mstrPromptQuestionTitleBarIndex'
-    PROMPT_QUESTION_TITLTE_NAME = PROMPT_QUESTION_TITLE_BAR + ' .mstrPromptQuestionTitleBarTitle'
+    PROMPT_QUESTION_TITLE_NAME = PROMPT_QUESTION_TITLE_BAR + ' .mstrPromptQuestionTitleBarTitle'
 
     def __init__(self):
         super().__init__()
@@ -87,30 +89,79 @@ class PromptBrowserPage(BaseBrowserPage):
 
         answer.double_click()
 
-    def select_answer_for_value_prompt(self, index, text):
-        self._select_prompt_from_list(index)
+    def select_answer_for_value_prompt(self, prompt_number, text):
+        container = self._find_container_by_prompt_number(prompt_number)
 
-        prompt = self.get_element_by_css(PromptBrowserPage.PROMPT_VALUE_ELEM)
+        prompt = container.get_element_by_css(PromptBrowserPage.PROMPT_VALUE_ELEM)
 
         prompt.double_click()
         prompt.send_keys(text)
         self.press_tab()
         self.press_tab()
 
-    def _check_prompt_name(self, prompt_number, prompt_name):
-        title_index = self._get_object_name_in_prompt(PromptBrowserPage.PROMPT_QUESTION_TITLE_INDEX)
+    def provide_answer_for_date_prompt(self, prompt_number, date, hour, minute, second):
+        self._select_prompt_from_list(prompt_number)
 
-        title_name = self._get_object_name_in_prompt(PromptBrowserPage.PROMPT_QUESTION_TITLTE_NAME)
+        container = self._find_container_by_prompt_number(prompt_number)
+
+        prompt = container.get_element_by_css(PromptBrowserPage.PROMPT_VALUE_ELEM)
+
+        prompt.double_click()
+        prompt.send_keys(date)
+
+        prompt_hour = container.get_element_by_css('.mstrHour')
+        prompt_hour.double_click()
+        prompt_hour.send_keys(hour)
+
+        prompt_hour = container.get_element_by_css('.mstrMinute')
+        prompt_hour.double_click()
+        prompt_hour.send_keys(minute)
+
+        prompt_hour = container.get_element_by_css('.mstrSecond')
+        prompt_hour.double_click()
+        prompt_hour.send_keys(second)
+
+    def clear_prompt_input(self, prompt_number):
+        self._select_prompt_from_list(prompt_number)
+
+        container = self._find_container_by_prompt_number(prompt_number)
+
+        prompt = container.get_element_by_css(PromptBrowserPage.PROMPT_VALUE_ELEM)
+
+        prompt.click()
+        prompt.double_click()
+        prompt.send_keys(Keys.BACKSPACE)
+
+    def _check_prompt_name(self, prompt_number, prompt_name):
+
+        title_index_objects = self._get_all_objects_in_prompt_by_selector(PromptBrowserPage.PROMPT_QUESTION_TITLE_INDEX)
+
+        title_name_objects = self._get_all_objects_in_prompt_by_selector(PromptBrowserPage.PROMPT_QUESTION_TITLE_NAME)
 
         number = prompt_number + '.'
 
-        if title_index != number or title_name != prompt_name:
-            raise MstrException(f'Prompt title does not match: [{title_index}]. '
-                                f'[{title_name}], text: [{number}]. [{prompt_name}]')
+        for title_index, title_name in zip(title_index_objects, title_name_objects):
+            if title_index.text == number and title_name.text == prompt_name:
+                return
 
-    def _get_object_name_in_prompt(self, object_selector):
+        raise MstrException(f'Could not find prompt: [{number}]. [{prompt_name}] ')
+
+    def _get_all_objects_in_prompt_by_selector(self, object_selector):
         self.focus_on_prompt_frame()
 
-        name_input = self.get_element_by_css(object_selector)
+        selected_objects = self.get_elements_by_css(object_selector)
 
-        return name_input.text
+        return selected_objects
+
+    def _find_container_by_prompt_number(self, prompt_number):
+        containers = self.get_elements_by_css(PromptBrowserPage.PROMPT_OBJECT_QUESTION_CONTAINER)
+
+        number = prompt_number + '.'
+
+        for container in containers:
+            if container.get_attribute('Id'):
+                if container.get_element_by_css(PromptBrowserPage.PROMPT_QUESTION_TITLE_INDEX).text == number:
+                    return container
+
+        raise MstrException(f'Could not find container with prompt number: [{prompt_number}] ')
+
