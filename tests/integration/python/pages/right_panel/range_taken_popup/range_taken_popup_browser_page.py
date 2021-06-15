0ffd472a17e0806ca5fc07bd1326b7f8fc21ce8b
agorski <agorski@microstrategy.com>
2021-06-15 13:21:28 +0200

    Implemented methods for the "verify New Sheet is selected" step

--- tests/integration/python/pages/right_panel/range_taken_popup/range_taken_popup_browser_page.py
+++ tests/integration/python/pages/right_panel/range_taken_popup/range_taken_popup_browser_page.py
@@ -2,10 +2,16 @@ from framework.pages_base.base_browser_page import BaseBrowserPage
 from pages.right_panel.right_panel_tile.right_panel_tile_browser_page import RightPanelTileBrowserPage
 
 
+def _is_checked(element):
+    return element.get_attribute(RangeTakenPopupBrowserPage.CHECKED_ATTRIBUTE) == 'true'
+
+
 class RangeTakenPopupBrowserPage(BaseBrowserPage):
     RANGE_TAKEN_OK_BUTTON = '.range-taken-popup-footer-buttons .primary'
     RANGE_TAKEN_CANCEL_BUTTON = '.range-taken-popup-footer-buttons .basic'
     RANGE_TAKEN_ACTIVE_CELL_OPTION = 'active_cell'
+    CHECKED_ATTRIBUTE = 'checked'
+    NEW_SHEET_ELEMENT = 'new_sheet'
 
     def __init__(self):
         super().__init__()
@@ -26,3 +32,8 @@ class RangeTakenPopupBrowserPage(BaseBrowserPage):
         self.focus_on_add_in_frame()
 
         self.get_element_by_id(RangeTakenPopupBrowserPage.RANGE_TAKEN_ACTIVE_CELL_OPTION).click()
+
+    def verify_that_new_sheet_is_checked(self):
+        self.focus_on_add_in_frame()
+        element = self.get_element_by_id(RangeTakenPopupBrowserPage.NEW_SHEET_ELEMENT)
+        return _is_checked(element)
