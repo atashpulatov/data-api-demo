7ad48075e3f9210a8720f5fe6f0629cc202a7b8b
svc-corp-jenkins <jenkins@microstrategy.com>
2022-09-21 18:04:34 -0400

    updating BRANCH_OFF_COMMIT to 62c60b00f39cab71ff35fc768dc1c2d8ee5e17e2

--- workspace-settings/shell/set.env.bash
+++ workspace-settings/shell/set.env.bash
@@ -55,4 +55,4 @@ function become_jenkins_manager(){
   export HATS=$HATS:jenkins
 }
 
-export BRANCH_OFF_COMMIT=8e9487a590e6ea318fde6763c964c37b2da6ecdd
+export BRANCH_OFF_COMMIT=62c60b00f39cab71ff35fc768dc1c2d8ee5e17e2
