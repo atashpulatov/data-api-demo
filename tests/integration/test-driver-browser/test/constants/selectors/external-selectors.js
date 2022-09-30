a5b141d5eb2cdc7b2411f5a8fe386c57416f4842
Gorski, Artur <agorski@microstrategy.com>
2022-09-30 09:13:45 +0200

    US453370;remove unused deps and automation mstr office (#1493)
    * US453370;remove-unused-deps-and-automation-mstr-office
    
    * remove-chromedriver-and-eslint-typescript-unused-deps

--- tests/integration/test-driver-browser/test/constants/selectors/external-selectors.js
+++
@@ -1,6 +0,0 @@
-export const externalSelectors = {
-  //Selector for additional pages used to test the plugin
-  bingSearchField: '.sb_form_q',
-  bingSearchBox: '.b_searchbox'
-
-}
--- tests/integration/test-driver-browser/test/constants/strings-used-in-specs.js
+++
@@ -1,4 +0,0 @@
-export const stringsUsedInSpecs = {
-  longText: 'This is a veeery long text that would exceed the limit of the input text field',
-  versionError: 'Selected visualization cannot be imported in current version of the Add-in',
-};
