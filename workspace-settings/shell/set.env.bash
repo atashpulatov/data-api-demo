b8b169fc3e65a77b6e121429de5b1a060cfe794b
svc-corp-jenkins <svc-corp-jenkins@MICROSTRATEGY.onmicrosoft.com>
2023-06-26 09:39:21 -0400

    upgrade nodejs to 18.16.1

--- workspace-settings/shell/set.env.bash
+++ workspace-settings/shell/set.env.bash
@@ -18,7 +18,7 @@ export VAGRANT_BOXES_OSX_VERSION='1.0.0.next'
 
 jdk_version="jdk-11.0.16"
 
-nodejs_version="18.16.0"
+nodejs_version='18.16.1'
 export nodejs_image_tag=$nodejs_version
 
 if uname -a | grep -q "Darwin"; then
