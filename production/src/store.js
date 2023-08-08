91af65a538a2c8149347a77ff3abf5ea1ae9fa35
Vargas <jvargas@TYCM15978.local>
2023-08-08 10:48:33 -0600

    update redux store configuration to use redux toolkit configureStore

--- production/src/store.js
+++ production/src/store.js
@@ -1,7 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
 import {
-  createStore, combineReducers, applyMiddleware, compose
-} from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
+  persistStore, persistReducer,
+  FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 import { sessionReducer } from './redux-reducer/session-reducer/session-reducer';
@@ -33,17 +35,15 @@ const persistConfig = {
   blacklist: ['notificationReducer', 'navigationTree', 'operationReducer', 'objectReducer'],
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-let middleWare;
-if (process.env.NODE_ENV === 'development') {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  middleWare = composeEnhancers(applyMiddleware(thunk));
-} else {
-  middleWare = applyMiddleware(thunk);
-}
-export const reduxStore = createStore(persistedReducer, middleWare);
-export const reduxPersistor = persistStore(reduxStore);
 
-window.store = reduxStore;
+export const reduxStore = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV === 'development',
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(
+    { serializableCheck: { ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER] } }
+  )
+});
+export const reduxPersistor = persistStore(reduxStore);
 
 if (localStorage) {
   const version = localStorage.getItem('version');
