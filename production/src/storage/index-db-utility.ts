67adf8e041c1e2d78629611be1b18440910d613a
Azizbek Tashpulatov <atashpulatov@microstrategy.com>
2023-10-06 15:13:12 -0400

    US513997;Convert-to-typescript-files

---
+++ production/src/storage/index-db-utility.ts
@@ -0,0 +1,34 @@
+import db from './index-db';
+
+  /**
+   * Saves the list of stored prompt answers in indexDB.
+   *
+   * @param {String} promptAnswersKey - Prompt answers key.
+   * @param {String} promptAnswersData - The list of prompt answers.
+   */
+export async function savePromptAnswersInIndexDB(promptAnswersKey: string, promptAnswersData: Array<Object>) {
+    if (promptAnswersData) {
+        if (db?.promptAnswers) {
+            db?.promptAnswers.clear();
+        }
+        await db?.promptAnswers.add({ key: promptAnswersKey, data: promptAnswersData });
+    }
+}
+
+  /**
+   * Retrieves the list of stored prompt answers from indexDB by prompt answers key.
+   *
+   * @param {String} promptAnswersKey - Prompt answers key.
+   * @returns {Array} The list of prompt answers.
+   */
+export async function getPromptAnswersFromIndexDB(promptAnswersKey: string) {
+    const storedPromptAnswers = await db?.promptAnswers
+      .where('key')
+      .equals(promptAnswersKey)
+      .toArray();
+
+    if (storedPromptAnswers && storedPromptAnswers.length > 0) {
+      return storedPromptAnswers[0]?.data;
+    }
+    return null;
+  }
