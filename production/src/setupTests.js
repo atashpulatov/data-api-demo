a62ab89fde386663a4316490c3d3c4802b03d192
agorski <agorski@microstrategy.com>
2023-11-13 11:00:53 +0100

    update setupTests.js file

--- production/src/setupTests.js
+++ production/src/setupTests.js
@@ -4,21 +4,42 @@ import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import 'jest-localstorage-mock';
 import 'regenerator-runtime';
+import { SideInfoPanel } from '@mstr/connector-components';
 import { diContainer } from './dependency-container';
+import '@testing-library/jest-dom';
 
-jest.mock('@mstr/connector-components', () => ({
-  // eslint-disable-next-line react/react-in-jsx-scope
-  ObjectTable: () => <div />,
-  // eslint-disable-next-line react/react-in-jsx-scope,react/no-multi-comp
-  TopFilterPanel: () => <div />,
-  // eslint-disable-next-line react/no-multi-comp,react/react-in-jsx-scope
-  SidePanel: () => <div />,
-  objectNotificationTypes: {
-    PROGRESS: 'PROGRESS',
-    WARNING: 'WARNING',
-    SUCCESS: 'SUCCESS',
-  }
-}));
+jest.mock('@mstr/connector-components', () => {
+  const originalModule = jest.requireActual('@mstr/connector-components');
+
+  return {
+    ...originalModule,
+    // eslint-disable-next-line react/react-in-jsx-scope
+    ObjectTable: () => <div />,
+    // eslint-disable-next-line react/react-in-jsx-scope,react/no-multi-comp
+    TopFilterPanel: () => <div />,
+    // eslint-disable-next-line react/no-multi-comp,react/react-in-jsx-scope
+    SidePanel: () => <div />,
+    objectNotificationTypes: {
+      PROGRESS: 'PROGRESS',
+      WARNING: 'WARNING',
+      SUCCESS: 'SUCCESS',
+    }
+  };
+});
+
+//   return {
+//   // eslint-disable-next-line react/react-in-jsx-scope
+//   ObjectTable: () => <div />,
+//   // eslint-disable-next-line react/react-in-jsx-scope,react/no-multi-comp
+//   TopFilterPanel: () => <div />,
+//   // eslint-disable-next-line react/no-multi-comp,react/react-in-jsx-scope
+//   SidePanel: () => <div />,
+//   objectNotificationTypes: {
+//     PROGRESS: 'PROGRESS',
+//     WARNING: 'WARNING',
+//     SUCCESS: 'SUCCESS',
+//   }
+// }));
 global.ResizeObserver = require('resize-observer-polyfill');
 
 configure({ adapter: new Adapter() });
