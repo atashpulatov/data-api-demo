47cbf26311e96bc36af5cd7a170ccada12418342
Tejas Sahasranaman <tsahasranaman@microstrategy.com>
2023-12-06 19:35:09 -0500

    update jsdoc

--- production/src/customTranslation.js
+++ production/src/customTranslation.js
@@ -1,10 +1,9 @@
 import i18n from './i18n';
 
 /**
-  * It gets the translation of the string (if available)
-  *
-  * It is using the translation stored in json file from i18n
-  * to get the translated version of the string for the currently used language
+  * It gets the translation of the string for the currently used language
+  * otherwise it will return the string for the fallback language
+  * specified `fallbackLng` setting in the i18n configuration
   *
   * @param {String} str - String to be translated
   * @returns {String} Translation of the string for the currently used language
