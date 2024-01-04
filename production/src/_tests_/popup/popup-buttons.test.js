c3f35412c664a74ac16ab424a2902b548bf18aac
mbereszczuk <mbereszczuk@microstrategy.com>
2024-01-04 15:51:34 +0100

    US525802;removing enzyme

--- production/src/_tests_/popup/popup-buttons.test.js
+++ production/src/_tests_/popup/popup-buttons.test.js
@@ -1,7 +1,5 @@
-// TODO: Rewrite tests
-/* eslint-disable jest/no-disabled-tests */
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { fireEvent, render } from '@testing-library/react';
 import { PopupButtonsNotConnected } from '../../popup/popup-buttons/popup-buttons';
 
 describe('PopupButtons', () => {
@@ -10,154 +8,101 @@ describe('PopupButtons', () => {
       // given
       const secondaryAction = jest.fn();
       // when
-      const buttonsWrapped = shallow(<PopupButtonsNotConnected />);
+      const { queryByText } = render(<PopupButtonsNotConnected />);
       // then
-      expect(buttonsWrapped.exists('#prepare')).not.toBeTruthy();
+      expect(queryByText('Prepare Data')).not.toBeInTheDocument();
     });
 
   it('should display prepare data when secondary action provided', () => {
     // given
     const secondaryAction = jest.fn();
     // when
-    const buttonsWrapped = mount(<PopupButtonsNotConnected
+    const { getByText } = render(<PopupButtonsNotConnected
       handleSecondary={secondaryAction}
       hideSecondary={false}
     />);
-    // thenfix
-    expect(buttonsWrapped.exists('#prepare')).toBeTruthy();
+    // then
+    expect(getByText('Prepare Data')).toBeInTheDocument();
   });
 
   it('should display back button when handleBack provided', () => {
     // given
     const handleBack = jest.fn();
     // when
-    const buttonsWrapped = mount(<PopupButtonsNotConnected
+    const { getByText } = render(<PopupButtonsNotConnected
       handleBack={handleBack}
     />);
-    // thenfix
-    expect(buttonsWrapped.exists('#back')).toBeTruthy();
+    // then
+    expect(getByText('Back')).toBeInTheDocument();
   });
 
   it('should NOT display back button with cancel action when handleBack NOT provided', () => {
     // given
     const handleBack = jest.fn();
     // when
-    const buttonsWrapped = shallow(<PopupButtonsNotConnected />);
-    // thenfix
-    expect(buttonsWrapped.exists('#backCancel')).toBeFalsy();
+    const { queryByText } = render(<PopupButtonsNotConnected />);
+    // then
+    expect(queryByText('Back')).not.toBeInTheDocument();
   });
 
-  it.skip('should call secondary action when prepare data clicked', () => {
+  it('should call secondary action when prepare data clicked', () => {
     // given
     const secondaryAction = jest.fn();
-    const buttonsWrapped = mount(<PopupButtonsNotConnected
+    const { getByText } = render(<PopupButtonsNotConnected
       handleSecondary={secondaryAction}
     />);
-    const secondaryButton = buttonsWrapped.find('Button#prepare');
+    const secondaryButton = getByText('Prepare Data');
     // when
-    secondaryButton.simulate('click');
+    fireEvent.click(secondaryButton);
     // then
     expect(secondaryAction).toBeCalled();
   });
-  it.skip('should render a tooltip span if the buttons are disabled', () => {
+  it('should render a tooltip span if the buttons are disabled', () => {
     // given
     const disableActiveActions = true;
     // when
-    const buttonsWrapped = mount(<PopupButtonsNotConnected
+    const { container } = render(<PopupButtonsNotConnected
       disableActiveActions={disableActiveActions}
     />);
-    const tooltipSpan = buttonsWrapped.find('Popover.button-tooltip');
     // then
-    expect(tooltipSpan).toHaveLength(2);
+    expect(container.querySelector('span.mstr-rc-tooltip-source')).toBeInTheDocument();
   });
 
-  it.skip('should render a tooltip span if the cube Isn’t  published', () => {
+  it('should render a tooltip span if the cube Isn’t  published', () => {
     // when
-    const buttonsWrapped = mount(<PopupButtonsNotConnected
+    const { container } = render(<PopupButtonsNotConnected
       isPublished={false}
     />);
-    const tooltipSpan = buttonsWrapped.find('Popover.button-tooltip');
     // then
-    expect(tooltipSpan).toHaveLength(2);
-  });
-  it('should not render a tooltip span if the buttons are enabled', () => {
-    // given
-    const disableActiveActions = false;
-    // when
-    const buttonsWrapped = shallow(<PopupButtonsNotConnected
-      disableActiveActions={disableActiveActions}
-    />);
-    const tooltipSpan = buttonsWrapped.find('.button-tooltip');
-    // then
-    expect(tooltipSpan).toHaveLength(0);
+    expect(container.querySelector('span.mstr-rc-tooltip-source')).toBeInTheDocument();
   });
 
   it('should NOT display secondary button when hideSecondary prop is provided',
     () => {
       // when
-      const buttonsWrapped = mount(<PopupButtonsNotConnected hideSecondary />);
+      const { queryByText } = render(<PopupButtonsNotConnected hideSecondary />);
       // then
-      expect(buttonsWrapped.exists('#data-preview')).not.toBeTruthy();
-      expect(buttonsWrapped.exists('#import')).toBeTruthy();
+      expect(queryByText('Data Preview')).not.toBeInTheDocument();
     });
 
   it('should display secondary button when hideSecondary prop is not provided',
     () => {
       // when
-      const buttonsWrapped = mount(<PopupButtonsNotConnected hideSecondary={false} />);
+      const { getByText } = render(<PopupButtonsNotConnected hideSecondary={false} />);
       // then
-      expect(buttonsWrapped.exists('#data-preview')).toBeTruthy();
-      expect(buttonsWrapped.exists('#import')).toBeTruthy();
+      expect(getByText('Data Preview')).toBeInTheDocument();
     });
 
-  it.skip('should display only secondary button tooltip when disableSecondary prop is provided ',
-    () => {
-      // when
-      const buttonsWrapped = mount(<PopupButtonsNotConnected handleSecondary disableSecondary />);
-      const tooltipSpan = buttonsWrapped.find('Popover.button-tooltip');
-      // then
-      expect(tooltipSpan).toHaveLength(1);
-    });
-
-  it.skip('should display primary button tooltip CHECKING_SELECTION when isChecking is provided ',
+  it('should display primary button as Apply when useImportAsRunButton is provided ',
     () => {
       // given
       // when
-      const buttonsWrapped = mount(<PopupButtonsNotConnected
-        hideSecondary
-        checkingSelection
-      />);
-      const tooltipSpan = buttonsWrapped.find('Popover.button-tooltip');
-      // then
-      expect(tooltipSpan).toHaveLength(1);
-      // TODO expect('text of tooltip').toBe(CHECKING_SELECTION);
-    });
-
-  it.skip('should display primary button tooltip NOT_SUPPORTED_VIZ when isChecking is provided ',
-    () => {
-      // given
-      // when
-      const buttonsWrapped = mount(<PopupButtonsNotConnected
-        hideSecondary
-        isPublished={false}
-        disableSecondary
-      />);
-      const tooltipSpan = buttonsWrapped.find('Popover.button-tooltip');
-      // then
-      expect(tooltipSpan).toHaveLength(1);
-      // TODO expect('text of tooltip').toBe(NOT_SUPPORTED_VIZ);
-    });
-
-  it('should display primary button as Run when useImportAsRunButton is provided ',
-    () => {
-      // given
-      // when
-      const buttonsWrapped = mount(<PopupButtonsNotConnected
+      const { getByText } = render(<PopupButtonsNotConnected
         useImportAsRunButton
       />);
       // then
-      expect(buttonsWrapped.exists('#data-preview')).toBeTruthy();
-      expect(buttonsWrapped.exists('#run')).toBeTruthy();
-      expect(buttonsWrapped.exists('#cancel')).toBeTruthy();
+      expect(getByText('Data Preview')).toBeInTheDocument();
+      expect(getByText('Apply')).toBeInTheDocument();
+      expect(getByText('Cancel')).toBeInTheDocument();
     });
 });
