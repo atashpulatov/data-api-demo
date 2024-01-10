68196bf65072e7ccf6db2d48e4b9419e2c390c81
Tejas Sahasranaman <tsahasranaman@microstrategy.com>
2024-01-10 00:27:23 -0500

    fix UT

--- production/src/_tests_/helpers/visualization-image-helper.test.js
+++ production/src/_tests_/helpers/visualization-image-helper.test.js
@@ -4,7 +4,7 @@ describe('VisualizationImageUtils', () => {
   describe('convertImageToBase64', () => {
     it('should convert image to base64', async () => {
       const mockImage = {
-        ArrayBuffer: jest.fn().mockImplementation(() => Promise.resolve(new ArrayBuffer(8)))
+        arrayBuffer: jest.fn().mockImplementation(() => Promise.resolve(new ArrayBuffer(8)))
       };
       const result = await convertImageToBase64(mockImage);
       expect(result).toEqual('AAAAAAAAAAA=');
