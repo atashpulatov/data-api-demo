8f9166b8fb5fb71dc78f7d9f9ca12525ac3744e3
Azizbek Tashpulatov <atashpulatov@microstrategy.com>
2024-01-30 13:42:12 -0500

    DE285033;Formatting

--- production/src/mstr-object/constants.js
+++ production/src/mstr-object/constants.js
@@ -1,11 +1,9 @@
-import {MANIPULATE_VISUALIZATION_IMAGE} from '..'
-
 export const objectImportType = {
   TABLE: 'table',
-  IMAGE: 'image'
+  IMAGE: 'image',
 };
 
 export const importOperationStepDict = {
-    GET_OFFICE_TABLE_IMPORT: objectImportType.IMAGE,
-    MANIPULATE_VISUALIZATION_IMAGE: objectImportType.IMAGE
-  };
+  GET_OFFICE_TABLE_IMPORT: objectImportType.IMAGE,
+  MANIPULATE_VISUALIZATION_IMAGE: objectImportType.IMAGE,
+};
