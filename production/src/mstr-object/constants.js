d31e06809ec38b1ee60f89558cb83c7c7434aa17
Azizbek Tashpulatov <atashpulatov@microstrategy.com>
2024-01-30 10:50:54 -0500

    DE285033;Fix-duplicate-operation-for-visualization

--- production/src/mstr-object/constants.js
+++ production/src/mstr-object/constants.js
@@ -1,4 +1,11 @@
+import {MANIPULATE_VISUALIZATION_IMAGE} from '..'
+
 export const objectImportType = {
   TABLE: 'table',
   IMAGE: 'image'
 };
+
+export const importOperationStepDict = {
+    GET_OFFICE_TABLE_IMPORT: objectImportType.IMAGE,
+    MANIPULATE_VISUALIZATION_IMAGE: objectImportType.IMAGE
+  };
