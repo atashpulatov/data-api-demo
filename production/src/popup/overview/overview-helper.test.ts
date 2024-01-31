c511e2cb768b0fcb9200e94b73cfb5a1ae15b860
agorski <agorski@microstrategy.com>
2024-01-31 08:50:49 +0100

    overview helper unit tests

--- production/src/popup/overview/overview-helper.test.ts
+++ production/src/popup/overview/overview-helper.test.ts
@@ -2,9 +2,10 @@ import { popupHelper } from '../popup-helper';
 import overviewHelper, { OverviewActionCommands } from './overview-helper';
 
 describe('overview-helper', () => {
+  const objectWorkingIds = [1, 2, 3];
+
   it('should send refresh request to side panel', () => {
     // Given
-    const objectWorkingIds = [1, 2, 3];
     const officeMessageParentMock = jest.spyOn(popupHelper, 'officeMessageParent').mockImplementation();
     // When
     overviewHelper.sendRefreshRequest(objectWorkingIds);
@@ -18,7 +19,6 @@ describe('overview-helper', () => {
 
   it('should send delete request to side panel', () => {
     // Given
-    const objectWorkingIds = [1, 2, 3];
     const officeMessageParentMock = jest.spyOn(popupHelper, 'officeMessageParent').mockImplementation();
     // When
     overviewHelper.sendDeleteRequest(objectWorkingIds);
@@ -26,6 +26,32 @@ describe('overview-helper', () => {
     // Then
     expect(officeMessageParentMock).toHaveBeenCalledWith({
       command: OverviewActionCommands.REMOVE,
+      objectWorkingIds
+    });
+  });
+
+  it('should send duplicate request to side panel', () => {
+    // Given
+    const officeMessageParentMock = jest.spyOn(popupHelper, 'officeMessageParent').mockImplementation();
+    // When
+    overviewHelper.sendDuplicateRequest(objectWorkingIds);
+
+    // Then
+    expect(officeMessageParentMock).toHaveBeenCalledWith({
+      command: OverviewActionCommands.DUPLICATE,
+      objectWorkingIds
+    });
+  });
+
+  it('should send Dismiss Notification request to side panel', () => {
+    // Given
+    const officeMessageParentMock = jest.spyOn(popupHelper, 'officeMessageParent').mockImplementation();
+    // When
+    overviewHelper.sendDismissNotificationRequest(objectWorkingIds);
+
+    // Then
+    expect(officeMessageParentMock).toHaveBeenCalledWith({
+      command: OverviewActionCommands.DISMISS_NOTIFICATION,
       objectWorkingIds
     });
   });
