c78e31d986ef0dadd52e65d0ad53aa93575ac86a
pzajk <pzajk@microstrategy.com>
2024-02-02 12:18:54 +0100

    Add data overview component

--- production/src/popup/overview/overview-window.jsx
+++ production/src/popup/overview/overview-window.jsx
@@ -1,28 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { DataOverview } from '@mstr/connector-components';
 import PropTypes from 'prop-types';
+import './overview-window.scss';
 
-// TODO this component should be replaced with CC Overview component
 const OverviewWindowNotConnected = (props) => {
   const { onRefresh, onDelete } = props;
   const { objects } = props;
 
   return (
-    <div>
-      <h1>Overview Demo Window</h1>
-      <h3>Object list</h3>
-      <ol>
-        {objects.map((object) => (
-          <li key={object.objectWorkingId}>
-            <h5>{object.name}</h5>
-            <button type="button" onClick={() => onRefresh([object.objectWorkingId])}>Refresh</button>
-            <button type="button" onClick={() => onDelete([object.objectWorkingId])}>Delete</button>
-          </li>
-        ))}
-      </ol>
-      <div>
-        <h2>Objects</h2>
-      </div>
+    <div className="data-overview-wrapper">
+      <DataOverview loadedObjects={objects} applicationType="EXCEL" onRefresh={onRefresh} onDelete={onDelete} />
     </div>
   );
 };
