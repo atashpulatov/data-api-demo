cf3ed2794a6a129fe4f885de9d124cd7f45fc93d
agorski <agorski@microstrategy.com>
2024-02-07 15:48:31 +0100

    use useStateSyncOnDialogMessage hook in OverviewWindow

--- production/src/popup/overview/overview-window.jsx
+++ production/src/popup/overview/overview-window.jsx
@@ -11,11 +11,14 @@ import { ApplicationTypeEnum } from '../../office-constants';
 
 import './overview-window.scss';
 import overviewHelper from './overview-helper';
+import useStateSyncOnDialogMessage from './use-state-sync-on-dialog-message';
 
 export const OverviewWindowNotConnected = (props) => {
   const {
     objects, onRefresh, onDelete, onDuplicate, notifications
   } = props;
+
+  useStateSyncOnDialogMessage();
 
   // TODO This will be passed to DataOverview component once the component is updated
   const shouldDisableActions = useMemo(
@@ -23,26 +26,6 @@ export const OverviewWindowNotConnected = (props) => {
   );
 
   const objectsToRender = overviewHelper.transformExcelObjects(objects, notifications);
-
-  useEffect(() => {
-    // Get Message from Right side panel
-    Office.context.ui.addHandlerAsync(Office.EventType.DialogParentMessageReceived, (msg) => {
-      const message = JSON.parse(msg.message);
-      const { popupType } = message;
-
-      if (popupType === PopupTypeEnum.importedDataOverview) {
-        const {
-          objects: objectsToSync,
-          notifications: notificationsToSync,
-          globalNotification: globalNotificationToSync
-        } = message;
-
-        reduxStore.dispatch(restoreAllObjects(objectsToSync));
-        reduxStore.dispatch(restoreAllNotifications(notificationsToSync));
-        reduxStore.dispatch(createGlobalNotification(globalNotificationToSync));
-      }
-    });
-  }, []);
 
   return (
     <div className="data-overview-wrapper">
