a5ecabe15359f61a0f838f7e450c3352b83ed285
Gregorio Parra <gparra@microstrategy.com>
2024-02-12 15:47:06 -0500

    First check-in...

--- production/src/popup/popup-buttons/popup-buttons.jsx
+++ production/src/popup/popup-buttons/popup-buttons.jsx
@@ -7,7 +7,6 @@ import PropTypes from 'prop-types';
 import i18n from '../../i18n';
 import { errorMessages } from '../../error/constants';
 import { DataPreviewButton } from './data-preview-button';
-import { BackButton } from './back-button';
 import { PrepareDataButton } from './prepare-data-button';
 import { ImportButton } from './import-button';
 import { CancelButton } from './cancel-button';
@@ -58,7 +57,6 @@ export const PopupButtonsNotConnected = ({
   );
   return (
     <div className="popup-buttons popup-footer">
-      {handleBack && <BackButton handleBack={handleBack} t={t} />}
       {(!hideSecondary && !handleSecondary) && (
         <DataPreviewButton
           onPreviewClick={onPreviewClick}
