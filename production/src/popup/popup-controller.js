b969db0279691ea4bf4444ad4ceceb71384f8f53
Piotr Nastaj <pnastaj@microstrategy.com>
2024-02-14 16:02:02 +0100

    Remove console.logs

--- production/src/popup/popup-controller.js
+++ production/src/popup/popup-controller.js
@@ -32,11 +32,9 @@ class PopupController {
   clearPopupStateIfNeeded = () => {
     // TODO: convert this to a redux action
     const { isDataOverviewOpen } = this.reduxStore.getState().popupStateReducer;
-    console.log('🚀 ~ PopupController ~ isDataOverviewOpen:', isDataOverviewOpen);
 
     if (!isDataOverviewOpen) {
       this.reduxStore.dispatch(popupStateActions.onClearPopupState());
-      console.log('=============== reset state 1');
       this.reduxStore.dispatch(this.popupActions.resetState());
     }
   };
@@ -82,13 +80,9 @@ class PopupController {
   };
 
   runPopup = async (popupType, height, width, reportParams = null) => {
-    console.log('🚀 ~ PopupController ~ runPopup= ~ popupType:', popupType);
     const isDialogForMultipleRepromptOpen = this.getIsDialogAlreadyOpenForMultipleReprompt();
     const { isDataOverviewOpen } = this.reduxStore.getState().popupStateReducer;
-    console.log('🚀 ~ PopupController ~ runPopup= ~ isDataOverviewOpen:', isDataOverviewOpen);
     const isRepromptOrOvieviewPopupOpen = isDialogForMultipleRepromptOpen || isDataOverviewOpen;
-    console.log('🚀 ~ PopupController ~ runPopup= ~ isDataOverviewOpen:', isDataOverviewOpen);
-    console.log('🚀 ~ PopupController ~ runPopup= ~ isDialogForMultipleRepromptOpen:', isDialogForMultipleRepromptOpen);
 
     this.reduxStore.dispatch(popupStateActions.setMstrData({ popupType }));
     this.reportParams = reportParams;
@@ -104,7 +98,6 @@ class PopupController {
     const splittedUrl = url.split('?'); // we need to get rid of any query params
     try {
       await officeApiHelper.getExcelSessionStatus();
-      console.log('🚀 ~ PopupController ~ runPopup= ~ isRepromptOrOvieviewPopupOpen:', isRepromptOrOvieviewPopupOpen);
       if (isRepromptOrOvieviewPopupOpen) {
         // US530793: If dialog already open, send message to dialog to reload with new object data.
         // This only occurs during Multiple Reprompt workflow.
@@ -131,7 +124,6 @@ class PopupController {
                 // Event received on dialog close
                 Office.EventType.DialogEventReceived,
                 () => {
-                  console.log('=============== reset state 2');
                   this.reduxStore.dispatch(this.popupActions.resetState());
                   this.reduxStore.dispatch(popupStateActions.onClearPopupState());
                   this.reduxStore.dispatch(officeActions.hidePopup());
@@ -152,7 +144,6 @@ class PopupController {
   };
 
   sendMessageToDialog = (message) => {
-    console.log('🚀 ~ PopupController ~ sendMessageToDialog ~ message:', message);
     this.dialog?.messageChild && this.dialog?.messageChild(message);
   };
 
@@ -160,7 +151,6 @@ class PopupController {
     const isMultipleRepromptQueueEmpty = this.getIsMultipleRepromptQueueEmpty();
     const { message } = arg;
     const response = JSON.parse(message);
-    console.log('🚀 ~ PopupController ~ onMessageFromPopup= ~ response:', response);
 
     const dialogType = this.reduxStore.getState().popupStateReducer.popupType;
     const { isDataOverviewOpen } = this.reduxStore.getState().popupStateReducer;
@@ -202,7 +192,6 @@ class PopupController {
           }
           break;
         case selectorProperties.commandCancel:
-          console.log('===================== Command Cancel');
           if (!isMultipleRepromptQueueEmpty || isDataOverviewOpen) {
             // Close dialog when user cancels, but only if there are objects left to Multiple Reprompt,
             // since we were previously keeping the dialog open in between objects.
@@ -291,7 +280,6 @@ class PopupController {
   // Used to reset popup-related state variables in Redux Store
   // and the dialog reference stored in the class object.
   resetPopupStates = () => {
-    console.log('=============== reset state 3');
     this.reduxStore.dispatch(this.popupActions.resetState());
     this.reduxStore.dispatch(popupStateActions.onClearPopupState());
     this.reduxStore.dispatch(officeActions.hidePopup());
