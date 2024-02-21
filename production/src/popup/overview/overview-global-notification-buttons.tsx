59fff240e5613e0fe9e1e2ef7871fe6d4016f6e4
agorski <agorski@microstrategy.com>
2024-02-21 15:46:15 +0100

    review overview-global-notification-buttons component

--- production/src/popup/overview/overview-global-notification-buttons.tsx
+++ production/src/popup/overview/overview-global-notification-buttons.tsx
@@ -5,16 +5,16 @@ export interface NotificationButton {
   onClick: () => void;
 }
 export type NotificationButtonsProps = {
-  buttons: [NotificationButton];
+  buttons: NotificationButton[];
 };
 
 export const OverviewGlobalNotificationButtons = (props: NotificationButtonsProps): ReactElement => {
   const { buttons } = props;
   return (
     <>
-      {buttons.map((button: NotificationButton) => (
-        <button type="button" onClick={button.onClick} key={button.label}>
-          {button.label}
+      {buttons.map(({ label, onClick }) => (
+        <button type="button" onClick={onClick} key={label}>
+          {label}
         </button>
       ))}
     </>
