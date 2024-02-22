ad7f33d6ee91c7c1dd4aa0752922c5f1bb3b12e7
Azizbek Tashpulatov <atashpulatov@microstrategy.com>
2024-02-22 15:51:09 -0500

    DE285034;Fix-the-edge-case

--- production/src/mstr-object/constants.js
+++ production/src/mstr-object/constants.js
@@ -1,3 +1,6 @@
+const defaultImagerangePos = { top: 0, left: 0 };
+const defaultTableRangePos = 'A1';
+
 export const objectImportType = {
   TABLE: 'table',
   IMAGE: 'image',
@@ -6,4 +9,9 @@ export const objectImportType = {
 export const importOperationStepDict = {
   GET_OFFICE_TABLE_IMPORT: objectImportType.IMAGE,
   MANIPULATE_VISUALIZATION_IMAGE: objectImportType.IMAGE,
+};
+
+export const defaultRangePosition = {
+  [objectImportType.IMAGE]: defaultImagerangePos,
+  [objectImportType.TABLE]: defaultTableRangePos
 };
