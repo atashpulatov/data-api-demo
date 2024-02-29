77bec84838abe47f215860b146e086b6cdd5d529
Azizbek Tashpulatov <atashpulatov@microstrategy.com>
2024-02-29 12:55:26 -0500

    DE287439;Increase-coverage

--- production/src/prompts/prompts-window.jsx
+++ production/src/prompts/prompts-window.jsx
@@ -225,10 +225,10 @@ export const PromptsWindowNotConnected = (props) => {
         .create(documentProps)
         .then(async (dossierPage) => {
           const [chapter, objectId, instanceId, visualizations] = await Promise.all([
-            dossierPage.getCurrentChapter(),
-            dossierPage.getDossierId(),
-            dossierPage.getDossierInstanceId(),
-            dossierPage.getCurrentPageVisualizationList(),
+            dossierPage?.getCurrentChapter(),
+            dossierPage?.getDossierId(),
+            dossierPage?.getDossierInstanceId(),
+            dossierPage?.getCurrentPageVisualizationList(),
           ]);
 
           const dossierData = {
