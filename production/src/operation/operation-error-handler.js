3b1b94f2fdb7ed7d526b0ff47ea014957ba4443a
Parra, Gregorio <gparra@microstrategy.com>
2024-03-05 14:42:48 -0500

    Updated comments...

--- production/src/operation/operation-error-handler.js
+++ production/src/operation/operation-error-handler.js
@@ -150,8 +150,10 @@ class OperationErrorHandler {
   };
 
   /**
-   * Determines whether to force the Overview table reload or refresh if there are any reprompts in queue
-   * and an error has occurred while reprompting dossier/report in Overview window only.
+   * Determines whether to close/hide Reprompt dialog in Overview window if an error has occured
+   * and needs to be displayed in Overview. Normally, it's triggered when reprompting dossier/report
+   * and user interacts with Prompts' dialog or cube is not published or dossier/report is not
+   * available in the environment as it was deleted at the time of reprompting.
    */
   closePromptsDialogInOverview = () => {
     const { repromptsQueueReducer, popupStateReducer } = this.reduxStore.getState();
