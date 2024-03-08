9b2de4555e6e887f121f95a8745025d0eab0937b
mbereszczuk <mbereszczuk@microstrategy.com>
2024-03-08 12:58:09 +0100

    US53719;more home tests

--- production/src/_tests_/home/home.test.js
+++ production/src/_tests_/home/home.test.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
 import { HomeNotConnected } from '../../home/home';
+import { notificationService } from '../../notification-v2/notification-service';
 import { reduxStore } from '../../store';
 
 jest.mock('../../home/home-helper');
 
-describe('Home', () => {
+describe('HomeNotConnected', () => {
   it('should render home component and side panel', () => {
     // given
 
@@ -34,5 +35,54 @@ describe('Home', () => {
 
     // then
     getByText('You do not have the rights to access MicroStrategy for Office');
+  });
+
+  it('should call connectionLost when offline event is triggered and dialog is not open', () => {
+    // given
+    const connectionLostSpy = jest.spyOn(notificationService, 'connectionLost');
+    const props = {
+      loading: false,
+      isDialogOpen: false,
+      authToken: 'testToken',
+      hideDialog: jest.fn(),
+      toggleIsSettingsFlag: jest.fn(),
+      clearDialogState: jest.fn()
+    };
+
+    // when
+    render(
+      <Provider store={reduxStore}>
+        <HomeNotConnected {...props} />
+      </Provider>,
+    );
+
+    // simulate offline event
+    window.dispatchEvent(new Event('offline'));
+
+    // then
+    expect(connectionLostSpy).toHaveBeenCalled();
+  });
+
+  it('should call sessionRestored when authToken is null', () => {
+    // given
+    const sessionRestoredSpy = jest.spyOn(notificationService, 'sessionRestored');
+    const props = {
+      loading: false,
+      isDialogOpen: false,
+      authToken: null,
+      hideDialog: jest.fn(),
+      toggleIsSettingsFlag: jest.fn(),
+      clearDialogState: jest.fn()
+    };
+
+    // when
+    render(
+      <Provider store={reduxStore}>
+        <HomeNotConnected {...props} />
+      </Provider>,
+    );
+
+    // then
+    expect(sessionRestoredSpy).toHaveBeenCalled();
   });
 });
