9e8db60b54116a2c2c422d32a6c996396f4ed383
bkoziel <bkoziel@microstrategy.com>
2024-03-19 11:37:38 +0100

    change ts comment

--- production/src/entry-point/sidebar-entry-point.tsx
+++ production/src/entry-point/sidebar-entry-point.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-// @ts-expect-error
+// @ts-ignore
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { reduxPersistor, reduxStore } from '../store';
 
