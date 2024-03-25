899c000007377a653882545436b28123b3f8544c
bkoziel <bkoziel@microstrategy.com>
2024-03-25 10:11:52 +0100

    convert reprompt-queue reducer

--- production/src/redux-reducer/reprompt-queue-reducer/reprompt-queue-reducer.test.ts
+++ production/src/redux-reducer/reprompt-queue-reducer/reprompt-queue-reducer.test.ts
@@ -1,10 +1,18 @@
-import React from 'react';
+import {
+  AddRepromptTaskAction,
+  ClearRepromptTasksAction,
+  ExecuteNextRepromptTaskAction,
+  RepromptQueueActions,
+  RepromptQueueActionTypes,
+} from './reprompt-queue-reducer-types';
 
 import * as RepromptQueueReducer from './reprompt-queue-reducer';
 
 describe('reprompt-queue-reducer', () => {
   it('should return the initial state', () => {
-    expect(RepromptQueueReducer.repromptsQueueReducer(undefined, {})).toEqual({
+    expect(
+      RepromptQueueReducer.repromptsQueueReducer(undefined, {} as RepromptQueueActions)
+    ).toEqual({
       index: 0,
       repromptsQueue: [],
       total: 0,
@@ -15,14 +23,14 @@ describe('reprompt-queue-reducer', () => {
     isPrompted
     ${true}
     ${false}
-  `('should call the add reprom[t task callback', isPrompted => {
+  `('should call the add repromt task callback', isPrompted => {
     const repromptCallback = jest.fn();
-    const action = {
+    const action: AddRepromptTaskAction = {
       payload: {
         callback: repromptCallback,
         isPrompted: isPrompted.isPrompted,
       },
-      type: 'ADD_REPROMPT_TASK',
+      type: RepromptQueueActionTypes.ADD_REPROMPT_TASK,
     };
 
     if (isPrompted.isPrompted) {
@@ -52,8 +60,8 @@ describe('reprompt-queue-reducer', () => {
 
   it('should call the execute next reprompt callback', () => {
     const repromptCallback = jest.fn();
-    const action = {
-      type: 'EXECUTE_NEXT_REPROMPT_TASK',
+    const action: ExecuteNextRepromptTaskAction = {
+      type: RepromptQueueActionTypes.EXECUTE_NEXT_REPROMPT_TASK,
     };
 
     expect(
@@ -78,8 +86,8 @@ describe('reprompt-queue-reducer', () => {
   });
 
   it('should call the clear reprompt callback', () => {
-    const action = {
-      type: 'CLEAR_REPROMPT_TASKS',
+    const action: ClearRepromptTasksAction = {
+      type: RepromptQueueActionTypes.CLEAR_REPROMPT_TASKS,
     };
 
     expect(RepromptQueueReducer.repromptsQueueReducer(undefined, action)).toEqual({
