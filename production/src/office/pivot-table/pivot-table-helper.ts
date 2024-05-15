69103505060288fd93ceb2e63a162c81043f29f5
agorski <agorski@microstrategy.com>
2024-05-15 17:00:59 +0200

    add helper functions to pivot

--- production/src/office/pivot-table/pivot-table-helper.ts
+++ production/src/office/pivot-table/pivot-table-helper.ts
@@ -1,3 +1,5 @@
+import { officeApiHelper } from '../api/office-api-helper';
+
 import { reduxStore } from '../../store';
 
 import { MstrTable } from '../../redux-reducer/operation-reducer/operation-reducer-types';
@@ -64,6 +66,51 @@ class PivotTableHelper {
 
     if (pivotTableAddMetricsToValues) {
       await this.addMetricsToValues(pivotTable, mstrTable, excelContext);
+    }
+  }
+
+  async removePivotSourceWorksheet(
+    worksheet: Excel.Worksheet,
+    excelContext: Excel.RequestContext,
+    officeTable: Excel.Table,
+    pivotTableId: string
+  ): Promise<void> {
+    let doesWorksheetExist = false;
+
+    worksheet.load('isNullObject');
+    await excelContext.sync();
+
+    if (worksheet && !worksheet.isNullObject) {
+      doesWorksheetExist = true;
+    }
+
+    // Added not to trigger the onDelete sheet event
+    excelContext.runtime.enableEvents = false;
+
+    if (officeTable && doesWorksheetExist) {
+      worksheet.visibility = Excel.SheetVisibility.hidden;
+      await excelContext.sync();
+
+      worksheet.delete();
+      await excelContext.sync();
+
+      const officeContext = await officeApiHelper.getOfficeContext();
+
+      if (pivotTableId) {
+        officeContext.document.bindings.releaseByIdAsync(pivotTableId);
+      }
+    }
+
+    excelContext.runtime.enableEvents = true;
+  }
+
+  async removePivotTable(
+    pivotTable: Excel.PivotTable,
+    excelContext: Excel.RequestContext
+  ): Promise<void> {
+    if (!pivotTable.isNullObject) {
+      pivotTable.delete();
+      await excelContext.sync();
     }
   }
 }
