c4ee78d40d93f5da47bd0829ee6ce30b7e3cf0ec
Akgul, Semih <sakgul@microstrategy.com>
2024-05-28 16:01:12 +0200

    DE296136; Object info translation (#2036)
    * DE296136 Fix object info translation
    
    * Fix request
    
    * add filter key
    
    ---------
    
    Co-authored-by: Demirhan, Sakir <sdemirhan@microstrategy.com>

--- production/src/redux-reducer/settings-reducer/settings-constants.ts
+++ production/src/redux-reducer/settings-reducer/settings-constants.ts
@@ -1,26 +1,22 @@
-import i18n from '../../i18n';
-
-const { t } = i18n;
-
 export const initialSidePanelObjectInfoSettings = [
-  { key: 'importedBy', item: t('Imported By'), showToggle: true, toggleChecked: true },
-  { key: 'owner', item: t('Owner'), showToggle: true, toggleChecked: true },
-  { key: 'modifiedDate', item: t('Date Modified'), showToggle: true, toggleChecked: true },
-  { key: 'createdDate', item: t('Date Created'), showToggle: true, toggleChecked: false },
-  { key: 'description', item: t('Description'), showToggle: true, toggleChecked: false },
-  { key: 'ancestors', item: t('Location'), showToggle: true, toggleChecked: true },
-  { key: 'version', item: t('Version'), showToggle: true, toggleChecked: false },
-  { key: 'id', item: t('ID'), showToggle: true, toggleChecked: true },
+  { key: 'importedBy', item: 'Imported By', showToggle: true, toggleChecked: true },
+  { key: 'owner', item: 'Owner', showToggle: true, toggleChecked: true },
+  { key: 'modifiedDate', item: 'Date Modified', showToggle: true, toggleChecked: true },
+  { key: 'createdDate', item: 'Date Created', showToggle: true, toggleChecked: false },
+  { key: 'description', item: 'Description', showToggle: true, toggleChecked: false },
+  { key: 'ancestors', item: 'Location', showToggle: true, toggleChecked: true },
+  { key: 'version', item: 'Version', showToggle: true, toggleChecked: false },
+  { key: 'id', item: 'ID', showToggle: true, toggleChecked: true },
 ];
 
 export const initialWorksheetObjectInfoSettings = [
-  { key: 'name', item: t('Name'), showToggle: true, toggleChecked: false },
-  { key: 'owner', item: t('Owner'), showToggle: true, toggleChecked: false },
-  { key: 'description', item: t('Description'), showToggle: true, toggleChecked: false },
-  { key: 'filters', item: t('Filter'), showToggle: true, toggleChecked: false },
-  { key: 'importedBy', item: t('Imported By'), showToggle: true, toggleChecked: false },
-  { key: 'modifiedDate', item: t('Date Modified'), showToggle: true, toggleChecked: false },
-  { key: 'createdDate', item: t('Date Created'), showToggle: true, toggleChecked: false },
-  { key: 'id', item: t('ID'), showToggle: true, toggleChecked: false },
-  { key: 'pageBy', item: t('Page-By Information'), showToggle: true, toggleChecked: false },
+  { key: 'name', item: 'Name', showToggle: true, toggleChecked: false },
+  { key: 'owner', item: 'Owner', showToggle: true, toggleChecked: false },
+  { key: 'description', item: 'Description', showToggle: true, toggleChecked: false },
+  { key: 'filters', item: 'Filter', showToggle: true, toggleChecked: false },
+  { key: 'importedBy', item: 'Imported By', showToggle: true, toggleChecked: false },
+  { key: 'modifiedDate', item: 'Date Modified', showToggle: true, toggleChecked: false },
+  { key: 'createdDate', item: 'Date Created', showToggle: true, toggleChecked: false },
+  { key: 'id', item: 'ID', showToggle: true, toggleChecked: false },
+  { key: 'pageBy', item: 'Page-By Information', showToggle: true, toggleChecked: false },
 ];
