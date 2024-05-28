d3f192d486869a225f0f21a5b9cc397038a9adf1
Azizbek Tashpulatov <atashpulatov@microstrategy.com>
2024-05-28 17:05:03 -0400

    DE294452;Added-jsdoc

--- production/src/mstr-object/formatted-data-helper.ts
+++ production/src/mstr-object/formatted-data-helper.ts
@@ -1,3 +1,9 @@
+/**
+ * Retrieves the shape collection of given woksheet.
+ *
+ * @param worksheet Excel worksheet
+ * @param excelContext Reference to Excel Context used by Excel API functions
+ */
 export const getShapeCollection = async (worksheet: Excel.Worksheet, excelContext: Excel.RequestContext): Promise<Excel.ShapeCollection> => {
     worksheet.load('shapes');
     await excelContext.sync();
