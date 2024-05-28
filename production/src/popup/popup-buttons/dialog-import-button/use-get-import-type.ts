57bce525cd0baf12bc6547f7b22812775ff6a85d
Azizbek Tashpulatov <atashpulatov@microstrategy.com>
2024-05-28 12:02:19 -0400

    DE296134;Fix-formatted-data-default-import-type

--- production/src/popup/popup-buttons/dialog-import-button/use-get-import-type.ts
+++ production/src/popup/popup-buttons/dialog-import-button/use-get-import-type.ts
@@ -18,7 +18,13 @@ const useGetImportType = (options: ImportButtonOptionsType[]): ObjectImportType 
     return undefined;
   }
 
-  const isDefaultImportTypeSupportedByObject = options.some(({ key }) => key === defaultImportType);
+  // Despite the formatted data import type is being supported by default for report/dossiers, the supported 
+  // import options set is missing the formatted data import type, due to the object is not being selected yet.
+  const isDefaultImportTypeFormattedData = defaultImportType === ObjectImportType.FORMATTED_DATA;
+
+  const isDefaultImportTypeSupportedByObject = options.some(({ key }) => key === defaultImportType)
+    || isDefaultImportTypeFormattedData;
+
   const fallbackImportType = isDefaultImportTypeSupportedByObject
     ? defaultImportType
     : ObjectImportType.TABLE;
