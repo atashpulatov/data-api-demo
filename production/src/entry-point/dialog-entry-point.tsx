28d1278d28ddbc81c456864fcf84e158f64cd157
Parra, Gregorio <gparra@microstrategy.com>
2024-05-29 10:29:41 -0400

    DE291188; refactored, better check and updated comments

--- production/src/entry-point/dialog-entry-point.tsx
+++ production/src/entry-point/dialog-entry-point.tsx
@@ -13,15 +13,15 @@ import { Popup } from '../popup/popup';
 const DialogEntryPoint: React.FC = () => {
   useEffect(() => {
     // DE291188: Initialize popup type from URL to tacle Redux Re-hydration issue
-    // which causes popup type's value to be out-of-sync with SidePane's state
+    // which causes popup type's value to be out-of-sync with SidePanel's state
     // on dialog reload for imported data overview or overview's reprompt dialog type.
     if (window.location?.href?.includes('popupType')) {
       const url = new URL(window.location.href);
-      const popupType = url.searchParams.get('popupType') as DialogType;
-      if (popupType) {
+      const urlPopupType = url.searchParams.get('popupType');
+      if (urlPopupType && urlPopupType.trim() !== '') {
         reduxStore.dispatch({
           type: PopupStateActionTypes.SET_POPUP_TYPE,
-          popupType,
+          popupType: urlPopupType as DialogType,
         });
       }
     }
